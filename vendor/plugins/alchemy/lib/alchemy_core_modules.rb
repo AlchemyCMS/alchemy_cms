ee1d196d7130c48dad45b01b74fe84bbbacd1805
Thomas von Deyen <thomas@vondeyen.com>
2010-06-02 16:15:39 +0000

    new styling

--- vendor/plugins/alchemy/lib/alchemy_core_modules.rb
+++ vendor/plugins/alchemy/lib/alchemy_core_modules.rb
@@ -1,5 +1,8 @@
 # We use this file so that gettext can find them for translate the core alchemy modules
 _('module: dashboard')
 _('module: pages')
+_('module: systempages')
 _('module: users')
 _('module: library')
+_('module: files')
+_('module: images')
