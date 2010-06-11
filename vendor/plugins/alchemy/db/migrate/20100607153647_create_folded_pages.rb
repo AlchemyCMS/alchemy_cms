e77d7d644fd3ce7794a338a11b58d1022b9b31de
Thomas von Deyen <thomas@vondeyen.com>
2010-06-11 13:37:40 +0000

    moving migrations to plugin and updated translation files

--- vendor/plugins/alchemy/db/migrate/20100607153647_create_folded_pages.rb
+++ vendor/plugins/alchemy/db/migrate/20100607153647_create_folded_pages.rb
@@ -1,8 +1,9 @@
 class CreateFoldedPages < ActiveRecord::Migration
   def self.up
-    create_table :folded_pages, :id => false do |t|
+    create_table :folded_pages do |t|
       t.integer :page_id
       t.integer :user_id
+      t.boolean :folded, :default => false
     end
   end
 
