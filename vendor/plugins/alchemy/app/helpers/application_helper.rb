c29a33d2d876f029758f87eba709a23858448887
Thomas von Deyen <thomas@vondeyen.com>
2010-06-16 19:01:05 +0000

    removing log notice

--- vendor/plugins/alchemy/app/helpers/application_helper.rb
+++ vendor/plugins/alchemy/app/helpers/application_helper.rb
@@ -105,7 +105,6 @@ module ApplicationHelper
       partial_name = "_#{element.name.underscore}_#{part}.html.erb"
       if File.exists?(path1 + partial_name) || File.exists?(path2 + partial_name)
         if @preview_mode
-          logger.info "\n§§§ @preview_mode: #{@preview_mode}\n"
           render(
             :partial => 'admin/elements/element_preview',
             :locals => {
