f246c54e30ec90def6eedd181daa20c5017fc17f
Thomas von Deyen <thomas@vondeyen.com>
2010-07-02 13:35:23 +0000

    protocol in urls has to be strings....

--- vendor/plugins/alchemy/app/helpers/application_helper.rb
+++ vendor/plugins/alchemy/app/helpers/application_helper.rb
@@ -421,7 +421,7 @@ module ApplicationHelper
     )
     if @page.contains_feed?
     meta_string += %(
-      <link rel="alternate" type="application/rss+xml" title="RSS" href="#{multi_language? ? show_page_with_language_url(:protocol => :feed, :urlname => @page.urlname, :lang => session[:language], :format => :rss) : show_page_url(:protocol => :feed, :urlname => @page.urlname, :format => :rss)}" />
+      <link rel="alternate" type="application/rss+xml" title="RSS" href="#{multi_language? ? show_page_with_language_url(:protocol => 'feed', :urlname => @page.urlname, :lang => session[:language], :format => :rss) : show_page_url(:protocol => 'feed', :urlname => @page.urlname, :format => :rss)}" />
     )
     end
     return meta_string
