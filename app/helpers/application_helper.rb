080dc9de2a2121c08c6682a5b22e3e9f819604c9
Thomas von Deyen <thomas@vondeyen.com>
2010-07-21 12:36:54 +0000

    helpers for 001 layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,26 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def header_image
+    begin
+      url_for(
+        :controller => :pictures,
+        :action => :show,
+        :id => current_page.elements.find_by_name("header").content_by_type('EssencePicture').essence.picture.id,
+        :size => "1200x1200",
+        :format => :jpg
+      )
+    rescue
+      ""
+    end
+  end
+  
+  def language_switches
+    links = []
+    Page.find(:all, :conditions => "language_root_for IS NOT NULL AND public=1").each do |page|
+      links << link_to(page.language.upcase, show_page_with_language_url(:urlname => page.urlname, :lang => page.language), :class => (session[:language] == page.language ? 'active' : nil))
+    end
+    links.join("<span class='seperator'></span>")
+  end
+  
 end
