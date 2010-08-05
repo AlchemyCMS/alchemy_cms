655972c73acd79359f2c9b12b9d5744463b8b978
Thomas von Deyen <tvdeyen@gmail.com>
2010-08-05 13:39:40 +0200

    moving to plugin structure removing all rails app specfific failes and moved the plugin to root

--- lib/tasks/custom_plugins_tasks.rake
+++
@@ -1,3 +0,0 @@
-Dir.glob(File.dirname(__FILE__) + "/../../vendor/plugins/alchemy/plugins/**/tasks/*.rake").each do |rake_file|
-  import rake_file
-end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 ENV["RAILS_ENV"] = "test"
-require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+require "#{RAILS_ROOT}/config/environment"
 require 'test_help'
 
 class Test::Unit::TestCase
