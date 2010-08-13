88c3fdafbb516cb04489e79880d94aaad6997c04
Thomas von Deyen <tvdeyen@gmail.com>
2010-08-13 14:22:27 +0200

    fixed rdoc for Mail model

--- app/models/mail.rb
+++ app/models/mail.rb
@@ -1,49 +1,48 @@
+# This is a tableless model only used for validating Contactform Fields.
+# You can specify the fields for your contactform in the config/alchemy/config.yml file in the :mailer options
+# 
+# === Options:
+# 
+# - form_layout_name: A PageLayout name (String). Used to render the contactform on a page with this layout.
+# 
+# === Fieldtypes are:
+#
+# - string
+# - text
+# - boolean
+# 
+# === Validating fields:
+# 
+# Pass the field name as symbol and a message_id (will be translated) to :validate_fields:
+# 
+# === Translating the validation messages:
+# 
+# Validationmessages will be send to +I18n.t+ method with the scope +"contactform.validations.#{field[1][:message].to_s}"+.
+# So a +name+ field with the validation message_id +blank_name+ will be available for translation in your locale files like:
+# 
+#   de:
+#     contactform:
+#       validations:
+#         blank_name: 'Bitte geben Sie Ihren Namen an'
+#   
+# === Example Contactform Configuration:
+# 
+#   :mailer:
+#     :form_layout_name: contact
+#     :fields:
+#       - subject: :string
+#       - name: :string
+#       - email: :string
+#       - message: :text
+#       - info: :boolean
+#     :validate_fields:
+#       :name:
+#         :message: blank_name
+#       :email:
+#         :message: blank_email
+
+
 class Mail < ActiveRecord::Base
-  
-  # This is a tableless model only used for validating Contactform Fields.
-  # You can specify the fields for your contactform in the config/alchemy/config.yml file in the :mailer options
-  # 
-  # == Options:
-  # 
-  # - form_layout_name: A PageLayout name (String). Used to render the contactform on a page with this layout.
-  # 
-  # == Fieldtypes are:
-  #
-  # - string
-  # - text
-  # - boolean
-  # 
-  # == Validating fields:
-  # 
-  # Pass the field name as symbol and a message_id (will be translated) to :validate_fields:
-  # 
-  # == Translating the validation messages:
-  # 
-  # Validationmessages will be send to I18n.t method with the scope "contactform.validations.#{field[1][:message].to_s}".
-  # So a 'name' field with the validation message_id 'blank_name' will be available for translation in your locale files like:
-  # 
-  # === Example Translation:
-  # 
-  #   de:
-  #     contactform:
-  #       validations:
-  #         blank_name: 'Bitte geben Sie Ihren Namen an'
-  #   
-  # == Example Contactform Configuration:
-  # 
-  #   :mailer:
-  #     :form_layout_name: contact
-  #     :fields:
-  #       - subject: :string
-  #       - name: :string
-  #       - email: :string
-  #       - message: :text
-  #       - info: :boolean
-  #     :validate_fields:
-  #       :name:
-  #         :message: blank_name
-  #       :email:
-  #         :message: blank_email
   
   include Tableless
   
@@ -63,7 +62,7 @@ class Mail < ActiveRecord::Base
   
 private
   
-  def email_is_filled 
+  def email_is_filled #:nodoc:
     !email.blank? 
   end
   
