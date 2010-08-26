7e198a9209c07b3317e4e0c75510f927e6c949d6
Thomas von Deyen <tvdeyen@gmail.com>
2010-08-26 16:09:06 +0200

    Feature: closing upload overlay after upload queue completes

--- assets/javascripts/swfupload/handlers.js
+++ assets/javascripts/swfupload/handlers.js
@@ -1,15 +1,5 @@
-/* Demo Note:  This demo uses a FileProgress class that handles the UI for displaying the file name and percent complete.
-The FileProgress class is not part of SWFUpload.
-*/
-
-
 /* **********************
-   Event Handlers
-   These are my custom event handlers to make my
-   web application behave the way I went when SWFUpload
-   completes different tasks.  These aren't part of the SWFUpload
-   package.  They are part of my application.  Without these none
-   of the actions SWFUpload makes will show up in my application.
+   Alchemy SWFUpload Event Handlers
    ********************** */
 function fileQueued(file) {
 	try {
@@ -177,4 +167,5 @@ function queueComplete(numFilesUploaded) {
 	status.show();
 	status.innerHTML = numFilesUploaded + " file" + (numFilesUploaded === 1 ? "" : "s") + " uploaded.";
 	$('btnCancel').hide();
+	alchemy_window.close();
 }
