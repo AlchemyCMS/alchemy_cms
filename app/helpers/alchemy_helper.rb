3e0993621ef47cc48fea82ad16afd74cecd28921
Thomas von Deyen <tvdeyen@gmail.com>
2011-02-15 12:02:48 +0100

    Disabling all links in preview window. Fixes #692

--- app/helpers/alchemy_helper.rb
+++ app/helpers/alchemy_helper.rb
@@ -1095,17 +1095,16 @@ module AlchemyHelper
   
   def alchemy_preview_mode_code
     if @preview_mode
-      #jquery_script_tag = javascript_include_tag("alchemy/jquery-1.4.4.min", :cache => 'preview')
       append_javascript = %(
       var s = document.createElement('script');
-      s.src = '/javascripts/alchemy/jquery-1.4.4.min.js';
+      s.src = '/javascripts/alchemy/jquery-1.5.min.js';
       s.language = 'javascript';
       s.type = 'text/javascript';
       document.getElementsByTagName("body")[0].appendChild(s);
       )
-      str = javascript_tag("if(typeof(jQuery)=='function'){jQuery.noConflict();} else {#{append_javascript}}")
-      str += javascript_include_tag("alchemy/alchemy", :cache => 'preview')
-      str += javascript_tag("jQuery(document).ready(function(){Alchemy.ElementSelector();});")
+      str = javascript_tag("if(typeof(jQuery)=='function'){jQuery.noConflict();}else{#{append_javascript}}")
+      str += javascript_include_tag("alchemy/alchemy")
+      str += javascript_tag("jQuery(document).ready(function(){Alchemy.ElementSelector();});jQuery('a').attr('href', 'javascript:void(0)');")
       return str
     else
       return nil
