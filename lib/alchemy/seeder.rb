642a3cae82b0daca6f0e8ddc782879e505585d85
Thomas von Deyen <tvdeyen@gmail.com>
2011-02-28 10:24:45 +0100

    Better seeder

--- lib/alchemy/seeder.rb
+++ lib/alchemy/seeder.rb
@@ -5,6 +5,8 @@ module Alchemy
     # Put Alchemy::Seeder.seed! inside your db/seeds.rb file and run it with rake db:seed.
     def self.seed!
       errors = []
+      notices = []
+
       lang = Language.find_or_initialize_by_code(
         :name => 'Deutsch',
         :code => 'de',
@@ -13,91 +15,115 @@ module Alchemy
         :public => true,
         :default => true
       )
-      if lang.new_record? && lang.save
-        puts "== Created Language #{lang.name}"
-        root = Page.find_or_initialize_by_name(
-          :name => 'Root',
-          :do_not_autogenerate => true,
-          :do_not_sweep => true,
-          :language => lang
-        )
-        if root.new_record? && root.save
-          puts "== Created Page #{root.name}"
+      if lang.new_record?
+        if lang.save
+          puts "== Created language #{lang.name}"
+        else
+          errors << "Errors creating language #{lang.name}: #{lang.errors.full_messages}"
+        end
+      else
+        notices << "Language #{lang.name} already present"
+      end
+
+      root = Page.find_or_initialize_by_name(
+        :name => 'Root',
+        :do_not_autogenerate => true,
+        :do_not_sweep => true,
+        :language => lang
+      )
+      if root.new_record?
+        if root.save
           # We have to remove the language, because active record validates its presence on create.
           root.language = nil
           root.save
+          puts "== Created page #{root.name}"
         else
-          errors << "Root page could not be created"
-        end
-        index = Page.find_or_initialize_by_name(
-          :name => lang.frontpage_name,
-          :page_layout => lang.page_layout,
-          :public => false,
-          :visible => true,
-          :language => lang,
-          :language_root => true
-        )
-        if index.new_record? && index.save
-          puts "== Created Page #{index.name}"
-        else
-          errors << "Index page could not be created"
-        end
-        layoutroot = Page.find_or_initialize_by_name(
-          :name => 'LayoutRoot',
-          :do_not_autogenerate => true,
-          :do_not_sweep => true,
-          :layoutpage => true,
-          :language => lang
-        )
-        if layoutroot.new_record? && layoutroot.save
-          puts "== Created Page #{layoutroot.name}"
-        else
-          errors << "Layoutroot page could not be created"
-        end
-        header = Page.find_or_initialize_by_name(
-          :name => 'Layout Header',
-          :page_layout => 'layout_header',
-          :layoutpage => true,
-          :language => lang
-        )
-        if header.new_record? && header.save
-          puts "== Created Page #{header.name}"
-        else
-          errors << "Header layoutpage could not be created"
-        end
-        footer = Page.find_or_initialize_by_name(
-          :name => 'Layout Footer',
-          :page_layout => 'layout_footer',
-          :layoutpage => true,
-          :language => lang
-        )
-        if footer.new_record? && footer.save
-          puts "== Created Page #{footer.name}"
-        else
-          errors << "Footer layoutpage could not be created"
-        end        
-        if errors.blank?
-          index.move_to_child_of root
-          layoutroot.move_to_child_of root
-          header.move_to_child_of layoutroot
-          footer.move_to_child_of layoutroot
-        else
-          puts "== Aborting! Some pages could not be created:\n#{errors.join('\n')}"
-          raise
+          errors << "Errors creating page #{root.name}: #{root.errors.full_messages}"
         end
       else
-        raise "== Aborting! Creating language failed!"
+        notices << "Page #{root.name} already present"
       end
+
+      index = Page.find_or_initialize_by_name(
+        :name => lang.frontpage_name,
+        :page_layout => lang.page_layout,
+        :language => lang,
+        :language_root => true
+      )
+      if index.new_record?
+        if index.save
+          puts "== Created page #{index.name}"
+        else
+          errors << "Errors creating page #{index.name}: #{index.errors.full_messages}"
+        end
+      else
+        notices << "Page #{index.name} already present"
+      end
+      
+      layoutroot = Page.find_or_initialize_by_name(
+        :name => 'LayoutRoot',
+        :do_not_autogenerate => true,
+        :do_not_sweep => true,
+        :layoutpage => true,
+        :language => lang
+      )
+      if layoutroot.new_record?
+        if layoutroot.save
+          puts "== Created Page #{layoutroot.name}"
+        else
+          errors << "Errors creating page #{layoutroot.name}: #{layoutroot.errors.full_messages}"
+        end
+      else
+        notices << "Page #{layoutroot.name} already present"
+      end
+      
+      header = Page.find_or_initialize_by_name(
+        :name => 'Layout Header',
+        :page_layout => 'layout_header',
+        :layoutpage => true,
+        :language => lang
+      )
+      if header.new_record?
+        if header.save
+          puts "== Created Page #{header.name}"
+        else
+          errors << "Errors creating page #{header.name}: #{header.errors.full_messages}"
+        end
+      else
+        notices << "Page #{header.name} already present"
+      end
+      
+      footer = Page.find_or_initialize_by_name(
+        :name => 'Layout Footer',
+        :page_layout => 'layout_footer',
+        :layoutpage => true,
+        :language => lang
+      )
+      if footer.new_record?
+        if footer.save
+          puts "== Created Page #{footer.name}"
+        else
+          errors << "Errors creating page #{footer.name}: #{footer.errors.full_messages}"
+        end        
+      else
+        notices << "Page #{footer.name} already present"
+      end
+      
+      if errors.blank?
+        index.move_to_child_of root
+        layoutroot.move_to_child_of root
+        header.move_to_child_of layoutroot
+        footer.move_to_child_of layoutroot
+      else
+        puts "WARNING! Some pages could not be created:"
+        errors.map{ |error| puts error }
+      end
+      puts "Success!"
+      notices.map{ |note| puts note }
     end
     
     # This method is for running after an upgrade of Alchemy if you already seeded the db once.
     # Put Alchemy::Seeder.upgrade! inside your db/seeds.rb file and run it with rake db:seed.
-    #
-    # CAUTION!
-    #
-    # Please replace existing Alchemy::Seeder.seed! with Alchemy::Seeder.upgrade! or 
-    # your db might get corrupted.
-    #
     def self.upgrade!
       lang = Language.find_or_initialize_by_code(
         :name => 'Deutsch',
