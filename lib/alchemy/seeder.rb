3557d5f6aa269e59203e50e39ef3c11d380a6f1a
Thomas von Deyen <tvdeyen@gmail.com>
2011-03-04 20:05:57 +0100

    Fixing seeder

--- lib/alchemy/seeder.rb
+++ lib/alchemy/seeder.rb
@@ -4,9 +4,14 @@ module Alchemy
     # This seed builds the necessary page structure for alchemy in your db.
     # Put Alchemy::Seeder.seed! inside your db/seeds.rb file and run it with rake db:seed.
     def self.seed!
+      FastGettext.add_text_domain 'alchemy', :path => File.join(Rails.root, 'vendor/plugins/alchemy/locale')
+      FastGettext.text_domain = 'alchemy'
+      FastGettext.available_locales = ['de', 'en']
+      FastGettext.locale = 'de'
+      
       errors = []
       notices = []
-
+      
       lang = Language.find_or_initialize_by_code(
         :name => 'Deutsch',
         :code => 'de',
@@ -88,19 +93,10 @@ module Alchemy
       notices.map{ |note| puts note }
     end
     
-    # This method is for running after an upgrade of Alchemy if you already seeded the db once.
+    # This method is for running after upgrading an old Alchemy version without Language Model (pre v1.5).
     # Put Alchemy::Seeder.upgrade! inside your db/seeds.rb file and run it with rake db:seed.
     def self.upgrade!
-      lang = Language.find_or_initialize_by_code(
-        :name => 'Deutsch',
-        :code => 'de',
-        :frontpage_name => 'Startseite',
-        :page_layout => 'intro',
-        :public => true
-      )
-      lang.default = true
-      lang.save
-      ActiveRecord::Base.connection.execute("UPDATE pages SET language_root = 1 WHERE language_root IS NOT NULL")
+      seed!
       Page.all.each do |page|
         unless page.language_code.blank?
           root = page.get_language_root
