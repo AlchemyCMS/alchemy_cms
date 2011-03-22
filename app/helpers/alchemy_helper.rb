8da0f387620b34d9a419cbfaff735979e444345b
Thomas von Deyen <tvdeyen@gmail.com>
2011-03-22 18:37:18 +0100

    Now one can create a missing content editor inside of an element editor. Closes #706

--- app/helpers/alchemy_helper.rb
+++ app/helpers/alchemy_helper.rb
@@ -193,13 +193,13 @@ module AlchemyHelper
       }
     )
   end
-
+  
   # Renders the Content editor partial from the given Content.
   # For options see -> render_essence
   def render_essence_editor(content, options = {})
     render_essence(content, :editor, :for_editor => options)
   end
-
+  
   # Renders the Content view partial from the given Content.
   # For options see -> render_essence
   def render_essence_view(content, options = {}, html_options = {})
@@ -249,7 +249,7 @@ module AlchemyHelper
     content = element.contents.find_by_position(position)
     render_essence(content, :view, {:for_view => options}, html_options)
   end
-
+  
   # Renders the Content editor partial from the given Element by position (e.g. 1).
   # For options see -> render_essence
   def render_essence_editor_by_position(element, position, options = {})
@@ -260,17 +260,26 @@ module AlchemyHelper
     content = element.contents.find_by_position(position)
     render_essence(content, :editor, :for_editor => options)
   end
-
+  
   # Renders the Content editor partial found in views/contents/ for the content with name inside the passed Element.
   # For options see -> render_essence
+  # 
+  # Content creation on the fly:
+  # 
+  # If you update the elements.yml file after creating an element this helper displays a error message with an option to create the content.
+  #
   def render_essence_editor_by_name(element, name, options = {})
     if element.blank?
       return warning('Element is nil', _("no_element_given"))
     end
     content = element.content_by_name(name)
-    render_essence(content, :editor, :for_editor => options)
+    if content.blank?
+      render :partial => 'admin/contents/missing', :locals => {:element => element, :name => name}
+    else
+      render_essence(content, :editor, :for_editor => options)
+    end
   end
-
+  
   # Renders the Content view partial from the passed Element for passed content name.
   # For options see -> render_essence
   def render_essence_view_by_name(element, name, options = {}, html_options = {})
