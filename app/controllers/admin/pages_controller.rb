c8c8353ab3ed0f3ca31fa4627779c390c6af42f9
Thomas von Deyen <tvdeyen@gmail.com>
2011-04-14 16:19:41 +0200

    Setting correct I18n.locale for Admin::Pages.show (preview)

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -20,7 +20,7 @@ class Admin::PagesController < AlchemyController
   def show
     # fetching page via before filter
     @preview_mode = true
-    @language = Alchemy::Controller.current_language
+    I18n.locale = session[:language_code]
     @root_page = Page.language_root_for(session[:language_id])
     render :layout => params[:layout].blank? ? 'pages' : params[:layout] == 'none' ? false : params[:layout]
   end
