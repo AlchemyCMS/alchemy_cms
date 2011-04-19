f67ffb41a6dce4e459844e9733b78006e2a31dbb
Robin Böning <robin.boening@nethosting4you.de>
2011-04-19 21:03:26 +0200

    v1.5.4.1 html view for Elements#show

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -1,12 +1,16 @@
 class ElementsController < AlchemyController
   
   filter_access_to [:show], :attribute_check => true
+  layout false
   
+  # Returns the element partial as HTML or as JavaScript that tries to replace a given +container_id+ with the partial content via jQuery.
   def show
     @element = Element.find(params[:id])
     @page = @element.page
-    @container_id = params[:container_id]
-    render :layout => false
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
   
 end
