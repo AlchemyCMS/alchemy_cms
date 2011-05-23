fb73de1c130f552b9ea1cabeb27a8001fd382e07
Thomas von Deyen <tvdeyen@gmail.com>
2011-05-23 12:12:37 +0200

    Fixed Alchemy::Config.read_files

--- lib/alchemy/config.rb
+++ lib/alchemy/config.rb
@@ -17,25 +17,34 @@ module Alchemy
   private
     
     def self.read_files
-      config_1 = {}
-      config_2 = {}
-      config_3 = {}
-      name = name.to_sym if name.class == String
-      
       # Looking for any Rails Environment specific configuration
-      config_file_1 = "#{Rails.root.to_s}/config/alchemy/config_#{Rails.env}.yml"
-      config_1 = YAML.load_file(config_file_1) if File.exists? config_file_1
+      if File.exists? "#{Rails.root.to_s}/config/alchemy/config_#{Rails.env}.yml"
+        config_1 = YAML.load_file( "#{Rails.root.to_s}/config/alchemy/config_#{RAILS_ENV}.yml" )
+      else
+        config_1 = {}
+      end
       
-      # Looking for Applikation specific configuration
-      config_file_2 = "#{Rails.root.to_s}/config/alchemy/config.yml"
-      config_2 = YAML.load_file(config_file_2) if File.exists? config_file_2
+      # Looking for Application specific configuration
+      if File.exists? "#{Rails.root.to_s}/config/alchemy/config.yml"
+        config_2 = YAML.load_file( "#{Rails.root.to_s}/config/alchemy/config.yml" )
+      else
+        config_2 = {}
+      end
       
       # Reading Alchemy standard configuration
-      config_file_3 = File.join(File.dirname(__FILE__), '..', '..', 'config/alchemy/config.yml')
-      config_3 = YAML.load_file(config_file_3) if File.exists? config_file_3
+      if File.exists? File.join(File.dirname(__FILE__), '..', '..', 'config/alchemy/config.yml')
+        config_3 = YAML.load_file( File.join(File.dirname(__FILE__), '..', '..', 'config/alchemy/config.yml') )
+      else
+        config_3 = {}
+      end
       
       # Mergin all together
-      config = config_3.merge(config_2.merge(config_1))
+      if config_1.blank? && config_3.blank? && config_3.blank?
+        raise LoadError, 'No Alchemy config file found!'
+      else
+        [config_1, config_2, config_3].map(&:stringify_keys!)
+        return config_3.merge(config_2.merge(config_1))
+      end
     end
     
   end
