c38d91762ab86112bd8034c0b5a5f57fc9b3018e
Thomas von Deyen <tvdeyen@gmail.com>
2011-05-29 18:13:33 +0200

    Converted alchemy:elements generator to Rails 3 syntax and refactored it

---
+++ lib/generators/alchemy/elements/elements_generator.rb
@@ -0,0 +1,35 @@
+require 'rails'
+
+module Alchemy
+  module Generators
+    class ElementsGenerator < ::Rails::Generators::Base
+      desc "This generator generates your elements view partials."
+      source_root File.expand_path('templates', File.dirname(__FILE__))
+      
+      def create_directory
+        @element_dir = "#{Rails.root}/app/views/elements"
+        empty_directory @element_dir
+      end
+      
+      def create_partials
+        @elements = get_elements_from_yaml
+        @elements.each do |element|
+          @element = element
+          @contents = element["contents"]
+          @element_name = element["name"].underscore
+          template "editor.html.erb", "#{@element_dir}/_#{@element_name}_editor.html.erb"
+          template "view.html.erb", "#{@element_dir}/_#{@element_name}_view.html.erb"
+        end if @elements
+      end
+      
+    private
+      
+      def get_elements_from_yaml
+        YAML.load_file "#{Rails.root}/config/alchemy/elements.yml"
+      rescue Errno::ENOENT
+        puts "\nERROR: Could not read config/alchemy/elements.yml file. Please run: rails generate alchemy:scaffold"
+      end
+      
+    end
+  end
+end
