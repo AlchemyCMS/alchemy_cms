e1a6d48db07912c1dd64be7d48e2feaf9ad63617
Thomas von Deyen <tvdeyen@gmail.com>
2011-05-29 16:45:12 +0200

    Converted alchemy_plugin generator to Rails 3 syntax

--- generators/alchemy_plugin/templates/init.rb
+++
@@ -1,4 +0,0 @@
-if defined? FastGettext
-  FastGettext.add_text_domain '<%= file_name %>', :path => File.join(File.dirname(__FILE__), 'locale'), :format => :po
-  FastGettext.text_domain = '<%= file_name %>'
-end
