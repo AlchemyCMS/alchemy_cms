ca9bdc44f7423765be566b7f904ff65b60cc1a0a
Thomas von Deyen <tvd@magiclabs.de>
2011-09-29 01:17:15 +0200

    Updated to TinyMCE 3.4.5 and added autoresize plugin

--- config/initializers/tiny_mce_hammer.rb
+++ config/initializers/tiny_mce_hammer.rb
@@ -1,6 +1,6 @@
 if defined?(Tinymce::Hammer)
   Tinymce::Hammer.install_path = '/javascripts/alchemy/tiny_mce'
-  Tinymce::Hammer.plugins = %w(safari paste fullscreen inlinepopups alchemy_link)
+  Tinymce::Hammer.plugins = %w(autoresize paste fullscreen inlinepopups alchemy_link)
   Tinymce::Hammer.languages = ['de', 'en']
   Tinymce::Hammer.init = {
     :paste_convert_headers_to_strong => true,
@@ -16,16 +16,12 @@ if defined?(Tinymce::Hammer)
     :content_css => "/stylesheets/alchemy/alchemy_tinymce_content.css",
     :dialog_type => "modal",
     :width => "100%",
-    :height => '185',
     :theme_advanced_toolbar_align => 'left',
     :theme_advanced_toolbar_location => 'top',
     :theme_advanced_statusbar_location => 'bottom',
     :theme_advanced_buttons1 => 'bold,italic,underline,strikethrough,sub,sup,|,numlist,bullist,indent,outdent,|,alchemy_link,unlink,|,removeformat,cleanup,|,fullscreen',
     :theme_advanced_buttons2 => 'pastetext,pasteword,charmap,code,help',
     :theme_advanced_buttons3 => '',
-    :theme_advanced_resizing => 'true',
-    :theme_advanced_resize_horizontal => false,
-    :theme_advanced_resizing_min_height => '185',
     :fix_list_elements => true,
     :convert_urls => false
   }
