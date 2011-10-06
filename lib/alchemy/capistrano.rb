88174a7502fa66a633d51e62d314e89fe630a572
Robin Böning <robin.boening@nethosting4you.de>
2011-10-06 20:00:17 +0200

    removing /uploads/* not /public/uploads before symlinking

--- lib/alchemy/capistrano.rb
+++ lib/alchemy/capistrano.rb
@@ -28,7 +28,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       # Call after deploy:symlink like +after "deploy:symlink", "alchemy:symlink_folders"+ in your +deploy.rb+.
       desc "Sets the symlinks for uploads, picture cache and ferret index folder. Call after deploy:symlink"
       task :symlink, :roles => :app do
-        run "rm -rf #{current_path}/public/uploads/*"
+        run "rm -rf #{current_path}/uploads/*"
         run "ln -nfs #{shared_path}/uploads/pictures/ #{current_path}/uploads/pictures"
         run "ln -nfs #{shared_path}/uploads/attachments/ #{current_path}/uploads/attachments"
         run "rm -rf #{current_path}/public/pictures"
