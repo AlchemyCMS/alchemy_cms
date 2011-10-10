abb035de3e39b5e0e124aac3dd32bba1f32ca80e
Thomas von Deyen <tvd@magiclabs.de>
2011-10-10 20:25:07 +0200

    Added a helper for rendering the site name from standard set as page title

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -128,4 +128,12 @@ module PagesHelper
     end
   end
 
+  def sitename_from_header_page
+    header_page = Page.find_by_page_layout_and_layoutpage('layout_header', true)
+    return "" if header_page.nil?
+    page_title = header_page.elements.find_by_name('sitename')
+    return "" if page_title.nil?
+    page_title.ingredient('name')
+  end
+
 end
