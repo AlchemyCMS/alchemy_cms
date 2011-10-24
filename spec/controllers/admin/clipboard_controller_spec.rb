cef46a63803b1067f56ed28bedf7b053eba16daa
Thomas von Deyen <tvd@magiclabs.de>
2011-10-24 20:17:28 +0200

    Specs for Clipboard holding elements

--- spec/controllers/admin/clipboard_controller_spec.rb
+++ spec/controllers/admin/clipboard_controller_spec.rb
@@ -4,18 +4,44 @@ describe Admin::ClipboardController do
 
 	before(:each) do
 		activate_authlogic
-		UserSession.create FactoryGirl.create(:admin_user)
+		UserSession.create Factory(:admin_user)
 	end
 
   context "clipboard" do
 
-    it "should not insert the same element twice" do
-			pending "We have to spec our models before we write integration tests"
-			@page = FactoryGirl.create(:page, :parent_id => Page.rootpage.id)
-		  @element = FactoryGirl.create(:element, :page => @page)
-			post(:insert, {:remarkable_type => 'element', :remarkable_id => element.id, :format => :js})
-			post(:insert, {:remarkable_type => 'element', :remarkable_id => element.id, :format => :js})
-			session[:clipboard][:elements].should == [element.id]
+    it "should hold element ids" do
+			@page = Factory(:page, :parent_id => Page.rootpage.id)
+		  @element = Factory(:element, :page => @page)
+		  @another_element = Factory(:element, :page => @page)
+			session[:clipboard] = { :elements => [@element.id] }
+			post(:insert, {:remarkable_type => 'element', :remarkable_id => @another_element.id, :format => :js})
+			session[:clipboard][:elements].should == [@element.id, @another_element.id]
+    end
+
+    it "should not have the same element twice" do
+			@page = Factory(:page, :parent_id => Page.rootpage.id)
+		  @element = Factory(:element, :page => @page)
+			session[:clipboard] = { :elements => [@element.id] }
+			post(:insert, {:remarkable_type => 'element', :remarkable_id => @element.id, :format => :js})
+			session[:clipboard][:elements].should == [@element.id]
+    end
+
+    it "should remove element ids" do
+			@page = Factory(:page, :parent_id => Page.rootpage.id)
+		  @element = Factory(:element, :page => @page)
+		  @another_element = Factory(:element, :page => @page)
+			session[:clipboard] = { :elements => [@element.id, @another_element.id] }
+			delete(:remove, {:remarkable_type => 'element', :remarkable_id => @another_element.id, :format => :js})
+			session[:clipboard][:elements].should == [@element.id]
+    end
+
+    it "should be clearable" do
+			@page = Factory(:page, :parent_id => Page.rootpage.id)
+		  @element = Factory(:element, :page => @page)
+		  @another_element = Factory(:element, :page => @page)
+			session[:clipboard] = { :elements => [@element.id, @another_element.id] }
+			delete(:clear, :format => :js)
+			session[:clipboard].should == {}
     end
 
   end
