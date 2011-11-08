3f782cb4a955caafab6f7777428d2e0d11870273
Thomas von Deyen <tvd@magiclabs.de>
2011-11-08 16:49:34 +0100

    Fixes pending spec

--- spec/controllers/admin/trash_controller_spec.rb
+++ spec/controllers/admin/trash_controller_spec.rb
@@ -2,19 +2,16 @@ require 'spec_helper'
 
 describe Admin::TrashController do
 
+	render_views
+
 	before(:each) do
 		activate_authlogic
 		UserSession.create Factory(:admin_user)
 	end
 
-	it "should hold trashed elements" do
-		pending "The controller behaves correct, the test not."
+	it "should hold trashed elements", :focus => true do
 		@page = Factory(:page, :parent_id => Page.rootpage.id)
-	  @element = Factory(:element, :page => @page)
-		# Rails, RSpec and co. are sucking
-		@element.reload
-		@element.trash
-		@element.reload
+		@element = Factory(:element, :page => nil, :public => false, :position => 0, :folded => true)
 		get :index, :page_id => @page.id
 		response.body.should have_selector('#trash_items #element_4.element_editor')
 	end
