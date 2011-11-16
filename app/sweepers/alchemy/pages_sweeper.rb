d0ebed3c1fdc64ea8b8f344c119f51e69fde098d
Thomas von Deyen <tvd@magiclabs.de>
2011-11-16 23:46:27 +0100

    Namespaced sweepers

--- app/sweepers/alchemy/pages_sweeper.rb
+++ app/sweepers/alchemy/pages_sweeper.rb
@@ -1,47 +1,49 @@
-class Alchemy::PagesSweeper < ActionController::Caching::Sweeper
+module Alchemy
+	class PagesSweeper < ActionController::Caching::Sweeper
 
-  observe Alchemy::Page
+		observe Page
 
-  def after_update(page)
-    unless page.layoutpage?
-      expire_page(page)
-      check_multipage_elements(page)
-    end
-  end
+		def after_update(page)
+			unless page.layoutpage?
+				expire_page(page)
+				check_multipage_elements(page)
+			end
+		end
 
-  def after_destroy(page)
-    unless page.layoutpage?
-      expire_page(page)
-      check_multipage_elements(page)
-    end
-  end
+		def after_destroy(page)
+			unless page.layoutpage?
+				expire_page(page)
+				check_multipage_elements(page)
+			end
+		end
 
-private
+	private
 
-  def check_multipage_elements(page)
-    page.elements.each do |element|
-      # are their pages beneath mine?
-      if !element.to_be_sweeped_pages.detect{ |p| p != page }.nil?
-        # yepp! there are more pages then mine
-        pages = element.to_be_sweeped_pages.find_all_by_public_and_locked(true, false)
-        if !pages.blank?
-          # expire current page, even if it's locked
-          pages.push(page).each do |page|
-            expire_page(page)
-          end
-        end
-      end
-    end
-  end
+		def check_multipage_elements(page)
+			page.elements.each do |element|
+				# are their pages beneath mine?
+				if !element.to_be_sweeped_pages.detect{ |p| p != page }.nil?
+					# yepp! there are more pages then mine
+					pages = element.to_be_sweeped_pages.find_all_by_public_and_locked(true, false)
+					if !pages.blank?
+						# expire current page, even if it's locked
+						pages.push(page).each do |page|
+							expire_page(page)
+						end
+					end
+				end
+			end
+		end
 
-  def expire_page(page)
-    return if page.do_not_sweep
-    expire_action(
-      :controller => '/alchemy/pages',
-      :action => :show,
-      :urlname => page.urlname_was,
-      :lang => multi_language? ? page.language_code : nil
-    )
-  end
+		def expire_page(page)
+			return if page.do_not_sweep
+			expire_action(
+				:controller => '/alchemy/pages',
+				:action => :show,
+				:urlname => page.urlname_was,
+				:lang => multi_language? ? page.language_code : nil
+			)
+		end
 
+	end
 end
--- app/sweepers/alchemy/pictures_sweeper.rb
+++ app/sweepers/alchemy/pictures_sweeper.rb
@@ -1,21 +1,23 @@
-class Alchemy::PicturesSweeper < ActionController::Caching::Sweeper
-  observe Alchemy::Picture
+module Alchemy
+	class PicturesSweeper < ActionController::Caching::Sweeper
+		observe Alchemy::Picture
 
-  def after_update(picture)
-    expire_cache_for(picture)
-  end
+		def after_update(picture)
+			expire_cache_for(picture)
+		end
 
-  def after_destroy(picture)
-    expire_cache_for(picture)
-  end
+		def after_destroy(picture)
+			expire_cache_for(picture)
+		end
 
-private
+	private
 
-  def expire_cache_for(picture)
-    # Removing all variants of the picture with FileUtils.
-    FileUtils.rm_rf("#{Rails.root}/public/pictures/show/#{picture.id}")
-    FileUtils.rm_rf("#{Rails.root}/public/pictures/thumbnails/#{picture.id}")
-    expire_page(zoom_picture_path(picture, :format => 'png'))
-  end
+		def expire_cache_for(picture)
+			# Removing all variants of the picture with FileUtils.
+			FileUtils.rm_rf("#{Rails.root}/public/pictures/show/#{picture.id}")
+			FileUtils.rm_rf("#{Rails.root}/public/pictures/thumbnails/#{picture.id}")
+			expire_page(zoom_picture_path(picture, :format => 'png'))
+		end
 
+	end
 end
