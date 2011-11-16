895b12456decb7f32950a5815082917741532d67
Thomas von Deyen <tvd@magiclabs.de>
2011-11-16 23:46:27 +0100

    Namespaced sweepers

--- app/sweepers/alchemy/content_sweeper.rb
+++ app/sweepers/alchemy/content_sweeper.rb
@@ -1,20 +1,22 @@
-class Alchemy::ContentSweeper < ActionController::Caching::Sweeper
-  observe Alchemy::Element
-  
-  def after_update(element)
-    expire_cache_for(element.contents)
-  end
-  
-  def after_destroy(element)
-    expire_cache_for(element.contents)
-  end
-  
-private
-  
-  def expire_cache_for(contents)
-    contents.each do |content|
-      expire_fragment(content)
-    end
-  end
-  
+module Alchemy
+	class ContentSweeper < ActionController::Caching::Sweeper
+		observe Element
+
+		def after_update(element)
+			expire_cache_for(element.contents)
+		end
+
+		def after_destroy(element)
+			expire_cache_for(element.contents)
+		end
+
+	private
+
+		def expire_cache_for(contents)
+			contents.each do |content|
+				expire_fragment(content)
+			end
+		end
+
+	end
 end
