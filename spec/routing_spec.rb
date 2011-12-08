1807e01e92b208d7061b52d71f8d37a8e2be2540
Thomas von Deyen <tvd@magiclabs.de>
2011-12-08 22:24:39 +0100

    Finally getting the routing specs to work!!

--- spec/routing_spec.rb
+++ spec/routing_spec.rb
@@ -2,56 +2,54 @@ require 'spec_helper'
 
 describe "The Routing", :type => :routing do
 
+	before(:each) { @routes = Alchemy::Engine.routes }
+
 	context "for downloads" do
-	
+
 		it "should have a named route" do
-			pending "Because Rspec does not support namespaced engines yet!"
-		  {
-				:get => "/alchemy/attachment/32/download/Presseveranstaltung.pdf"
+			{
+				:get => "/attachment/32/download/Presseveranstaltung.pdf"
 			}.should route_to(
-	    	:controller => "alchemy/attachments",
-	    	:action => "download",
-	    	:id => "32",
+				:controller => "alchemy/attachments",
+				:action => "download",
+				:id => "32",
 				:name => "Presseveranstaltung",
 				:suffix => "pdf"
 			)
 		end
-		
+
 		it "should have a route for legacy Alchemy 1.x downloads" do
-			pending "Because Rspec does not support namespaced engines yet!"
-		  {
-				:get => "/alchemy/attachment/32/download?name=Presseveranstaltung.pdf"
+			{
+				:get => "/attachment/32/download?name=Presseveranstaltung.pdf"
 			}.should route_to(
-	    	:controller => "alchemy/attachments",
-	    	:action => "download",
-	    	:id => "32"
+				:controller => "alchemy/attachments",
+				:action => "download",
+				:id => "32"
 			)
 		end
-		
-	  it "should have a route for legacy washAPP downloads" do
-			pending "Because Rspec does not support namespaced engines yet!"
+
+		it "should have a route for legacy washAPP downloads" do
 			{
-				:get => "/alchemy/wa_files/download/11"
+				:get => "/wa_files/download/11"
 			}.should route_to(
-		    :controller => "alchemy/attachments",
-		    :action => "download",
-		    :id => "11"
-		  )
+				:controller => "alchemy/attachments",
+				:action => "download",
+				:id => "11"
+			)
 		end
 
 		it "should have a route for legacy WebMate downloads" do
-			pending "Because Rspec does not support namespaced engines yet!"
-		  {
-				:get => "/alchemy/uploads/files/0000/0028/Pressetext.pdf"
+			{
+				:get => "/uploads/files/0000/0028/Pressetext.pdf"
 			}.should route_to(
-	    	:controller => "alchemy/attachments",
-	    	:action => "download",
-	    	:id => "0028",
+				:controller => "alchemy/attachments",
+				:action => "download",
+				:id => "0028",
 				:name => "Pressetext",
 				:suffix => "pdf"
 			)
 		end
-		
+
 	end
 
 end
