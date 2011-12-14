e90b39cf14cf9deddbfb104e8ffc98c2e4bc9cc4
Robin Böning <robin@magiclabs.de>
2011-12-14 23:22:43 +0100

    Updating installer for Alchemy version 2.1

--- lib/alchemy_cms.rb
+++ lib/alchemy_cms.rb
@@ -22,7 +22,7 @@ if defined?(Rails) && Rails::VERSION::MAJOR == 3
 	end
 	require File.join(File.dirname(__FILE__), "alchemy", "seeder")
 else
-	raise "Alchemy 2.0 needs Rails 3.0 or higher. You are currently using Rails #{Rails::VERSION::MAJOR}"
+	raise "Alchemy 2.1 needs Rails 3.1 or higher. You are currently using Rails #{Rails::VERSION::MAJOR}"
 end
 
 module Alchemy
