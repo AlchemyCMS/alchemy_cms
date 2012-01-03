7717704239e44fcb892aafe801eb6db8df7ecd79
Thomas von Deyen <tvd@magiclabs.de>
2012-01-03 18:15:16 +0100

    Checking for HTML5 upload capability

---
+++ app/assets/javascripts/alchemy/alchemy.html5.js
@@ -0,0 +1,22 @@
+// Testing for HTML5 features
+
+if (typeof(Alchemy) === 'undefined') {
+	var Alchemy = {};
+}
+
+Alchemy.HTML5 = {};
+
+Alchemy.HTML5.hasUploadSupport = function() {
+	return typeof(window.FileReader) !== 'undefined' && supportFileAPI() && supportAjaxUploadProgressEvents();
+	
+	function supportFileAPI() {
+		var fi = document.createElement('INPUT');
+		fi.type = 'file';
+		return 'files' in fi;
+	};
+	
+	function supportAjaxUploadProgressEvents() {
+		var xhr = new XMLHttpRequest();
+		return !! (xhr && ('upload' in xhr) && ('onprogress' in xhr.upload));
+	};
+}
