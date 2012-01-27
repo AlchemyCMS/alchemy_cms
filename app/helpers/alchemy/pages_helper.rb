791d6128eee867d9b11441d07f31265dd7faedc5
Thomas von Deyen <tvd@magiclabs.de>
2012-01-27 11:06:20 +0100

    Removing old deprecated form helpers

--- app/helpers/alchemy/pages_helper.rb
+++ app/helpers/alchemy/pages_helper.rb
@@ -14,61 +14,6 @@ module Alchemy
 			content.essence.caption
 		end
 
-		def alchemy_form_select(name, select_options, options={})
-			select "mail_data", name, select_options, :selected => (session[:mail_data][name.to_sym] rescue "")
-		end
-
-		def alchemy_form_input_field(name, options = {})
-			if options[:value].blank? && session[:mail_data].blank?
-				value = nil
-			elsif options[:value].blank? && !session[:mail_data].blank?
-				value = session[:mail_data][name.to_sym]
-			else
-				value = options[:value]
-			end
-			text_field("mail_data", name, {:value => value}.merge(options))
-		end
-
-		def alchemy_form_text_area(name, options={})
-			text_area "mail_data", name, :class => options[:class], :value => (session[:mail_data][name.to_sym] rescue "")
-		end
-
-		def alchemy_form_check_box(name, options={})
-			box = hidden_field_tag "mail_data[#{name}]", 0, :id => nil
-			box += check_box_tag("mail_data[#{name}]", 1, (session[:mail_data] && session[:mail_data][name.to_sym] == "1"))
-			box
-		end
-
-		def alchemy_form_label(element, name, options={})
-			label_tag "mail_data_#{name}", render_essence_view_by_name(element, name), options
-		end
-
-		def alchemy_form_reset_button(name, options={})
-			button_to_function(
-				name,
-				remote_function(
-					:url => {
-						:controller => "contact_form",
-						:action => "clear_session"
-					},
-					:before => %(
-						this.form.reset();
-						this.form.descendants().each(
-							function(d){
-								if ((d.type!='button') && (d.type!='submit') && (d.type!='hidden') && !d.disabled) {
-									d.value = '';
-									if (d.type == 'checkbox') {
-										d.checked = false;
-									}
-								}
-							}
-						)
-					)
-				),
-				options
-			)
-		end
-
 		# == Helper for rendering language switches
 		# 
 		# Renders links to all public language root pages
