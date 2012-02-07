e8d0962b50a411d7456ed08515d2d69b6ece9389
Thomas von Deyen <tvd@magiclabs.de>
2012-02-07 12:18:37 +0100

    Fixes authorization check of elements

--- app/controllers/alchemy/elements_controller.rb
+++ app/controllers/alchemy/elements_controller.rb
@@ -1,17 +1,22 @@
 module Alchemy
 	class ElementsController < Alchemy::BaseController
-  
-		filter_access_to [:show], :attribute_check => true
+
+		filter_access_to :show, :attribute_check => true, :model => Alchemy::Element, :load_method => :load_element
 		layout false
 
 		# Returns the element partial as HTML or as JavaScript that tries to replace a given +container_id+ with the partial content via jQuery.
 		def show
-			@element = Element.find(params[:id])
 			@page = @element.page
 			respond_to do |format|
 				format.html
 				format.js
 			end
+		end
+
+	private
+
+		def load_element
+			@element = Element.find(params[:id])
 		end
 
 	end
