ea69d78b433c365138a4b3923d3dbad384476b7e
Thomas von Deyen <tvd@magiclabs.de>
2012-02-19 15:43:43 +0100

    Fixes content validation error messages. Somehow

--- app/helpers/alchemy/admin/essences_helper.rb
+++ app/helpers/alchemy/admin/essences_helper.rb
@@ -82,6 +82,7 @@ module Alchemy
 			# === Options:
 			# 
 			#   :only            [Hash]     # Pagelayout names. Only pages with this page_layout will be displayed inside the select.
+			#   :except          [Hash]     # Pagelayout names. No pages with this page_layout will be displayed inside the select.
 			#   :page_attribute  [Symbol]   # The Page attribute which will be stored.
 			#   :global          [Boolean]  # Display only global pages. Default is false.
 			# 
