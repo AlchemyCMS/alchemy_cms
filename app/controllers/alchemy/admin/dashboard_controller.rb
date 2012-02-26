57ec5b323e0f41e4d0168a17f8a714157f462c07
Thomas von Deyen <tvd@magiclabs.de>
2012-02-26 22:17:06 +0100

    Correcting greeting for first login.

--- app/controllers/alchemy/admin/dashboard_controller.rb
+++ app/controllers/alchemy/admin/dashboard_controller.rb
@@ -8,6 +8,7 @@ module Alchemy
 				@last_edited_pages = Page.all_last_edited_from(current_user)
 				@locked_pages = Page.all_locked
 				@online_users = User.all_online.to_a.delete_if { |u| u == current_user }
+				@first_time = current_user.login_count == 1 && current_user.last_login_at.nil?
 			end
 
 		end
