e5b6124f26eeb09583eff10891a52400399aa3f6
Thomas von Deyen <tvd@magiclabs.de>
2012-03-15 13:22:38 +0100

    Refactoring admin/contents#create. Fixes #206

--- app/controllers/alchemy/admin/contents_controller.rb
+++ app/controllers/alchemy/admin/contents_controller.rb
@@ -12,20 +12,27 @@ module Alchemy
 			def create
 				@element = Element.find(params[:content][:element_id])
 				@content = Content.create_from_scratch(@element, params[:content])
-				@options = params[:options]
+				@options = params[:options] || {}
+				@html_options = params[:html_options] || {}
 				# If options params come from Flash uploader then we have to parse them as hash.
 				if @options.is_a?(String)
 					@options = Rack::Utils.parse_query(@options)
 				end
 				if @content.essence_type == "Alchemy::EssencePicture"
+					@element_dom_id = "#add_content_#{@element.id}"
 					@content.essence.picture_id = params[:picture_id]
 					@content.essence.save
 					@contents_of_this_type = @element.contents.find_all_by_essence_type('Alchemy::EssencePicture')
 					@dragable = @contents_of_this_type.length > 1
-					@options = @options.merge(
-						:dragable => @dragable
-					) if @options
+					@options = @options.merge(:dragable => @dragable)
+				else
+					@element_dom_id = "#add_content_for_element_#{@element.id}"
 				end
+				@locals = {
+				  :content => @content,
+				  :options => @options.symbolize_keys,
+				  :html_options => @html_options.symbolize_keys
+				}
 			end
 
 			def update
