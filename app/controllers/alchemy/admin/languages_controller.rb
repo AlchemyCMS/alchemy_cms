1f3597778fac06c77d8af0988ccda6e7e871e522
masche842 <marc@zilia.com>
2012-03-28 17:22:59 +0200

    use default_language.page_layout as default when creating a new language

--- app/controllers/alchemy/admin/languages_controller.rb
+++ app/controllers/alchemy/admin/languages_controller.rb
@@ -2,6 +2,13 @@ module Alchemy
 	module Admin
 		class LanguagesController < Alchemy::Admin::ResourcesController
 
+			def new
+				@language = Alchemy::Language.new
+				default_page_layout = Alchemy::Config.get(:default_language).try('[]', 'page_layout')
+				@language.page_layout = default_page_layout if default_page_layout.present?
+				render :layout => !request.xhr?
+			end
+
 		end
 	end
 end
