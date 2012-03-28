f9af2556118f126252c4c338c446b286b5c8e0d6
masche842 <marc@zilia.com>
2012-03-28 17:03:06 +0200

    generalize credential-invalid-message to improve security

--- app/models/alchemy/user_session.rb
+++ app/models/alchemy/user_session.rb
@@ -2,6 +2,8 @@ module Alchemy
 	class UserSession < Authlogic::Session::Base
 		logout_on_timeout(::Rails.env != 'development')
 
+		generalize_credentials_error_messages true
+
 		before_destroy :unlock_pages
 
 		def unlock_pages
