9d4685d745e4dfadda62bd2d9898960f24233d16
Thomas von Deyen <tvd@magiclabs.de>
2012-04-28 00:29:10 +0200

    Setting precision and scale on events.entry_fee table. Should fix builds against mysql on travis ci.

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -273,7 +273,7 @@ ActiveRecord::Schema.define(:version => 20120302040145) do
     t.datetime "starts_at"
     t.datetime "ends_at"
     t.text     "description"
-    t.decimal  "entrance_fee"
+    t.decimal  "entrance_fee", :precision => 6, :scale => 2
     t.boolean  "published"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
