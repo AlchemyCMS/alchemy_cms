8c40d01dbab6e01b286128ad4d9c5f7af47ff3bf
masche842 <schettke@gmail.com>
2012-05-04 16:00:32 +0200

    passing integration-specs for resources without muting access control (even with js)

--- spec/support/integration_spec_helper.rb
+++ spec/support/integration_spec_helper.rb
@@ -1,13 +1,12 @@
 require 'declarative_authorization/maintenance'
 include Authorization::TestHelper
 
-# Trying to load additional authorization-rules in dummy-app.
-# When accessing dummy-app in development-env it works without loading,
-# in capybara-test this doesn't work. Unfortunately the method below doesn't work neither.
-# I leave it here as a reminder
+# Load additional authorization_rules for specs.
+# For some strange reason, this isn't done automatically while running the specs
 def load_authorization_rules
-  Alchemy::AuthEngine.get_instance.load(File.join(File.dirname(__FILE__), '../..', 'config/authorization_rules.rb'))
-  Alchemy::AuthEngine.get_instance.load(File.join(File.dirname(__FILE__), '../dummy' 'config/authorization_rules.rb'))
+  instance = Alchemy::AuthEngine.get_instance
+  instance.load(File.join(File.dirname(__FILE__), '../dummy', 'config/authorization_rules.rb'))
+  instance.load(File.join(File.dirname(__FILE__), '../..', 'config/authorization_rules.rb'))
 end
 
 def create_admin_user
@@ -20,5 +19,3 @@ def login_with_admin_user
   fill_in('alchemy_user_session_password', :with => 's3cr3t')
   click_on('Login')
 end
-
-
