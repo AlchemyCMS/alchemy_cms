a77281d6c57f5add103bc2d4a61c224ba3f55310
Thomas von Deyen <tvd@magiclabs.de>
2012-05-29 17:38:57 +0200

    Skipping capybara-webkit integration specs on travis-ci.org

--- spec/integration/admin/modules_integration_spec.rb
+++ spec/integration/admin/modules_integration_spec.rb
@@ -1,31 +1,34 @@
-require 'spec_helper'
+# Skipping on Travis-CI, because capybara-webkit does not install on travis.
+unless ENV["CI"]
+  require 'spec_helper'
 
-describe "Modules" do
+  describe "Modules" do
 
-  context "A custom module with a main-apps controller" do
+    context "A custom module with a main-apps controller" do
 
-    before(:each) do
-      load_authorization_rules
-      create_admin_user
-      login_into_alchemy
-    end
+      before(:each) do
+        load_authorization_rules
+        create_admin_user
+        login_into_alchemy
+      end
 
-    it "should have a button in main_navigation, pointing to the configured controller" do
-      Alchemy::Modules.register_module(
-        {
-          :name => 'events',
-          :navigation => {
-            :icon => 'icon events',
-            :name => 'Events',
-            :controller => '/admin/events',
-            :action => 'index'
-          }
-        })
-      visit '/alchemy/admin'
-      click_on 'Events'
-      page.should_not have_content('Upps!')
+      it "should have a button in main_navigation, pointing to the configured controller" do
+        Alchemy::Modules.register_module(
+          {
+            :name => 'events',
+            :navigation => {
+              :icon => 'icon events',
+              :name => 'Events',
+              :controller => '/admin/events',
+              :action => 'index'
+            }
+          })
+        visit '/alchemy/admin'
+        click_on 'Events'
+        page.should_not have_content('Upps!')
+      end
+
     end
 
   end
-
 end
