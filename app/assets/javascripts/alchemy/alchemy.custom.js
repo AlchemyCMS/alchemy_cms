49db86a40b69b41c25949de133c4e6dbca5904a8
masche842 <schettke@gmail.com>
2012-06-25 14:05:47 +0200

    adapted to Alchemy's CI

--- app/assets/javascripts/alchemy/alchemy.custom.js
+++ app/assets/javascripts/alchemy/alchemy.custom.js
@@ -1,1 +1,1 @@
-// You can overwrite this file in your app or engine to customize javascript available in alchemy's backend.
+// You can overwrite this file in your app or engine to customize javascript available in Alchemy's backend.
