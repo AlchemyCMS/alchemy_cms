d56c4417f16d91496b5ddc951547655b6b0558d2
Thomas von Deyen <tvd@magiclabs.de>
2012-07-04 15:01:18 +0200

    Do not require localeapp/rails on TravisCI

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,5 +1,9 @@
-require 'localeapp/rails'
+unless ENV['CI']
 
-Localeapp.configure do |config|
-  config.api_key = 'afwgJCX6Xq40Y2gQSU9sdYFJ2XmQdAhgKmZPd69TOLdsT7e4Sy'
+  require 'localeapp/rails'
+
+  Localeapp.configure do |config|
+    config.api_key = 'afwgJCX6Xq40Y2gQSU9sdYFJ2XmQdAhgKmZPd69TOLdsT7e4Sy'
+  end
+
 end
