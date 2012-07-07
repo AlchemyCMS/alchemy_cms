ab3a0bb384991f0fe50e5501d0f46fae17798bfb
Thomas von Deyen <tvd@magiclabs.de>
2012-07-07 22:12:56 +0200

    Pass overlay value from observer to window constructor.
    Fixes bug where select boxes forces overlay to show scrollbars, instead of overflowing it.

--- app/assets/javascripts/alchemy/alchemy.windows.js
+++ app/assets/javascripts/alchemy/alchemy.windows.js
@@ -290,7 +290,7 @@ if (typeof(Alchemy) === 'undefined') {
         var $this = $(this);
         var options = $this.data('alchemy-overlay');
         event.preventDefault();
-        Alchemy.openWindow($this.attr('href'), options.title, options.size_x, options.size_y, options.resizable, options.overflow);
+        Alchemy.openWindow($this.attr('href'), options.title, options.size_x, options.size_y, options.resizable, options.modal, options.overflow);
         return false;
       });
 
