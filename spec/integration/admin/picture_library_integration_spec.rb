3e74b89fb3091347c5f9ab3a52835e2a8dc8cbf0
Thomas von Deyen <tvd@magiclabs.de>
2012-08-03 13:24:31 +0200

    Fixes picture specs.

--- spec/integration/admin/picture_library_integration_spec.rb
+++ spec/integration/admin/picture_library_integration_spec.rb
@@ -1,74 +1,44 @@
-unless ENV["CI"]
+require 'spec_helper'
 
-  require 'spec_helper'
-  require 'support/alchemy/specs_helpers'
+describe "Picture Library", :js => true do
 
-  describe "Picture Library", :type => :request do
-
-    before(:all) do
-      create_admin_user
-      load_authorization_rules
-      login_into_alchemy
-    end
-
-    describe "Upload" do
-
-      it "should be possible to tag pictures while uploading them" do
-        visit '/alchemy/admin/pictures'
-        click_on 'Upload image(s)'
-        fill_in :tag_list, :with => 'tag1, tag2'
-        attach_file 'Browse', File.expand_path('../../../support/image.png', __FILE__)
-        click_on 'upload'
-        Alchemy::Picture.find_by_image_filename('image.png').tag_list.should include('tag1')
-      end
-
-    end
-
-    describe "Tagging" do
-
-      it "is possible to edit tags after clicking on the picture", :js => true do
-        picture = FactoryGirl.create(:picture, :tag_list => 'tag1')
-        visit '/alchemy/admin/pictures'
-save_and_open_page
-        within("#picture_#{picture.id}") do
-          click_link 'show_in_window'
-        end
-save_and_open_page
-
-        # Doesn't work, so:
-        #visit "/alchemy/admin/pictures/#{picture.id}/show_in_window"
-
-        fill_in :tag_list, :with => 'tag3, tag4'
-        click_on 'Update Picture'
-        page.should have_content 'tag3'
-      end
-
-      it "is possible to filter tags by clicking on its name in the tag list" do
-        picture = FactoryGirl.create(:picture, :tag_list => 'tag1', :name => 'TaggedWith1')
-        picture = FactoryGirl.create(:picture, :tag_list => 'tag2', :name => 'TaggedWith2')
-        visit '/alchemy/admin/pictures'
-        click_on 'tag1'
-        page.should have_content 'TaggedWith1'
-        page.should_not have_content 'TaggedWith2'
-      end
-    end
-
-
-    describe "Filter by tag" do
-      before do
-        FactoryGirl.create(:picture, :tag_list => 'bla')
-      end
-      it "should list all applied tags" do
-        visit '/alchemy/admin/pictures'
-        page.should have_content 'bla'
-      end
-
-      it "should be possible to filter pictures by tag" do
-        visit '/alchemy/admin/pictures'
-        click_on 'tag1'
-        page.should have_content 'bla'
-      end
-    end
+  before(:all) do
+    create_admin_user
   end
 
+  before(:each) do
+    login_into_alchemy
+  end
+
+  describe "Tagging" do
+
+    it "it should be possible to filter tags by clicking on its name in the tag list" do
+      picture = FactoryGirl.create(:picture, :tag_list => 'tag1', :name => 'TaggedWith1')
+      picture = FactoryGirl.create(:picture, :tag_list => 'tag2', :name => 'TaggedWith2')
+      visit '/alchemy/admin/pictures'
+      click_on 'tag1 (1)'
+      page.should have_content 'TaggedWith1'
+      page.should_not have_content 'TaggedWith2'
+    end
+
+  end
+
+  describe "Filter by tag" do
+
+    before do
+      FactoryGirl.create(:picture, :tag_list => 'bla')
+    end
+
+    it "should list all applied tags" do
+      visit '/alchemy/admin/pictures'
+      page.should have_content 'bla'
+    end
+
+    it "should be possible to filter pictures by tag" do
+      visit '/alchemy/admin/pictures'
+      click_on 'bla (1)'
+      page.should have_content 'bla'
+    end
+
+  end
 end
--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -1,97 +1,101 @@
 require 'spec_helper'
 
-describe Alchemy::Picture do
+module Alchemy
+  describe Picture do
 
-  it "is valid with valid attributes" do
-    picture = Alchemy::Picture.new(:image_file => File.new(File.expand_path('../support/image.png', __FILE__)))
-    picture.should be_valid
-  end
-
-  describe '#suffix' do
-
-    it "should return the suffix of original filename" do
-      pic = stub_model(Alchemy::Picture, :image_filename => 'kitten.JPG')
-      pic.suffix.should == "jpg"
+    let :image_file do
+      File.new(File.expand_path('../../support/image.png', __FILE__))
     end
 
-    context "image has no suffix" do
+    it "is valid with valid attributes" do
+      picture = Picture.new(:image_file => image_file)
+      picture.should be_valid
+    end
 
-      before(:each) do
-        @pic = stub_model(Alchemy::Picture, :image_filename => 'kitten')
+    describe '#suffix' do
+
+      it "should return the suffix of original filename" do
+        pic = stub_model(Picture, :image_filename => 'kitten.JPG')
+        pic.suffix.should == "jpg"
       end
 
-      it "should return empty string" do
-        @pic.suffix.should == ""
-      end
+      context "image has no suffix" do
 
-    end
+        before(:each) do
+          @pic = stub_model(Picture, :image_filename => 'kitten')
+        end
 
-  end
+        it "should return empty string" do
+          @pic.suffix.should == ""
+        end
 
-  describe '#humanized_name' do
-
-    it "should return a humanized version of original filename" do
-      pic = stub_model(Alchemy::Picture, :image_filename => 'cute_kitten.JPG')
-      pic.humanized_name.should == "Cute kitten"
-    end
-
-    it "should not remove incidents of suffix from filename" do
-      pic = stub_model(Alchemy::Picture, :image_filename => 'cute_kitten_mo.jpgi.JPG')
-      pic.humanized_name.should == "Cute kitten mo.jpgi"
-      pic.humanized_name.should_not == "Cute kitten moi"
-    end
-
-    context "image has no suffix" do
-
-      before(:each) do
-        @pic = stub_model(Alchemy::Picture, :image_filename => 'cute_kitten')
-        @pic.stub!(:suffix).and_return("")
-      end
-
-      it "should return humanized name" do
-        @pic.humanized_name.should == "Cute kitten"
       end
 
     end
 
-  end
+    describe '#humanized_name' do
 
-  describe '#self.last_upload' do
+      it "should return a humanized version of original filename" do
+        pic = stub_model(Picture, :image_filename => 'cute_kitten.JPG')
+        pic.humanized_name.should == "Cute kitten"
+      end
 
-    it "should return all pictures that have the same upload-hash as the most recent picture" do
-      other_upload = Alchemy::Picture.create!(:image_file => File.open(File.expand_path('../support/image.png', __FILE__)), :upload_hash => '456')
-      same_upload = Alchemy::Picture.create!(:image_file => File.open(File.expand_path('../support/image.png', __FILE__)), :upload_hash => '123')
-      most_recent = Alchemy::Picture.create!(:image_file => File.open(File.expand_path('../support/image.png', __FILE__)), :upload_hash => '123')
+      it "should not remove incidents of suffix from filename" do
+        pic = stub_model(Picture, :image_filename => 'cute_kitten_mo.jpgi.JPG')
+        pic.humanized_name.should == "Cute kitten mo.jpgi"
+        pic.humanized_name.should_not == "Cute kitten moi"
+      end
 
-      Alchemy::Picture.last_upload.should include(most_recent)
-      Alchemy::Picture.last_upload.should include(same_upload)
-      Alchemy::Picture.last_upload.should_not include(other_upload)
+      context "image has no suffix" do
 
-      [other_upload, same_upload, most_recent].each { |p| p.destroy }
+        before(:each) do
+          @pic = stub_model(Picture, :image_filename => 'cute_kitten')
+          @pic.stub!(:suffix).and_return("")
+        end
+
+        it "should return humanized name" do
+          @pic.humanized_name.should == "Cute kitten"
+        end
+
+      end
+
+    end
+
+    describe '#self.last_upload' do
+
+      it "should return all pictures that have the same upload-hash as the most recent picture" do
+        other_upload = Picture.create!(:image_file => image_file, :upload_hash => '456')
+        same_upload = Picture.create!(:image_file => image_file, :upload_hash => '123')
+        most_recent = Picture.create!(:image_file => image_file, :upload_hash => '123')
+
+        Picture.last_upload.should include(most_recent)
+        Picture.last_upload.should include(same_upload)
+        Picture.last_upload.should_not include(other_upload)
+
+        [other_upload, same_upload, most_recent].each { |p| p.destroy }
+      end
+
+    end
+
+    describe '#self.recent' do
+
+      before(:all) do
+        now = Time.now
+        @recent = Picture.create!(:image_file => image_file)
+        @old_picture = Picture.create!(:image_file => image_file)
+        @recent.update_column(:created_at, now-23.hours)
+        @old_picture.update_column(:created_at, now-10.days)
+      end
+
+      it "should return all pictures that have been created in the last 24 hours" do
+        Picture.recent.should include(@recent)
+      end
+
+      it "should not return old pictures" do
+        Picture.recent.should_not include(@old_picture)
+      end
+
     end
 
   end
-
-  describe '#self.recent' do
-
-    before(:all) do
-      now = Time.now
-      @recent = Alchemy::Picture.create!(:image_file => File.open(File.expand_path('../support/image.png', __FILE__)))
-      @old_picture = Alchemy::Picture.create!(:image_file => File.open(File.expand_path('../support/image.png', __FILE__)))
-      @recent.update_attribute(:created_at, now-23.hours)
-      @old_picture.update_attribute(:created_at, now-10.days)
-    end
-
-    after(:all) { [recent, now].each { |p| p.destroy } }
-
-    it "should return all pictures that have been created in the last 24 hours" do
-      Alchemy::Picture.recent.should include(@recent)
-    end
-
-    it "should not return old pictures" do
-      Alchemy::Picture.recent.should_not include(@old_picture)
-    end
-
-  end
-
 end
