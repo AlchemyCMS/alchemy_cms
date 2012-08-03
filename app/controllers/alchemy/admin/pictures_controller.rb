412ec18827ebb3b3202815abe5896dc312c779e2
Thomas von Deyen <tvd@magiclabs.de>
2012-08-03 18:44:22 +0200

    Better picture upload params.

--- app/controllers/alchemy/admin/pictures_controller.rb
+++ app/controllers/alchemy/admin/pictures_controller.rb
@@ -41,7 +41,10 @@ module Alchemy
       end
 
       def create
-        @picture = Picture.new(:image_file => params[:Filedata], :upload_hash => params[:hash], :tag_list => params[:picture].try(:[],:tag_list))
+        @picture = Picture.new(
+          :image_file => params[:Filedata],
+          :upload_hash => params[:upload_hash]
+        )
         @picture.name = @picture.humanized_name
         @picture.save
         @size = params[:size] || 'medium'
