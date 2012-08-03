87687706555a39155b2bfb5c715104cfd646004d
Thomas von Deyen <tvd@magiclabs.de>
2012-08-03 22:20:48 +0200

    Test tweaking

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,8 @@ def configure
   ActionMailer::Base.default_url_options[:host] = "test.com"
 
   Rails.backtrace_cleaner.remove_silencers!
+  # Disable rails loggin for faster IO. Remove this if you want to have a test.log
+  Rails.logger.level = 4
 
   # Configure capybara for integration testing
   require "capybara/rails"
@@ -29,6 +31,8 @@ def configure
   Capybara.default_driver = :rack_test
   Capybara.default_selector = :css
   Capybara.javascript_driver = :poltergeist
+  # Rainsing the default wait time for capybara requests
+  Capybara.default_wait_time = 5
 
   # Load support files
   Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
@@ -39,6 +43,7 @@ def configure
     config.include Alchemy::Engine.routes.url_helpers
     config.mock_with :rspec
     config.use_transactional_fixtures = true
+    # Make sure the database is clean and ready for test
     config.before(:suite) do
       truncate_all_tables
       Alchemy::Seeder.seed!
@@ -51,40 +56,4 @@ if defined?(Spork)
   Spork.prefork { configure }
 else
   configure
-end
-
-# Fixes Capybara database connection issues
-# Found http://blog.plataformatec.com.br/2011/12/three-tips-to-improve-the-performance-of-your-test-suite/
-
-class ActiveRecord::Base
-  mattr_accessor :shared_connection
-  @@shared_connection = nil
-
-  def self.connection
-    @@shared_connection || retrieve_connection
-  end
-end
-
-# Forces all threads to share the same connection. This works on
-# Capybara because it starts the web server in a thread.
-ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
-
-# fast truncation of all tables that need truncations (select is 10x faster then truncate)
-# http://grosser.it/2012/07/03/rubyactiverecord-fastest-way-to-truncate-test-database/
-def truncate_all_tables
-  config = ActiveRecord::Base.configurations[::Rails.env]
-  connection = ActiveRecord::Base.connection
-  connection.disable_referential_integrity do
-    connection.tables.each do |table_name|
-      next if connection.select_value("SELECT count(*) FROM #{table_name}") == 0
-      case config["adapter"]
-        when "mysql2", "postgresql"
-          connection.execute("TRUNCATE #{table_name}")
-        when "sqlite3"
-          connection.execute("DELETE FROM #{table_name}")
-          connection.execute("DELETE FROM sqlite_sequence where name='#{table_name}'")
-      end
-    end
-    connection.execute("VACUUM") if config["adapter"] == "sqlite3"
-  end
 end
