ae3e9a84081f60e1b159e3ac7cdeb3ead3599f3b
Thomas von Deyen <tvd@magiclabs.de>
2012-08-16 15:12:41 +0200

    Best button disable solution so far.

--- app/assets/javascripts/alchemy/alchemy.buttons.js
+++ app/assets/javascripts/alchemy/alchemy.buttons.js
@@ -15,16 +15,18 @@ if (typeof(Alchemy) === 'undefined') {
     disableButton: function(button) {
       var $button = $(button),
         $clone = $button.clone(),
-        width = $button.outerWidth(),
-        text = $button.text();
+        width = Alchemy.getButtonWidth($button),
+        height = Alchemy.getButtonHeight($button),
+        spinner = '<img src="/assets/alchemy/ajax_loader.gif" style="width: 16px; height: 16px">';
       $button.hide().addClass('disabled');
       $button.parent().append($clone);
       $clone.attr({
         disabled: true,
         href: 'javascript:void(0)'
       }).addClass('cloned-button').css({
-        width: width
-      }).html('<img src="/assets/alchemy/ajax_loader.gif" style="width: 16px; height: 16px">').show();
+        width: width,
+        'line-height': height + 'px'
+      }).html(spinner).show();
       return true;
     },
 
@@ -33,6 +35,14 @@ if (typeof(Alchemy) === 'undefined') {
       $button.show().removeClass('disabled');
       $button.parent().find('.cloned-button').remove();
       return true;
+    },
+
+    getButtonWidth: function($button) {
+      return $button.outerWidth() - (parseInt($button.css('border-left-width'), 10) + parseInt($button.css('border-right-width'), 10));
+    },
+
+    getButtonHeight: function($button) {
+      return $button.outerHeight() - (parseInt($button.css('border-top-width'), 10) + parseInt($button.css('border-bottom-width'), 10));
     }
 
   });
