321d6791729504eba8ead5fec15e8d9cfde0ad25
Thomas von Deyen <tvdeyen@gmail.com>
2012-10-26 10:47:58 +0200

    Fixes spec dummy app migrations.
    Moved the events table creation from the dummy app into its own migration file and made a symlink for alchemy migration.

---
+++ spec/dummy/db/migrate/20121026104128_create_events.rb
@@ -0,0 +1,19 @@
+class CreateEvents < ActiveRecord::Migration
+
+  def change
+    create_table "events" do |t|
+      t.string   "name"
+      t.string   "hidden_name"
+      t.datetime "starts_at"
+      t.datetime "ends_at"
+      t.text     "description"
+      t.decimal  "entrance_fee", :precision => 6, :scale => 2
+      t.boolean  "published"
+      t.integer  "location_id"
+      t.integer  "organizer_id"
+      t.datetime "created_at",                                 :null => false
+      t.datetime "updated_at",                                 :null => false
+    end
+  end
+
+end
