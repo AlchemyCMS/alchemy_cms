897849dfbee32c2a6887e49f7b64946eedc499c3
Thomas von Deyen <tvdeyen@gmail.com>
2012-10-30 18:26:26 +0100

    Fixes admin users controller spec

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -5,26 +5,30 @@ describe Alchemy::Admin::UsersController do
   describe "POST update" do
 
     let!(:user) { FactoryGirl.create(:admin_user) }
-    before(:each) { Alchemy::UserSession.create user }
+
+    before do
+      activate_authlogic
+      Alchemy::UserSession.create user
+    end
 
     it "assigns user to @user" do
-      post :update, :id => user.id
+      post :update, :id => user.id, :user => {}
       assigns(:user).should eq(user)
     end
 
     context "if user is permitted to update roles" do
       it "updates the user including role" do
-        controller.stub(:permitted_to?).with(:update_role).and_return { false }
-        user.should_receive :update_attributes, :with => {:role => 'Administrator'}
-        post :update, :id => user.id, :role => 'Administrator'
+        controller.stub(:permitted_to?).with(:update_role).and_return { true }
+        Alchemy::User.any_instance.should_receive(:update_attributes).with({'role' => 'Administrator'})
+        post :update, :id => user.id, :user => {:role => 'Administrator'}, :format => :js
       end
     end
 
     context "if the user is not permitted to update roles" do
       it "updates user without role" do
         controller.stub(:permitted_to?).with(:update_role).and_return { false }
-        user.should_receive :update_attributes, :with => {}
-        post :update, :id => user.id, :role => 'Administrator'
+        Alchemy::User.any_instance.should_receive(:update_attributes).with({})
+        post :update, :id => user.id, :user => {'role' => 'Administrator'}, :format => :js
       end
     end
 
