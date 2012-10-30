81be6392e9a06cc885af1f15a27a6a85fd7379a5
Thomas von Deyen <tvdeyen@gmail.com>
2012-10-30 14:41:17 +0100

    Moved the picture request security from middleware into controller.

--- app/controllers/alchemy/pictures_controller.rb
+++ app/controllers/alchemy/pictures_controller.rb
@@ -3,7 +3,7 @@ module Alchemy
 
     caches_page :show, :thumbnail, :zoom
 
-    before_filter :load_picture
+    before_filter :load_picture, :ensure_secure_params
 
     filter_access_to :show, :attribute_check => true, :model => Alchemy::Picture, :load_method => :load_picture
     filter_access_to :thumbnail
@@ -58,6 +58,21 @@ module Alchemy
 
     def load_picture
       @picture ||= Picture.find(params[:id])
+    end
+
+    def ensure_secure_params
+      token = params[:sh]
+      digest = Digest::SHA1.hexdigest(secured_params)[0..15]
+      bad_request unless token && (token == digest)
+    end
+
+    def secured_params
+      [params[:id], params[:size], params[:crop], params[:crop_from], params[:crop_size], Rails.configuration.secret_token].join('-')
+    end
+
+    def bad_request
+      render :text => "Bad picture parameters in #{request.path}", :status => 400
+      return false
     end
 
   end
