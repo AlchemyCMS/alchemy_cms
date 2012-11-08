580be511298a4ec2eaf1202e2c75c49d2a4d3d81
Robin Böning <rb@magiclabs.de>
2012-11-08 21:53:32 +0100

    Put BaseHelper spec into Alchemy module.

--- spec/helpers/base_helper_spec.rb
+++ spec/helpers/base_helper_spec.rb
@@ -1,21 +1,23 @@
 require 'spec_helper'
 
-describe Alchemy::BaseHelper do
+module Alchemy
+  describe BaseHelper do
 
-  describe "#render_message" do
+    describe "#render_message" do
 
-    context "if no argument is passed" do
-      it "should render a div with an info icon and the given content" do
-        helper.render_message{ content_tag(:p, "my notice") }.should match(/<div class="info message"><span class="icon info"><\/span><p>my notice/)
+      context "if no argument is passed" do
+        it "should render a div with an info icon and the given content" do
+          helper.render_message{ content_tag(:p, "my notice") }.should match(/<div class="info message"><span class="icon info"><\/span><p>my notice/)
+        end
       end
-    end
 
-    context "if an argument is passed" do
-      it "should render the passed argument as the css classname for the icon container" do
-        helper.render_message(:error){ content_tag(:p, "my notice") }.should match(/<div class="error message"><span class="icon error">/)
+      context "if an argument is passed" do
+        it "should render the passed argument as the css classname for the icon container" do
+          helper.render_message(:error){ content_tag(:p, "my notice") }.should match(/<div class="error message"><span class="icon error">/)
+        end
       end
+
     end
 
   end
-
 end
