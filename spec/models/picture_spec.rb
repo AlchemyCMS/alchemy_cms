70a5e389a1ba15a452196be9563b7574bbd1a3d0
Thomas von Deyen <tvdeyen@gmail.com>
2012-11-22 23:09:50 +0100

    Fixes picture specs.

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -12,23 +12,25 @@ module Alchemy
       picture.should be_valid
     end
 
+    it "is not valid without image file" do
+      picture = Picture.new
+      picture.should_not be_valid
+    end
+
     describe '#suffix' do
 
       it "should return the suffix of original filename" do
-        pic = stub_model(Picture, :image_filename => 'kitten.JPG')
+        pic = stub_model(Picture, :image_file_name => 'kitten.JPG')
+        pic.stub(:image_file).and_return(OpenStruct.new({:ext => 'jpg'}))
         pic.suffix.should == "jpg"
       end
 
       context "image has no suffix" do
-
-        before(:each) do
-          @pic = stub_model(Picture, :image_filename => 'kitten')
-        end
-
         it "should return empty string" do
-          @pic.suffix.should == ""
+          pic = stub_model(Picture, :image_file_name => 'kitten')
+          pic.stub(:image_file).and_return(OpenStruct.new({:ext => ''}))
+          pic.suffix.should == ""
         end
-
       end
 
     end
@@ -36,27 +38,24 @@ module Alchemy
     describe '#humanized_name' do
 
       it "should return a humanized version of original filename" do
-        pic = stub_model(Picture, :image_filename => 'cute_kitten.JPG')
+        pic = stub_model(Picture, :image_file_name => 'cute_kitten.JPG')
+        pic.stub(:image_file).and_return(OpenStruct.new({:ext => 'jpg'}))
         pic.humanized_name.should == "Cute kitten"
       end
 
       it "should not remove incidents of suffix from filename" do
-        pic = stub_model(Picture, :image_filename => 'cute_kitten_mo.jpgi.JPG')
+        pic = stub_model(Picture, :image_file_name => 'cute_kitten_mo.jpgi.JPG')
+        pic.stub(:image_file).and_return(OpenStruct.new({:ext => 'jpg'}))
         pic.humanized_name.should == "Cute kitten mo.jpgi"
         pic.humanized_name.should_not == "Cute kitten moi"
       end
 
       context "image has no suffix" do
-
-        before(:each) do
-          @pic = stub_model(Picture, :image_filename => 'cute_kitten')
-          @pic.stub!(:suffix).and_return("")
-        end
-
         it "should return humanized name" do
-          @pic.humanized_name.should == "Cute kitten"
+          pic = stub_model(Picture, :image_file_name => 'cute_kitten')
+          pic.stub(:suffix).and_return("")
+          pic.humanized_name.should == "Cute kitten"
         end
-
       end
 
     end
