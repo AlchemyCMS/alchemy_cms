52c19d1114320bcb11af97e398be31ae29d13fbf
Hendrik Mans <hendrik@mans.de>
2012-12-06 12:02:49 +0100

    Add some simple specs for the new Site model.

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
-describe Alchemy::Spec do
-  pending
+module Alchemy
+  describe Site do
+    let(:site) { FactoryGirl.create(:site) }
+    let(:another_site) { FactoryGirl.create(:site, name: 'Another Site', host: 'another.com') }
+
+    describe '.current' do
+      context 'when set to a site' do
+        before { Site.current = site }
+        specify "Language should be scoped to that site"
+      end
+
+      context 'when set to nil' do
+        before { Site.current = nil }
+        specify "Language should not be scoped to a site"
+      end
+    end
+
+    describe '#current?' do
+      subject { site.current? }
+
+      context 'when Site.current is set to the same site' do
+        before { Site.current = site }
+        it { should be_true }
+      end
+
+      context 'when Site.current is set to nil' do
+        before { Site.current = nil }
+        it { should be_false }
+      end
+
+      context 'when Site.current is set to a different site' do
+        before { Site.current = another_site }
+        it { should be_false }
+      end
+    end
+  end
 end
