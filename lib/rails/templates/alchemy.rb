9031b34cd1518967640c9431a07bbde56728d08b
Thomas von Deyen <tvdeyen@gmail.com>
2012-12-09 21:50:53 +0100

    Fixes binary and rails templates for standard set removal.

--- lib/rails/templates/alchemy.rb
+++ lib/rails/templates/alchemy.rb
@@ -4,11 +4,15 @@ require 'alchemy/version'
 # Installing Alchemy Gem
 
 gem 'alchemy_cms', "~> #{Alchemy::VERSION}"
+
+if yes? "\nDo you want to install the Alchemy demo kit (from http://demo.alchemy-cms.com)? (y/N)"
+  #gem 'alchemy-demo_kit'
+  ENV['INSTALL_DEMO_KIT']
+end
+
 gem 'ruby-debug', :group => :development, :platform => :ruby_18
 gem 'debugger', :group => :development, :platform => :ruby_19
 
 if yes?("\nDo you want to use Capistrano for deployment? (y/N)")
   gem 'capistrano', :group => :development
 end
-
-run 'bundle install'
