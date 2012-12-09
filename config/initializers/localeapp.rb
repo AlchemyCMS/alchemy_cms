0dbb842538666c35b2adda3d2758c99d808df601
Thomas von Deyen <tvdeyen@gmail.com>
2012-12-09 21:47:35 +0100

    Removes old, unused files.

--- config/initializers/localeapp.rb
+++
@@ -1,9 +0,0 @@
-# unless ENV['CI']
-
-#   require 'localeapp/rails'
-
-#   Localeapp.configure do |config|
-#     config.api_key = 'afwgJCX6Xq40Y2gQSU9sdYFJ2XmQdAhgKmZPd69TOLdsT7e4Sy'
-#   end
-
-# end
