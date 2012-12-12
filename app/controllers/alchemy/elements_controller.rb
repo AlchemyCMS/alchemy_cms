e995317273715845c68ee8428620c70431fcc619
Thomas von Deyen <tvdeyen@gmail.com>
2012-12-12 10:08:16 +0100

    Reduce element json data set.

--- app/controllers/alchemy/elements_controller.rb
+++ app/controllers/alchemy/elements_controller.rb
@@ -10,15 +10,31 @@ module Alchemy
     #
     # * html
     # * js (Tries to replace a given +container_id+ with the elements view partial content via jQuery.)
-    # * json (A JSON object that includes all contents and their essences)
+    # * json (A JSON object that includes all contents and their ingredients)
     #
     def show
       @page = @element.page
+      @options = params[:options]
+
       respond_to do |format|
         format.html
-        format.js
+        format.js { @container_id = params[:container_id] }
         format.json do
-          render json: @element.to_json(include: {contents: {include: :essence}})
+          render json: @element.to_json(
+            only: [:id, :name, :updated_at],
+            methods: [:tag_list],
+            include: {
+              contents: {
+                only: [:id, :name, :updated_at, :essence_type],
+                methods: [:ingredient],
+                include: {
+                  essence: {
+                    except: [:created_at, :creator_id, :do_not_index, :public, :updater_id]
+                  }
+                }
+              }
+            }
+          )
         end
       end
     end
