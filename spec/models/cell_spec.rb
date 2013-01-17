e7497daaf9b434ceba9c8291372fc34ae5ef31ad
Robin Böning <rb@magiclabs.de>
2013-01-17 21:03:30 +0100

    Corrects formatting of cell specs.

--- spec/models/cell_spec.rb
+++ spec/models/cell_spec.rb
@@ -5,46 +5,38 @@ module Alchemy
 
     let(:cell) { FactoryGirl.build(:cell) }
 
-    context "#available_elements" do
+    describe "#available_elements" do
 
       context "without assigned elements" do
-
         it "should return an empty Array" do
           cell.stub!(:description).and_return({})
           cell.available_elements.should == []
         end
-
       end
 
       context "with assigned elements" do
-
         it "should return an Array of element names" do
           cell.stub!(:description).and_return({'elements' => ['test_element', 'test_element_2']})
           cell.available_elements.should == ['test_element', 'test_element_2']
         end
-
       end
 
     end
 
-    context "#description" do
+    describe "#description" do
 
       context "without a definition for the expected cellname" do
-
         it "should return an empty Hash" do
           Cell.stub!(:definition_for).and_return({})
           cell.description.should == {}
         end
-
       end
 
-      context "with a definition found" do
-
-        it "should return the definition Hash" do
+      context "with a definition for the expected cellname found" do
+        it "should return its definition Hash" do
           Cell.stub!(:definition_for).and_return({'name' => 'test_cell', 'elements' => ['test_element', 'test_element_2']})
           cell.description.should == {'name' => 'test_cell', 'elements' => ['test_element', 'test_element_2']}
         end
-
       end
 
     end
