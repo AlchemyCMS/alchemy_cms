fb68c23e4d3ece358e2dfce598745b32b28700e7
Thomas von Deyen <tvdeyen@gmail.com>
2013-01-18 17:45:14 +0100

    Always set a flash notice after sending message.

--- app/controllers/alchemy/messages_controller.rb
+++ app/controllers/alchemy/messages_controller.rb
@@ -99,12 +99,12 @@ module Alchemy
     end
 
     def redirect_to_success_page
+      flash[:notice] = _t(:success, :scope => 'contactform.messages')
       if @element.ingredient("success_page")
         urlname = @element.ingredient("success_page")
       elsif mailer_config['forward_to_page'] && mailer_config['mail_success_page']
         urlname = Page.find_by_urlname(mailer_config['mail_success_page']).urlname
       else
-        flash[:notice] = _t(:success, :scope => 'contactform.messages')
         urlname = Page.language_root_for(session[:language_id]).urlname
       end
       redirect_to show_page_path(:urlname => urlname, :lang => multi_language? ? session[:language_code] : nil)
