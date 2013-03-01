d4b989c98cd9b895fc93874b50581e7bd12e93a3
Robin Böning <rb@magiclabs.de>
2013-03-01 17:19:08 +0100

    Adds compressed migrations for Alchemy 2.2

--- db/migrate/20101216173323_add_default_to_languages.rb
+++
@@ -1,9 +0,0 @@
-class AddDefaultToLanguages < ActiveRecord::Migration
-  def self.up
-    add_column :languages, :default, :boolean, :default => false
-  end
-
-  def self.down
-    remove_column :languages, :default
-  end
-end
---
+++ db/migrate/20120704000000_alchemy_two_point_two.rb
@@ -0,0 +1,284 @@
+# This is a compressed migration for creating all Alchemy 2.2 tables at once.
+#
+# === Notice
+#
+# In order to upgrade from an old version of Alchemy, you have to run all migrations from
+# each version you missed up to the version you want to upgrade to, before running this migration.
+#
+class AlchemyTwoPointTwo< ActiveRecord::Migration
+  def up
+
+    return if table_exists?(:alchemy_pages)
+
+    create_table "alchemy_attachments", :force => true do |t|
+      t.string   "name"
+      t.string   "filename"
+      t.string   "content_type"
+      t.integer  "size"
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+      t.datetime "created_at",   :null => false
+      t.datetime "updated_at",   :null => false
+    end
+
+    create_table "alchemy_essence_booleans", :force => true do |t|
+      t.boolean "value"
+      t.timestamps
+      t.userstamps
+    end
+
+    add_index "alchemy_essence_booleans", ["value"], :name => "index_alchemy_essence_booleans_on_value"
+
+    create_table "alchemy_cells", :force => true do |t|
+      t.integer  "page_id"
+      t.string   "name"
+      t.datetime "created_at", :null => false
+      t.datetime "updated_at", :null => false
+    end
+
+    create_table "alchemy_contents", :force => true do |t|
+      t.string   "name"
+      t.string   "essence_type"
+      t.integer  "essence_id"
+      t.integer  "element_id"
+      t.integer  "position"
+      t.datetime "created_at",   :null => false
+      t.datetime "updated_at",   :null => false
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+    end
+
+    add_index "alchemy_contents", ["element_id", "position"], :name => "index_contents_on_element_id_and_position"
+
+    create_table "alchemy_elements", :force => true do |t|
+      t.string   "name"
+      t.integer  "position"
+      t.integer  "page_id"
+      t.boolean  "public",     :default => true
+      t.boolean  "folded",     :default => false
+      t.boolean  "unique",     :default => false
+      t.datetime "created_at",                    :null => false
+      t.datetime "updated_at",                    :null => false
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+      t.integer  "cell_id"
+    end
+
+    add_index "alchemy_elements", ["page_id", "position"], :name => "index_elements_on_page_id_and_position"
+
+    create_table "alchemy_elements_alchemy_pages", :id => false, :force => true do |t|
+      t.integer "element_id"
+      t.integer "page_id"
+    end
+
+    create_table "alchemy_essence_audios", :force => true do |t|
+      t.integer  "attachment_id"
+      t.integer  "width",           :default => 400
+      t.integer  "height",          :default => 300
+      t.boolean  "show_eq",         :default => true
+      t.boolean  "show_navigation", :default => true
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+      t.datetime "created_at",                        :null => false
+      t.datetime "updated_at",                        :null => false
+    end
+
+    create_table "alchemy_essence_dates", :force => true do |t|
+      t.datetime "date"
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+      t.datetime "created_at", :null => false
+      t.datetime "updated_at", :null => false
+    end
+
+    create_table "alchemy_essence_files", :force => true do |t|
+      t.integer  "attachment_id"
+      t.string   "title"
+      t.string   "css_class"
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+      t.datetime "created_at",    :null => false
+      t.datetime "updated_at",    :null => false
+    end
+
+    create_table "alchemy_essence_flashes", :force => true do |t|
+      t.integer  "attachment_id"
+      t.integer  "width",          :default => 400
+      t.integer  "height",         :default => 300
+      t.string   "player_version", :default => "9.0.28"
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+      t.datetime "created_at",                           :null => false
+      t.datetime "updated_at",                           :null => false
+    end
+
+    create_table "alchemy_essence_htmls", :force => true do |t|
+      t.text     "source"
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+      t.datetime "created_at", :null => false
+      t.datetime "updated_at", :null => false
+    end
+
+    create_table "alchemy_essence_pictures", :force => true do |t|
+      t.integer  "picture_id"
+      t.string   "caption"
+      t.string   "title"
+      t.string   "alt_tag"
+      t.string   "link"
+      t.string   "link_class_name"
+      t.string   "link_title"
+      t.string   "css_class"
+      t.string   "link_target"
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+      t.datetime "created_at",      :null => false
+      t.datetime "updated_at",      :null => false
+      t.string   "crop_from"
+      t.string   "crop_size"
+      t.string   "render_size"
+    end
+
+    create_table "alchemy_essence_richtexts", :force => true do |t|
+      t.text     "body"
+      t.text     "stripped_body"
+      t.boolean  "do_not_index",  :default => false
+      t.boolean  "public"
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+      t.datetime "created_at",                       :null => false
+      t.datetime "updated_at",                       :null => false
+    end
+
+    create_table "alchemy_essence_selects", :force => true do |t|
+      t.string "value"
+      t.timestamps
+      t.userstamps
+    end
+
+    add_index "alchemy_essence_selects", ["value"], :name => "index_alchemy_essence_selects_on_value"
+
+    create_table "alchemy_essence_texts", :force => true do |t|
+      t.text     "body"
+      t.string   "link"
+      t.string   "link_title"
+      t.string   "link_class_name"
+      t.boolean  "public",          :default => false
+      t.boolean  "do_not_index",    :default => false
+      t.string   "link_target"
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+      t.datetime "created_at",                         :null => false
+      t.datetime "updated_at",                         :null => false
+    end
+
+    create_table "alchemy_essence_videos", :force => true do |t|
+      t.integer  "attachment_id"
+      t.integer  "width"
+      t.integer  "height"
+      t.boolean  "allow_fullscreen", :default => true
+      t.boolean  "auto_play",        :default => false
+      t.boolean  "show_navigation",  :default => true
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+      t.datetime "created_at",                          :null => false
+      t.datetime "updated_at",                          :null => false
+    end
+
+    create_table "alchemy_folded_pages", :force => true do |t|
+      t.integer "page_id"
+      t.integer "user_id"
+      t.boolean "folded",  :default => false
+    end
+
+    create_table "alchemy_languages", :force => true do |t|
+      t.string   "name"
+      t.string   "language_code"
+      t.string   "frontpage_name"
+      t.string   "page_layout",    :default => "intro"
+      t.boolean  "public",         :default => false
+      t.datetime "created_at",                          :null => false
+      t.datetime "updated_at",                          :null => false
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+      t.boolean  "default",        :default => false
+      t.string   "country_code"
+    end
+
+    add_index "alchemy_languages", ["language_code", "country_code"], :name => "index_alchemy_languages_on_language_code_and_country_code"
+    add_index "alchemy_languages", ["language_code"], :name => "index_alchemy_languages_on_language_code"
+
+    create_table "alchemy_pages", :force => true do |t|
+      t.string   "name"
+      t.string   "urlname"
+      t.string   "title"
+      t.string   "language_code"
+      t.boolean  "language_root"
+      t.string   "page_layout"
+      t.text     "meta_keywords"
+      t.text     "meta_description"
+      t.integer  "lft"
+      t.integer  "rgt"
+      t.integer  "parent_id"
+      t.integer  "depth"
+      t.boolean  "visible",                         :default => false
+      t.boolean  "public",                          :default => false
+      t.boolean  "locked",                          :default => false
+      t.integer  "locked_by"
+      t.boolean  "restricted",                      :default => false
+      t.boolean  "robot_index",                     :default => true
+      t.boolean  "robot_follow",                    :default => true
+      t.boolean  "sitemap",                         :default => true
+      t.boolean  "layoutpage",                      :default => false
+      t.datetime "created_at",                                         :null => false
+      t.datetime "updated_at",                                         :null => false
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+      t.integer  "language_id"
+    end
+
+    add_index "alchemy_pages", ["language_id"], :name => "index_pages_on_language_id"
+    add_index "alchemy_pages", ["parent_id", "lft"], :name => "index_pages_on_parent_id_and_lft"
+    add_index "alchemy_pages", ["urlname"], :name => "index_pages_on_urlname"
+
+    create_table "alchemy_pictures", :force => true do |t|
+      t.string   "name"
+      t.string   "image_filename"
+      t.integer  "image_width"
+      t.integer  "image_height"
+      t.datetime "created_at",     :null => false
+      t.datetime "updated_at",     :null => false
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+    end
+
+    create_table "alchemy_users", :force => true do |t|
+      t.string   "firstname"
+      t.string   "lastname"
+      t.string   "login"
+      t.string   "email"
+      t.string   "gender"
+      t.string   "role",                               :default => "registered"
+      t.string   "language"
+      t.string   "crypted_password",    :limit => 128, :default => "",           :null => false
+      t.string   "password_salt",       :limit => 128, :default => "",           :null => false
+      t.integer  "login_count",                        :default => 0,            :null => false
+      t.integer  "failed_login_count",                 :default => 0,            :null => false
+      t.datetime "last_request_at"
+      t.datetime "current_login_at"
+      t.datetime "last_login_at"
+      t.string   "current_login_ip"
+      t.string   "last_login_ip"
+      t.string   "persistence_token",                                            :null => false
+      t.string   "single_access_token",                                          :null => false
+      t.string   "perishable_token",                                             :null => false
+      t.datetime "created_at",                                                   :null => false
+      t.datetime "updated_at",                                                   :null => false
+      t.integer  "creator_id"
+      t.integer  "updater_id"
+    end
+
+    add_index "alchemy_users", ["perishable_token"], :name => "index_users_on_perishable_token"
+
+  end
+end
