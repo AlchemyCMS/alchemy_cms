35ee61059617647468e200aabc1a04612ad367e3
Thomas von Deyen <tvdeyen@gmail.com>
2013-03-29 17:40:16 +0100

    Move the Resource documentation to where it belongs.

--- lib/alchemy/resource.rb
+++ lib/alchemy/resource.rb
@@ -2,67 +2,66 @@ require 'active_support/inflector'
 require 'active_support/core_ext'
 
 module Alchemy
+  # = Alchemy::Resource
+  #
+  # Used to DRY up resource like structures in Alchemy's admin backend.
+  # So far Language, User and Tag already uses this.
+  #
+  # It provides convenience methods to create an admin interface without further knowledge about
+  # the model and the controller (it's instantiated with controller_path at least and guesses the model accordingly)
+  #
+  # For examples how to use in controllers see Alchemy::ResourcesController or inherit from it directly.
+  #
+  # == Naming Conventions
+  #
+  # As Rails' form helpers, path helpers, etc. and declarative authorization rely on controller_path even if the model
+  # class is named differently (or sits in another namespace) model and controller are handled separatly here.
+  # Therefore "resource" always refers to the controller_path whereas "model" refers to the model class.
+  #
+  # == Skip attributes
+  #
+  # Usually you don't want your users to edit all attributes provided by a model. Hence some default attributes,
+  # namely id, updated_at, created_at, creator_id and updater_id are not returned by Resource#attributes.
+  #
+  # If you want to skip a different set of attributes just define a skip_attributes class method in your model class
+  # that returns an array of strings: %W[id, updated_at]
+  #
+  # == Resource relations
+  #
+  # Alchemy::Resource can take care of ActiveRecord relations. You will have to announce relations by defining a
+  # resource_relations class method in your model class that returns a hash like this:
+  #
+  #     {
+  #       :location_id => {:attr_method => "location#name", :attr_type => :string},
+  #       :organizer_id => {:attr_method => "organizer#name", :attr_type => :string}
+  #     }
+  #
+  # With this knowledge Resource#attributes will return location#name and organizer#name instead of location_id
+  # and organizer_id. Refer to Alchemy::ResourcesController for further details on usage.
+  #
+  # == Creation
+  #
+  # Resource needs a controller_path at least. Without other arguments it will guess the model name from it and assume
+  # that the model doesn't live in an engine. Moreover model and controller has to follow Rails' naming convention:
+  #
+  #   Event -> EventsController
+  #
+  # It will also strip "admin" automatically, so this is also valid:
+  #
+  #   Event -> Admin::EventsController
+  #
+  # If your Resource and it's controllers are part of an engine you need to provide Alchemy's module_definition,
+  # so resource can provide the correct url_proxy. If you don't declare it in Alchemy, you need at least provide the
+  # following hash (i.e. if your engine is named EventEngine):
+  #
+  #     resource = Resource.new(controller_path, {"engine_name" => "event_engine"})
+  #
+  # If you don't want to stick with these conventions you can separate model and controller by providing
+  # a model class (for example used by Alchemy's Tags admin interface):
+  #
+  #     resource = Resource.new('/admin/tags', {"engine_name"=>"alchemy"}, ActsAsTaggableOn::Tag)
+  #
   class Resource
-    # = Alchemy::Resource
-    #
-    # Used to DRY up resource like structures in Alchemy's admin backend.
-    # So far Language, User and Tag already uses this.
-    #
-    # It provides convenience methods to create an admin interface without further knowledge about
-    # the model and the controller (it's instantiated with controller_path at least and guesses the model accordingly)
-    #
-    # For examples how to use in controllers see Alchemy::ResourcesController or inherit from it directly.
-    #
-    # == Naming Conventions
-    #
-    # As Rails' form helpers, path helpers, etc. and declarative authorization rely on controller_path even if the model
-    # class is named differently (or sits in another namespace) model and controller are handled separatly here.
-    # Therefore "resource" always refers to the controller_path whereas "model" refers to the model class.
-    #
-    # == Skip attributes
-    #
-    # Usually you don't want your users to edit all attributes provided by a model. Hence some default attributes,
-    # namely id, updated_at, created_at, creator_id and updater_id are not returned by Resource#attributes.
-    #
-    # If you want to skip a different set of attributes just define a skip_attributes class method in your model class
-    # that returns an array of strings: %W[id, updated_at]
-    #
-    # == Resource relations
-    #
-    # Alchemy::Resource can take care of ActiveRecord relations. You will have to announce relations by defining a
-    # resource_relations class method in your model class that returns a hash like this:
-    #
-    # {
-    #   :location_id => {:attr_method => "location#name", :attr_type => :string},
-    #   :organizer_id => {:attr_method => "organizer#name", :attr_type => :string}
-    # }
-    #
-    # With this knowledge Resource#attributes will return location#name and organizer#name instead of location_id
-    # and organizer_id. Refer to Alchemy::ResourcesController for further details on usage.
-    #
-    # == Creation
-    #
-    # Resource needs a controller_path at least. Without other arguments it will guess the model name from it and assume
-    # that the model doesn't live in an engine. Moreover model and controller has to follow Rails' naming convention:
-    #
-    # Event -> EventsController
-    #
-    # As namespaced models are not considered best practice it will strip "admin" automatically, so this is also valid:
-    #
-    # Event -> Admin::EventsController
-    #
-    # If your Resource and it's controllers are part of an engine you need to provide Alchemy's module_definition,
-    # so resource can provide the correct url_proxy. If you don't declare it in Alchemy, you need at least provide the
-    # following hash (i.e. if your engine is named EventEngine):
-    #
-    # resource = Resource.new(controller_path, {"engine_name" => "event_engine"})
-    #
-    # If you don't want to stick with these conventions you can separate model and controller by providing
-    # a model class (for example used by Alchemy's Tags admin interface):
-    #
-    # resource = Resource.new('/admin/tags', {"engine_name"=>"alchemy"}, ActsAsTaggableOn::Tag)
-    #
-
     attr_accessor :skip_attributes, :resource_relations, :model_associations
     attr_reader :model
 
