6ea6fff986b4e188a04c9d0766895851b46d4d05
Thomas von Deyen <tvdeyen@gmail.com>
2013-04-29 20:51:54 +0200

    Removes spec folder from coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter '/spec/'
+end
 
 begin
   require 'spork'
