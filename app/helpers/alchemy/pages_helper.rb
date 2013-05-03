35e84153c624affdfc572146e36867935e05f63e
Robin Böning <rb@magiclabs.de>
2013-05-03 11:38:15 +0200

    Refactoring the language_switcher helper. (Now called language_links)
    * Removed the following options (link_to_public_child, link_to_page_with_layout, as_select_box, show_flags)
    
    * Renders a partial instead of returning html markup as a string.

--- app/helpers/alchemy/pages_helper.rb
+++ app/helpers/alchemy/pages_helper.rb
@@ -15,82 +15,39 @@ module Alchemy
       content.essence.caption
     end
 
-    # == Helper for rendering language switches
+    # Renders links to language root pages of all published languages.
     #
-    # Renders links to all public language root pages
+    # @option options linkname [String] ('name') Renders name/code of language, or I18n translation for code.
+    # @option options show_title [Boolean] (true) Renders title attributes for the links.
+    # @option options spacer [String] ('') Renders the passed spacer string. You can also overwrite the spacer partial: "alchemy/language_links/_spacer".
+    # @option options reverse [Boolean] (false) Reverses the ordering of the links.
     #
-    # === Options:
-    #
-    #   :linkname => :name,
-    #   :spacer => "",
-    #   :link_to_public_child => configuration(:redirect_to_public_child),
-    #   :link_to_page_with_layout => nil,
-    #   :show_title => true,
-    #   :reverse => false,
-    #   :as_select_box => false,
-    #   :show_flags => false
-    #
-    def language_switcher(options={})
-      default_options = {
-        :linkname => :name,
-        :spacer => "",
-        :link_to_public_child => configuration(:redirect_to_public_child),
-        :link_to_page_with_layout => nil,
-        :show_title => true,
-        :reverse => false,
-        :as_select_box => false,
-        :show_flags => false
-      }
-      options = default_options.merge(options)
-      if multi_language?
-        language_links = []
-        pages = (options[:link_to_public_child] == true) ? Page.from_current_site.language_roots : Page.from_current_site.public_language_roots
-        return nil if (pages.blank? || pages.length == 1)
-        pages.each_with_index do |page, i|
-          if (options[:link_to_page_with_layout] != nil)
-            page_found_by_layout = Page.from_current_site.where(:page_layout => options[:link_to_page_with_layout].to_s, :language_id => page.language_id).first
-          end
-          page = page_found_by_layout || page
-          page = (options[:link_to_public_child] ? (page.first_public_child.blank? ? nil : page.first_public_child) : nil) if !page.public?
-          if !page.blank?
-            active = session[:language_id] == page.language.id
-            linkname = page.language.label(options[:linkname])
-            if options[:as_select_box]
-              language_links << [linkname, show_page_url(:urlname => page.urlname, :lang => page.language.code)]
-            else
-              language_links << link_to(
-                "#{content_tag(:span, '', :class => "flag") if options[:show_flags]}#{ content_tag(:span, linkname)}".html_safe,
-                alchemy.show_page_path(:urlname => page.urlname, :lang => page.language.code),
-                :class => "#{(active ? 'active ' : nil)}#{page.language.code} #{(i == 0) ? 'first' : (i==pages.length-1) ? 'last' : nil}",
-                :title => options[:show_title] ? Alchemy::I18n.t("alchemy.language_links.#{page.language.code}.title", :default => page.language.name) : nil
-              )
-            end
-          end
-          # when last iteration and we have just one language_link,
-          # we dont need to render it.
-          if (i==pages.length-1) && language_links.length == 1
-            return nil
-          end
-        end
-        return nil if language_links.empty? || language_links.length == 1
-        language_links.reverse! if options[:reverse]
-        if options[:as_select_box]
-          return select_tag(
-            'language',
-            options_for_select(
-              language_links,
-              show_page_url(:urlname => @page.urlname, :lang => @page.language.code)
-            ),
-            :onchange => "window.location=this.value"
-          )
-        else
-          raw(language_links.join(options[:spacer]))
-        end
-      else
-        nil
-      end
+    def language_links(options={})
+      options = {
+        linkname: 'name',
+        show_title: true,
+        spacer: '',
+        reverse: false
+      }.merge(options)
+      languages = Language.published.with_language_root.order("name #{options[:reverse] ? 'DESC' : 'ASC'}")
+      return nil if languages.count < 2
+      render(
+        partial: "alchemy/language_links/language",
+        collection: languages,
+        spacer_template: "alchemy/language_links/spacer",
+        locals: {languages: languages, options: options}
+      )
     end
-    alias_method :language_switches, :language_switcher
+
+    def language_switches(options={})
+      ActiveSupport::Deprecation.warn("Used deprecated language_switches helper. Please use language_links instead.")
+      language_links(options)
+    end
+
+    def language_switcher(options={})
+      ActiveSupport::Deprecation.warn("Used deprecated language_switcher helper. Please use language_links instead.")
+      language_links(options)
+    end
 
     # Renders the layout from @page.page_layout. File resists in /app/views/page_layouts/_LAYOUT-NAME.html.erb
     def render_page_layout(options={})
