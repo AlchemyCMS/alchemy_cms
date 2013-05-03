c610370656525176af5278a399950d17c5c5bb38
Thomas von Deyen <tvdeyen@gmail.com>
2013-05-04 00:17:12 +0200

    Adds upgrader for new dragonfly based attachments storage.

--- lib/alchemy/upgrader.rb
+++ lib/alchemy/upgrader.rb
@@ -20,10 +20,35 @@ module Alchemy
         Rake::Task['alchemy:install:migrations'].invoke
         Rake::Task['db:migrate'].invoke
         Seeder.seed!
+        convert_attachment_storage
         copy_new_config_file
       end
 
     private
+
+      def convert_attachment_storage
+        desc "Convert the attachment storage"
+        converted_files = []
+        files = Dir.glob Rails.root.join 'uploads/attachments/**/*.*'
+        if files.blank?
+          log "No attachments found", :skip
+        else
+          files.each do |file|
+            file_uid = file.gsub(/#{Rails.root.to_s}\/uploads\/attachments\//, '')
+            file_id = file_uid.split('/')[1].to_i
+            attachment = Alchemy::Attachment.find_by_id(file_id)
+            if attachment && attachment.file_uid.blank?
+              attachment.file_uid = file_uid
+              attachment.file_size = File.new(file).size
+              if attachment.save!
+                log "Converted #{file_uid}"
+              end
+            else
+              log "Attachment with id #{file_id} not found or already converted.", :skip
+            end
+          end
+        end
+      end
 
       def copy_new_config_file
         desc "Copy configuration file."
