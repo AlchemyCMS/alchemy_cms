99b73f357b3d6cc68612db73352060c7d4cf76fa
Thomas von Deyen <tvdeyen@gmail.com>
2013-05-15 12:33:36 +0200

    Fix namespace issues in essence generator.

--- lib/rails/generators/alchemy/essence/essence_generator.rb
+++ lib/rails/generators/alchemy/essence/essence_generator.rb
@@ -27,17 +27,18 @@ module Alchemy
   acts_as_essence(
     # Your options:
     #
-    # :ingredient_column =>   Symbol      # Specifies the column name you use for storing the content in the database. [Default] :body
-    # :validate_column =>     Symbol      # Which column should be validated. [Default] ingredient_column
-    # :preview_text_column => Symbol      # Specifies the column for the preview_text method. [Default] ingredient_column
-    # :preview_text_method => Symbol      # A method called on ingredient to get the preview text.
+    # ingredient_column:   [String or Symbol] - Specifies the column name you use for storing the content in the database. (Default :body)
+    # validate_column:     [String or Symbol] - Which column should be validated. (Default: ingredient_column)
+    # preview_text_column: [String or Symbol] - Specifies the column for the preview_text method. (Default: ingredient_column)
+    # preview_text_method: [String or Symbol] - A method called on ingredient to get the preview text. (Default: ingredient_column)
   )
 CLASSMETHOD
       end
 
       def copy_templates
-        template "view.html.erb", "#{@essence_view_path}/_#{@essence_name}_view.html.erb"
-        template "editor.html.erb", "#{@essence_view_path}/_#{@essence_name}_editor.html.erb"
+        essence_name = @essence_name.classify.demodulize
+        template "view.html.erb", "#{@essence_view_path}/_#{essence_name}_view.html.erb"
+        template "editor.html.erb", "#{@essence_view_path}/_#{essence_name}_editor.html.erb"
       end
 
       def show_todo
