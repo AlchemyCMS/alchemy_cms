8aac9bd67b0ee9fd2278837478d3f0236359a24e
Robin Böning <rb@magiclabs.de>
2013-05-23 22:46:53 +0200

    Adds specs for LayoutpagesController#index

---
+++ spec/controllers/admin/layoutpages_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+module Alchemy
+  describe Admin::LayoutpagesController do
+
+    before(:each) do
+      sign_in :user, FactoryGirl.create(:admin_user)
+    end
+
+    describe "#index" do
+      it "should assign @locked_pages" do
+        get :index
+        expect(assigns(:locked_pages)).to eq([])
+      end
+
+      it "should assign @layout_root" do
+        get :index
+        expect(assigns(:layout_root)).to be_a(Page)
+      end
+
+      it "should assign @languages" do
+        Language.stub!(:all).and_return([])
+        get :index
+        expect(assigns(:languages)).to eq([])
+      end
+    end
+  end
+end
