695ada2f00ba3e4ca2e0b73397ff9aea7bb70280
Robin Böning <rb@magiclabs.de>
2013-05-24 12:26:54 +0200

    Adds some specs for DashBoardController.

--- spec/controllers/admin/dashboard_controller_spec.rb
+++ spec/controllers/admin/dashboard_controller_spec.rb
@@ -6,6 +6,40 @@ module Alchemy
     before do
       sign_in :user, FactoryGirl.create(:admin_user)
     end
+
+    describe '#index' do
+      before do
+        Page.stub!(:all_last_edited_from).and_return([])
+        Page.stub_chain(:from_current_site, :all_locked).and_return([])
+        User.stub!(:logged_in).and_return([controller.current_user])
+        controller.current_user.stub!(:sign_in_count).and_return(5)
+      end
+
+      it "should assign @last_edited_pages" do
+        get :index
+        expect(assigns(:last_edited_pages)).to eq([])
+      end
+
+      it "should assign @locked_pages" do
+        get :index
+        expect(assigns(:locked_pages)).to eq([])
+      end
+
+      it "should assign @online_users" do
+        get :index
+        expect(assigns(:online_users)).to eq([])
+      end
+
+      it "should assign @first_time" do
+        get :index
+        expect(assigns(:first_time)).to eq(false)
+      end
+
+      it "should assign @sites" do
+        get :index
+        expect(assigns(:sites)).to eq(Site.scoped)
+      end
+    end
     
     describe '#info' do
       it "should assign @alchemy_version with the current Alchemy version" do
@@ -16,14 +50,11 @@ module Alchemy
 
     describe '#update_check' do
 
-      before do
-        Net::HTTP.any_instance.stub(:request).and_return(
-          OpenStruct.new({code: '200', body: '[{"number": "2.6"}, {"number": "2.5"}]'})
-        )
-      end
-
       context "if current Alchemy version equals the latest released version or it is newer" do
-        before { Alchemy.stub!(:version).and_return("2.6") }
+        before do
+          controller.stub!(:latest_alchemy_version).and_return('2.6')
+          Alchemy.stub!(:version).and_return("2.6")
+        end
 
         it "should render 'false'" do
           get :update_check
@@ -32,11 +63,55 @@ module Alchemy
       end
 
       context "if current Alchemy version is older than latest released version" do
-        before { Alchemy.stub!(:version).and_return("2.5") }
+        before do
+          controller.stub!(:latest_alchemy_version).and_return('2.6')
+          Alchemy.stub!(:version).and_return("2.5")
+        end
 
         it "should render 'true'" do
           get :update_check
           expect(response.body).to eq('true')
+        end
+      end
+
+      context "requesting rubygems.org" do
+        before do
+          Net::HTTP.any_instance.stub(:request).and_return(
+            OpenStruct.new({code: '200', body: '[{"number": "2.6"}, {"number": "2.5"}]'})
+          )
+          Alchemy.stub!(:version).and_return("2.6")
+        end
+
+        it "should have response code of 200" do
+          get :update_check
+          expect(response.code).to eq('200')
+        end
+      end
+
+      context "requesting github.com" do
+        before do
+          controller.stub!(:query_rubygems).and_return(OpenStruct.new({code: '503'}))
+          Net::HTTP.any_instance.stub(:request).and_return(
+            OpenStruct.new({code: '200', body: '[{"name": "2.6"}, {"name": "2.5"}]'})
+          )
+        end
+
+        it "should have response code of 200" do
+          get :update_check
+          expect(response.code).to eq('200')
+        end
+      end
+
+      context "rubygems.org and github.com are unavailable" do
+        before do
+          Net::HTTP.any_instance.stub(:request).and_return(
+            OpenStruct.new({code: '503'})
+          )
+        end
+
+        it "should have status code 503" do
+          get :update_check
+          expect(response.code).to eq('503')
         end
       end
 
