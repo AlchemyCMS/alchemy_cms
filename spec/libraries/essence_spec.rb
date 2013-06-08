a596a99fd10ebaef0a0bbec847cca979db06124b
Robin Böning <rb@magiclabs.de>
2013-06-08 21:07:19 +0200

    Adds and refactors specs for ActsAsEssence mixin.

--- spec/libraries/essence_spec.rb
+++ spec/libraries/essence_spec.rb
@@ -1,18 +1,38 @@
 require 'spec_helper'
 
-describe "ActsAsEssence" do
+module Alchemy
+  describe "ActsAsEssence" do
+    #let(:element) { FactoryGirl.create(:element, :name => 'headline', :create_contents_after_create => true) }
+    let(:essence) { Alchemy::EssenceText.new }
 
-  let(:element) { FactoryGirl.create(:element, :name => 'headline', :create_contents_after_create => true) }
+    describe '#ingredient=' do
+      it 'should set the value to ingredient column' do
+        essence.ingredient = 'Hallo'
+        expect(essence.ingredient).to eq('Hallo')
+      end
+    end
 
-  describe '#ingredient=' do
+    describe '#open_link_in_new_window?' do
 
-    it "should set the value to ingredient column" do
-      content = element.content_by_name('headline')
-      content.essence.ingredient = 'Hallo'
-      content.essence.save
-      content.essence.ingredient.should == 'Hallo'
+      subject { essence.open_link_in_new_window? }
+
+      context 'essence responds to link_taget' do
+        context 'if link_target attribute is set to "blank"' do
+
+          before { essence.link_target = 'blank' }
+
+          it "should return true" do
+            expect(subject).to eq(true)
+          end
+        end
+
+        context 'if link_target attribute is not "blank"' do
+          it "should return false" do
+            expect(subject).to eq(false)
+          end
+        end
+      end
     end
 
   end
-
 end
