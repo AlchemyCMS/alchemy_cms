AlchemyCMS/alchemy_cms

commit af0d042baefa0297a6af92cf289fded40d5cc9fa

Element takes use of #display_name (same behavior as before)

app/helpers/alchemy/admin/elements_helper.rb
@@ -48,7 +48,7 @@ module Alchemy
         return [] if elements.nil?
         options = elements.collect do |e|
           [
-            _t(e['name'], scope: 'element_names', default: e['name'].to_s.humanize),
+            e.display_name,
             e['name']
           ]
         end
@@ -89,7 +89,7 @@ module Alchemy
           ]
         else
           [
-            _t(e['name'], scope: :element_names, default: e['name'].to_s.humanize),
+            e.display_name,
             e[object_method] + (cell ? "##{cell['name']}" : "")
           ]
         end

spec/helpers/admin/elements_helper_spec.rb
@@ -30,7 +30,10 @@ describe Alchemy::Admin::ElementsHelper do
     before(:each) do
       @page.stub!(:layout_description).and_return({'name' => "foo", 'cells' => ["foo_cell"]})
       cell_descriptions = [{'name' => "foo_cell", 'elements' => ["1", "2"]}]
-      @elements = [{'name' => '1'}, {'name' => '2'}]
+      @elements = [
+        mock_model('Element', name: '1', display_name: '1'),
+        mock_model('Element', name: '2', display_name: '2')
+      ]
       Alchemy::Cell.stub!(:definitions).and_return(cell_descriptions)
     end
 
@@ -56,8 +59,8 @@ describe Alchemy::Admin::ElementsHelper do
   describe "#elements_for_select" do
     before do
       @elements = [
-        mock_model('Element', name: 'element_1'),
-        mock_model('Element', name: 'element_2')
+        mock_model('Element', name: 'element_1', display_name: 'Element 1'),
+        mock_model('Element', name: 'element_2', display_name: 'Element 2')
       ]
     end
 

commit 87522f06fe177a2b37dc605590ff74903f5b9640

Fixes issue with ElementsHelper#elements_for_select. Adds specs and refactors the code.
Issue was introduced by af0d042baefa0297a6af92cf289fded40d5cc9fa where Element#display_name was called on a Hash instead of an element instance.

app/helpers/alchemy/admin/elements_helper.rb
@@ -48,7 +48,7 @@ module Alchemy
         return [] if elements.nil?
         options = elements.collect do |e|
           [
-            e.display_name,
+            Element.display_name_for(e['name']),
             e['name']
           ]
         end
@@ -89,7 +89,7 @@ module Alchemy
           ]
         else
           [
-            e.display_name,
+            Element.display_name_for(e['name']),
             e[object_method] + (cell ? "##{cell['name']}" : "")
           ]
         end

app/models/alchemy/element.rb
@@ -178,6 +178,10 @@ module Alchemy
         clipboard_elements.select { |ce| allowed_element_names.include?(ce.name) }
       end
 
+      def display_name_for(name)
+        I18n.t(name, scope: 'element_names', default: name.to_s.humanize)
+      end
+
     end
 
     # Returns next public element from same page.
@@ -309,8 +313,7 @@ module Alchemy
     # If no translation is found a humanized name is used.
     #
     def display_name
-      return name.humanize if description.blank?
-      I18n.t(description['name'], scope: 'element_names', default: description['name'].to_s.humanize)
+      self.class.display_name_for(description['name'] || self.name)
     end
 
     # Gets the preview text from the first Content found in the +elements.yml+ Element description file.

spec/helpers/admin/elements_helper_spec.rb
@@ -1,74 +1,101 @@
 require 'spec_helper'
 
-include Alchemy::BaseHelper
+module Alchemy
 
-describe Alchemy::Admin::ElementsHelper do
+  include BaseHelper
 
-  context "partial rendering" do
+  describe Admin::ElementsHelper do
 
-    before do
-      @page = FactoryGirl.create(:public_page)
-      @element = FactoryGirl.create(:element, :page => @page, :create_contents_after_create => true)
-    end
+    context "partial rendering" do
 
-    it "should render an element editor partial" do
-      helper.render_editor(@element).should match(/class="essence_text content_editor".+id="essence_text_\d{1,}"/)
-    end
-
-    it "should render a picture gallery editor partial" do
-      helper.render_picture_gallery_editor(@element).should match(/class=".+picture_gallery_editor"/)
-    end
-
-  end
-
-  describe "#grouped_elements_for_select" do
-
-    before do
-      @page = FactoryGirl.create(:public_page)
-    end
-
-    before(:each) do
-      @page.stub!(:layout_description).and_return({'name' => "foo", 'cells' => ["foo_cell"]})
-      cell_descriptions = [{'name' => "foo_cell", 'elements' => ["1", "2"]}]
-      @elements = [
-        mock_model('Element', name: '1', display_name: '1'),
-        mock_model('Element', name: '2', display_name: '2')
-      ]
-      Alchemy::Cell.stub!(:definitions).and_return(cell_descriptions)
-    end
-
-    it "should return string of elements grouped by cell for select_tag helper" do
-      helper.grouped_elements_for_select(@elements).should == helper.grouped_options_for_select({"Foo cell" => [["1", "1#foo_cell"], ["2", "2#foo_cell"]]})
-    end
-
-    context "with empty elements array" do
-      it "should return an empty string" do
-        helper.grouped_elements_for_select([]).should == ""
+      before do
+        @page = FactoryGirl.create(:public_page)
+        @element = FactoryGirl.create(:element, :page => @page, :create_contents_after_create => true)
       end
-    end
 
-    context "with empty cell definitions" do
-      it "should return an empty string" do
-        @page.stub!(:layout_description).and_return({'name' => "foo"})
-        helper.grouped_elements_for_select(@elements).should == ""
+      it "should render an element editor partial" do
+        helper.render_editor(@element).should match(/class="essence_text content_editor".+id="essence_text_\d{1,}"/)
       end
+
+      it "should render a picture gallery editor partial" do
+        helper.render_picture_gallery_editor(@element).should match(/class=".+picture_gallery_editor"/)
+      end
+
+    end
+
+    describe "#grouped_elements_for_select" do
+
+      before do
+        @page = FactoryGirl.create(:public_page)
+      end
+
+      before(:each) do
+        @page.stub!(:layout_description).and_return({'name' => "foo", 'cells' => ["foo_cell"]})
+        cell_descriptions = [{'name' => "foo_cell", 'elements' => ["1", "2"]}]
+        @elements = [
+          mock_model('Element', name: '1', display_name: '1'),
+          mock_model('Element', name: '2', display_name: '2')
+        ]
+        Alchemy::Cell.stub!(:definitions).and_return(cell_descriptions)
+      end
+
+      it "should return string of elements grouped by cell for select_tag helper" do
+        helper.grouped_elements_for_select(@elements).should == helper.grouped_options_for_select({"Foo cell" => [["1", "1#foo_cell"], ["2", "2#foo_cell"]]})
+      end
+
+      context "with empty elements array" do
+        it "should return an empty string" do
+          helper.grouped_elements_for_select([]).should == ""
+        end
+      end
+
+      context "with empty cell definitions" do
+        it "should return an empty string" do
+          @page.stub!(:layout_description).and_return({'name' => "foo"})
+          helper.grouped_elements_for_select(@elements).should == ""
+        end
+      end
+
+    end
+
+    describe "#elements_for_select" do
+
+      context "passing element instances" do
+        let(:element_objects) do
+          [
+            mock_model('Element', name: 'element_1', display_name: 'Element 1'),
+            mock_model('Element', name: 'element_2', display_name: 'Element 2')
+          ]
+        end
+
+        it "should return a String with html option tags" do
+          helper.should_receive(:options_for_select)
+          helper.elements_for_select(element_objects)
+        end
+      end
+
+      context "passing a hash with element descriptions" do
+        let(:element_descriptions) do
+          [{
+            'name' => 'headline',
+            'contents' => []
+          }]
+        end
+
+        subject { helper.elements_for_select(element_descriptions) }
+
+        it "should return a String with html option tags" do
+          helper.should_receive(:options_for_select)
+          subject
+        end
+
+        it "should render the elements display name" do
+          Element.should_receive(:display_name_for).with('headline')
+          subject
+        end
+      end
+
     end
 
   end
-
-  describe "#elements_for_select" do
-    before do
-      @elements = [
-        mock_model('Element', name: 'element_1', display_name: 'Element 1'),
-        mock_model('Element', name: 'element_2', display_name: 'Element 2')
-      ]
-    end
-
-    it "should return a String with html option tags" do
-      helper.should_receive(:options_for_select)
-      helper.elements_for_select(@elements)
-    end
-
-  end
-
 end

spec/models/element_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'spec_helper'
 
 module Alchemy
@@ -458,6 +459,26 @@ module Alchemy
         it "should return an Array of these element definitions found for the given page_layout name" do
           expect(Element.elements_for_layout('my_layout')).to eq([{'name' => 'element_1'}, {'name' => 'element_2'}])
         end
+      end
+    end
+
+    describe '.display_name_for' do
+      it "should return the translation for the given name" do
+        I18n.should_receive(:t).with('subheadline', scope: "element_names", default: 'Subheadline').and_return('Überschrift')
+        expect(Element.display_name_for('subheadline')).to eq('Überschrift')
+      end
+
+      it "should return the humanized name if no translation found" do
+        expect(Element.display_name_for('not_existing_one')).to eq('Not existing one')
+      end
+    end
+
+    describe '#display_name' do
+      let(:element) { Element.new(name: 'article') }
+
+      it "should call .display_name_for" do
+        Element.should_receive(:display_name_for).with(element.name)
+        element.display_name
       end
     end
 