67d6673421c95a58b45edfcde503bb108a90b08c
Thomas von Deyen <tvdeyen@gmail.com>
2013-06-14 17:21:29 +0200

    Refactors the contents converter in 2.2 upgrader module.

--- lib/alchemy/upgrader/two_point_two.rb
+++ lib/alchemy/upgrader/two_point_two.rb
@@ -1,69 +1,114 @@
 module Alchemy
   module Upgrader::TwoPointTwo
 
-  private
-  
-    def convert_essence_texts_displayed_as_select_into_essence_selects
-      desc "Converting EssenceTexts displayed as select into EssenceSelects"
-      contents_found = 0
-      elements = Alchemy::Element.descriptions.select { |e| e['contents'].present? && !e['contents'].detect { |c| c['settings'].present? && c['settings']['display_as'] == 'select' }.nil? }
-      contents = elements.collect { |el| el['contents'] }.flatten.select { |c| c['settings'] && c['settings']['display_as'] == 'select' }.flatten
-      content_names = contents.collect { |c| c['name'] }
-      Alchemy::Content.essence_texts.where(
-        :name => content_names,
-        :alchemy_elements => {:name => elements.collect { |e| e['name'] }}
-      ).joins(:element).each do |content|
-        new_content = Alchemy::Content.new(:name => content.name, :element_id => content.element.id)
-        if new_content.create_essence!('name' => content.name, 'type' => 'EssenceSelect')
-          new_content.essence.value = content.ingredient
-          if new_content.essence.save
-            contents_found += 1
-            log "Converted #{content.name}'s essence_type into EssenceSelect"
-            content.destroy
-          else
-            log "Could not save essence: #{new_content.essence.errors.full_messages.join(', ')}", :error
+    class ContentsConverter
+      include Alchemy::Shell
+
+      def initialize(display_as, essence_type)
+        @contents_found = 0
+        @display_as = display_as
+        @essence_type = essence_type
+      end
+
+      def convert!
+        if essence_texts.any?
+          essence_texts.each do |content|
+            convert_content(content)
           end
+        else
+          log "No EssenceTexts displayed as #{@display_as} found.", :skip
+        end
+      end
+
+    private
+
+      def essence_texts
+        Alchemy::Content.essence_texts.where(
+          :name => content_names,
+          :alchemy_elements => {:name => elements.collect { |e| e['name'] }}
+        ).joins(:element)
+      end
+
+      def content_names
+        @content_names ||= contents.collect { |c| c['name'] }
+      end
+
+      def contents
+        @contents ||= elements.collect { |el|
+          el['contents']
+        }.flatten.select { |c|
+          c['settings'] && c['settings']['display_as'] == @display_as
+        }.flatten
+      end
+
+      def elements
+        @elements ||= Alchemy::Element.descriptions.select { |e|
+          e['contents'].present? && !e['contents'].detect { |c|
+            c['settings'].present? && c['settings']['display_as'] == @display_as
+          }.nil?
+        }
+      end
+
+      def convert_content(content)
+        if @new_content = new_content_from(content)
+          update_content(content)
         else
           log "Could not create content: #{new_content.errors.full_messages.join(', ')}", :error
         end
       end
-      if contents_found > 0
-        todo "Please open your elements.yml file and change all type values from these contents:\n\n#{content_names.join(', ')}\n\ninto EssenceSelect."
-      else
-        log "No EssenceTexts with display_as select setting found.", :skip
+
+      def new_content_from(content)
+        content = Alchemy::Content.create(
+          element_id: content.element.id,
+          name: content.name,
+          essence_type: essence_class
+        )
+        content.essence = essence_class.constantize.create
+        content
       end
+
+      def essence_class
+        "Alchemy::#{@essence_type.classify}"
+      end
+
+      def update_content(content)
+        if update_essence_from(content)
+          @contents_found += 1
+          content.destroy
+          log "Converted #{content.name}'s essence_type into #{@essence_type}"
+        else
+          log "Could not save essence: #{new_content.essence.errors.full_messages.join(', ')}", :error
+        end
+      end
+
+      def update_essence_from(content)
+        @new_content.essence.ingredient = content.ingredient
+        @new_content.essence.save!
+      end
+
+      def display_result
+        if @contents_found > 0
+          todo "Please open your elements.yml file and change all type values from these contents:\n\n#{@content_names.join(', ')}\n\ninto #{@essence_type}."
+        else
+          log "No EssenceTexts with display_as #{@display_as} setting found.", :skip
+        end
+      end
+    end
+
+  private
+
+    def convert_essence_texts_displayed_as_select_into_essence_selects
+      desc "Converting all EssenceTexts displayed as select into EssenceSelects"
+      converter = ContentsConverter.new('select', 'EssenceSelect')
+      converter.convert!
     end
 
     def convert_essence_texts_displayed_as_checkbox_into_essence_booleans
-      desc "Converting EssenceTexts displayed as checkbox into EssenceBooleans"
-      contents_found = 0
-      elements = Alchemy::Element.descriptions.select { |e| e['contents'].present? && !e['contents'].detect { |c| c['settings'].present? && c['settings']['display_as'] == 'checkbox' }.nil? }
-      contents = elements.collect { |el| el['contents'] }.flatten.select { |c| c['settings'] && c['settings']['display_as'] == 'checkbox' }.flatten
-      content_names = contents.collect { |c| c['name'] }
-      Alchemy::Content.essence_texts.where(
-        :name => content_names,
-        :alchemy_elements => {:name => elements.collect { |e| e['name'] }}
-      ).joins(:element).each do |content|
-        new_content = Alchemy::Content.new(:name => content.name, :element_id => content.element.id)
-        if new_content.create_essence!('name' => content.name, 'type' => 'EssenceBoolean')
-          new_content.essence.value = content.ingredient
-          if new_content.essence.save
-            contents_found += 1
-            log "Converted #{content.name}'s essence_type into EssenceBoolean"
-            content.destroy
-          else
-            log "Could not save essence: #{new_content.essence.errors.full_messages.join(', ')}", :error
-          end
-        else
-          log "Could not create content: #{new_content.errors.full_messages.join(', ')}", :error
-        end
-      end
-      if contents_found > 0
-        todo "Please open your elements.yml file and change all type values from these contents:\n\n#{content_names.join(', ')}\n\ninto EssenceBoolean."
-      else
-        log "No EssenceTexts with display_as checkbox setting found.", :skip
-      end
+      desc "Converting all EssenceTexts displayed as checkbox into EssenceBooleans"
+      converter = ContentsConverter.new('checkbox', 'EssenceBoolean')
+      converter.convert!
     end
 
   end
+
 end
