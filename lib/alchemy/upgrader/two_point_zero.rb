a58cbd8a6af31d9a425003b2f5d2e27376688a2a
Thomas von Deyen <tvdeyen@gmail.com>
2013-06-14 17:20:58 +0200

    Make 2.0 upgrader module database agnostic.

--- lib/alchemy/upgrader/two_point_zero.rb
+++ lib/alchemy/upgrader/two_point_zero.rb
@@ -2,11 +2,11 @@ module Alchemy
   module Upgrader::TwoPointZero
 
   private
-    
+
     def strip_alchemy_from_schema_version_table
-      database_yml = YAML.load_file(Rails.root.join("config", "database.yml"))
-      connection = Mysql2::Client.new(database_yml.fetch(Rails.env.to_s).symbolize_keys)
-      connection.query "UPDATE schema_migrations SET `schema_migrations`.`version` = REPLACE(`schema_migrations`.`version`,'-alchemy','')"
+      ActiveRecord::Base.connection.execute(
+        "UPDATE schema_migrations SET version = REPLACE(version,'-alchemy','')"
+      )
     end
 
   end
