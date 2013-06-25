1150a7c3786e4ebb3fa099bb96ab64f0f4808a8a
Robin Böning <rb@magiclabs.de>
2013-06-25 18:04:12 +0200

    Refactors Config class.
    Its behavior is still the same.

--- lib/alchemy/config.rb
+++ lib/alchemy/config.rb
@@ -1,51 +1,64 @@
+# encoding: utf-8
+
 module Alchemy
   class Config
 
     class << self
 
-      # Returns the configuration for given parameter name from +config/alchemy/config.yml+ file.
+      # Returns the configuration for given parameter name.
+      #
+      # @param name [String]
+      #
       def get(name)
         show[name.to_s]
       end
-
       alias_method :parameter, :get
 
-      # Returns the complete configuration from +config/alchemy/config.yml+ file.
+      # Returns a merged configuration of the following files
+      # 
+      # Alchemy´s default config: +gems/../alchemy_cms/config/alchemy/config.yml+
+      # Your apps default config: +your_app/config/alchemy/config.yml+
+      # Environment specific config: +your_app/config/alchemy/development.config.yml+
+      #
+      # An environment specific config overwrites the settings of your apps default config, 
+      # while your apps default config has precedence over Alchemy´s default config.
+      #
       def show
-        @@config ||= read_files
+        @config ||= merge_configs!(alchemy_config, main_app_config, env_specific_config)
       end
 
-      private
+    private
 
-      def read_files
-        # Looking for any Rails Environment specific configuration
-        if File.exists? "#{Rails.root.to_s}/config/alchemy/config_#{Rails.env}.yml"
-          config_1 = YAML.load_file("#{Rails.root.to_s}/config/alchemy/config_#{Rails.env}.yml")
-        else
-          config_1 = {}
-        end
+      # Alchemy default configuration
+      def alchemy_config
+        read_file(File.join(File.dirname(__FILE__), '..', '..', 'config/alchemy/config.yml'))
+      end
 
-        # Looking for Application specific configuration
-        if File.exists? "#{Rails.root.to_s}/config/alchemy/config.yml"
-          config_2 = YAML.load_file("#{Rails.root.to_s}/config/alchemy/config.yml")
-        else
-          config_2 = {}
-        end
+      # Application specific configuration
+      def main_app_config
+        read_file("#{Rails.root}/config/alchemy/config.yml")
+      end
 
-        # Reading Alchemy standard configuration
-        if File.exists? File.join(File.dirname(__FILE__), '..', '..', 'config/alchemy/config.yml')
-          config_3 = YAML.load_file(File.join(File.dirname(__FILE__), '..', '..', 'config/alchemy/config.yml'))
-        else
-          config_3 = {}
-        end
+      # Rails Environment specific configuration
+      def env_specific_config
+        read_file("#{Rails.root}/config/alchemy/config_#{Rails.env}.yml")
+      end
 
-        # Mergin all together
-        if config_1.blank? && config_3.blank? && config_3.blank?
-          raise LoadError, 'No Alchemy config file found!'
-        else
-          [config_1, config_2, config_3].map(&:stringify_keys!)
-          return config_3.merge(config_2.merge(config_1))
-        end
+      # Tries to load yaml file from given path.
+      # If it does not exist, or its empty, it returns an empty Hash.
+      #
+      def read_file(file)
+        return YAML.load_file(file) || {} if File.exists?(file) # YAML.load_file returns false if file is empty.
+        {}
+      end
+
+      # Merges all given configs together
+      # 
+      def merge_configs!(*config_files)
+        raise LoadError, 'No Alchemy config file found!' if config_files.map(&:blank?).all?
+        config = {}
+        config_files.each {|h| config.merge!(h.stringify_keys!) }
+        config
       end
 
     end
