a90fbbe63c24854e31c1f562a5c75db9e82e6b5a
Thomas von Deyen <tvdeyen@gmail.com>
2013-07-12 15:07:35 +0200

    Fix element model spec.

--- spec/models/element_spec.rb
+++ spec/models/element_spec.rb
@@ -129,7 +129,7 @@ module Alchemy
       let(:page) { FactoryGirl.build_stubbed(:public_page) }
       let(:clipboard) { [{id: element_1.id}, {id: element_2.id}] }
       before {
-        Element.stub(:where).and_return([element_1, element_2])
+        Element.stub(:all_from_clipboard).and_return([element_1, element_2])
       }
 
       it "return all elements from clipboard that could be placed on page" do
