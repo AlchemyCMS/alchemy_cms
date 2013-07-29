8402cd4abcb735eb4dbc196c8fbe36264a63a1e1
Robin Böning <rb@magiclabs.de>
2013-07-29 22:40:25 +0200

    Scopes last edited pages in admin/dashboard to current site. Closes #392

--- app/controllers/alchemy/admin/dashboard_controller.rb
+++ app/controllers/alchemy/admin/dashboard_controller.rb
@@ -5,7 +5,7 @@ module Alchemy
     class DashboardController < Alchemy::Admin::BaseController
 
       def index
-        @last_edited_pages = Page.all_last_edited_from(current_user)
+        @last_edited_pages = Page.from_current_site.all_last_edited_from(current_user)
         @locked_pages = Page.from_current_site.all_locked
         @online_users = User.logged_in.to_a - [current_user]
         @first_time = current_user.sign_in_count == 1 && current_user.last_sign_in_at.nil?
