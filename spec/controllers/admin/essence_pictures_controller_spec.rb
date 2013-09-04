4dfcf6810213f5a017c7d1087111d58a42953bba
Thomas von Deyen <tvdeyen@gmail.com>
2013-09-04 22:48:36 +0200

    Refactors Admin::EssencePicturesController

--- spec/controllers/admin/essence_pictures_controller_spec.rb
+++ spec/controllers/admin/essence_pictures_controller_spec.rb
@@ -1,12 +1,8 @@
-
 require 'spec_helper'
 
 module Alchemy
   describe Admin::EssencePicturesController do
-
-    before do
-      sign_in(admin_user)
-    end
+    before { sign_in(admin_user) }
 
     let(:essence) { EssencePicture.new }
     let(:content) { Content.new }
@@ -14,8 +10,8 @@ module Alchemy
 
     describe '#edit' do
       before do
-        EssencePicture.stub!(:find).and_return(essence)
-        Content.stub!(:find).and_return(content)
+        EssencePicture.should_receive(:find).and_return(essence)
+        Content.should_receive(:find).and_return(content)
       end
 
       it 'should assign @essence_picture and @content instance variables' do
@@ -24,16 +20,139 @@ module Alchemy
         expect(assigns(:content)).to be_a(Content)
       end
     end
-    
+
+    describe '#crop' do
+      before do
+        EssencePicture.should_receive(:find).and_return(essence)
+      end
+
+      context 'with no picture assigned' do
+        before { essence.should_receive(:picture).and_return(nil) }
+
+        it "renders error message" do
+          get :crop, id: 1
+          expect(assigns(:no_image_notice)).to eq(Alchemy::I18n.t(:no_image_for_cropper_found))
+        end
+      end
+
+      context 'with picture assigned' do
+        let(:default_mask) { {'x1' => '0', 'y1' => '0', 'x2' => '100', 'y2' => '100'} }
+
+        before do
+          picture.image_file_width = 300
+          picture.image_file_height = 250
+          essence.should_receive(:picture).and_return(picture)
+        end
+
+        context 'with no render_size present in essence' do
+          before do
+            essence.should_receive(:render_size).and_return(nil)
+          end
+
+          context 'with sizes in params' do
+            it "sets sizes to given values" do
+              get :crop, id: 1, options: {image_size: '300x250'}
+              expect(assigns(:size_x)).to eq('300')
+              expect(assigns(:size_y)).to eq('250')
+            end
+          end
+
+          context 'with no sizes in params' do
+            it "sets sizes to zero" do
+              get :crop, id: 1
+              expect(assigns(:size_x)).to eq(0)
+              expect(assigns(:size_y)).to eq(0)
+            end
+          end
+        end
+
+        context 'with render_size present in essence' do
+          before do
+            essence.stub(:render_size).and_return('30x25')
+          end
+
+          it "sets sizes from these values" do
+            get :crop, id: 1
+            expect(assigns(:size_x)).to eq('30')
+            expect(assigns(:size_y)).to eq('25')
+          end
+        end
+
+        context 'no crop sizes present in essence' do
+          before do
+            essence.stub(:crop_from).and_return(nil)
+            essence.stub(:crop_size).and_return(nil)
+            picture.should_receive(:default_mask).and_return(default_mask)
+          end
+
+          it "assigns default mask boxes" do
+            get :crop, id: 1
+            expect(assigns(:initial_box)).to eq(default_mask)
+            expect(assigns(:default_box)).to eq(default_mask)
+          end
+        end
+
+        context 'crop sizes present in essence' do
+          let(:mask) { {'x1' => '0', 'y1' => '0', 'x2' => '120', 'y2' => '160'} }
+
+          before do
+            essence.stub(:crop_from).and_return('0x0')
+            essence.stub(:crop_size).and_return('120x160')
+            picture.should_receive(:default_mask).and_return(default_mask)
+          end
+
+          it "assigns cropping boxes" do
+            essence.should_receive(:cropping_mask).and_return(mask)
+            get :crop, id: 1
+            expect(assigns(:initial_box)).to eq(mask)
+            expect(assigns(:default_box)).to eq(default_mask)
+          end
+        end
+
+        context 'with fixed_ratio set to false' do
+          it "sets ratio to false" do
+            get :crop, id: 1, options: {fixed_ratio: false}
+            expect(assigns(:ratio)).to eq(false)
+          end
+        end
+
+        context 'with no fixed_ratio set in params' do
+          it "sets a fixed ratio from sizes" do
+            get :crop, id: 1, options: {image_size: '80x60'}
+            expect(assigns(:ratio)).to eq(80.0/60.0)
+          end
+        end
+
+        context 'with size_y set to zero' do
+          it "sets ratio to 1" do
+            get :crop, id: 1
+            expect(assigns(:ratio)).to eq(1)
+          end
+        end
+      end
+    end
+
+    describe '#update' do
+      before do
+        EssencePicture.should_receive(:find).and_return(essence)
+        Content.should_receive(:find).and_return(content)
+      end
+
+      it "updates the essence attributes" do
+        essence.should_receive(:update_attributes).and_return(true)
+        xhr :put, :update, id: 1, essence_picture: {render_size: '1x1'}
+      end
+    end
+
     describe '#assign' do
       before do
-        Content.stub!(:find_by_id).and_return(content)
+        Content.should_receive(:find).and_return(content)
         Content.any_instance.stub(:essence).and_return(essence)
-        Picture.stub!(:find_by_id).and_return(picture)
+        Picture.should_receive(:find_by_id).and_return(picture)
       end
 
       it "should assign a Picture" do
-        put :assign, id: '1', picture_id: '1', format: 'js'
+        xhr :put, :assign, content_id: '1', picture_id: '1'
         expect(assigns(:content).essence.picture).to eq(picture)
       end
     end
