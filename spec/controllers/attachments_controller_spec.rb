f444f7e558b33d32a52e9e5729971ac0a5e968ff
Robin Böning <rb@magiclabs.de>
2013-09-09 18:33:37 +0200

    Throw 404 if attachment can not be found.

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -15,6 +15,10 @@ module Alchemy
       response.should redirect_to(login_path)
     end
 
+    it "should raise ActiveRecord::RecordNotFound for requesting not existing attachments" do
+      expect { get :download, id: 0 }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
     it "should not be possible to see attachments from restricted pages" do
       get :show, :id => attachment.id
       response.status.should == 302
