4042190c5621251e4a10102e9f642c463b80f14b
Robin Böning <rb@magiclabs.de>
2013-11-08 08:08:52 +0100

    Includes admin base_helper to attachments_helper and adds specs.
    The BaseHelper is now included in the AttachmentsHelper because the used _t method is defined in the BaseHelper.

--- app/helpers/alchemy/admin/attachments_helper.rb
+++ app/helpers/alchemy/admin/attachments_helper.rb
@@ -1,6 +1,7 @@
 module Alchemy
   module Admin
     module AttachmentsHelper
+      include Alchemy::Admin::BaseHelper
 
       def mime_to_human(mime)
         I18n.t(mime, scope: 'mime_types', default: _t(:document))
