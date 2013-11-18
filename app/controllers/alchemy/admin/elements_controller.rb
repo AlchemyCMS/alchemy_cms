414486b04f9914fff4cf096aef85b3bff51bd42b
Thomas von Deyen <tvdeyen@gmail.com>
2013-11-18 22:41:23 +0100

    Re-attach folding events on restored elements. Closes #411

--- app/controllers/alchemy/admin/elements_controller.rb
+++ app/controllers/alchemy/admin/elements_controller.rb
@@ -82,12 +82,14 @@ module Alchemy
       end
 
       def order
+        @trashed_elements = []
         params[:element_ids].each do |element_id|
           element = Element.find(element_id)
           if element.trashed?
             element.page_id = params[:page_id]
             element.cell_id = params[:cell_id]
             element.insert_at
+            @trashed_elements << element
           end
           element.move_to_bottom
         end
