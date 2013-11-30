3bda1d07b06f8555dd794d3f49176a912c02f778
Thomas von Deyen <tvdeyen@gmail.com>
2013-11-30 23:04:51 +0100

    Removes the shorten_to option from essence_richtext_view.

--- spec/views/essences/essence_richtext_view_spec.rb
+++ spec/views/essences/essence_richtext_view_spec.rb
@@ -16,31 +16,5 @@ describe 'alchemy/essences/_essence_richtext_view' do
       expect(rendered).to have_content('Lorem ipsum dolor sit amet consectetur adipiscing elit.')
       expect(rendered).to_not have_selector('h1')
     end
-
-    context 'and with options[:shorten_to] set to 5' do
-      it "renders the text body shortened to 5 words" do
-        render content, content: content, options: {plain_text: true, shorten_to: 5}
-        expect(rendered).to have_content('Lorem ipsum dolor sit amet')
-        expect(rendered).to_not have_content('consectetur adipiscing elit')
-        expect(rendered).to_not have_selector('h1')
-      end
-    end
   end
-
-  context 'with options[:shorten_to] set to 5' do
-    it "renders the html body shortened to 5 words" do
-      render content, content: content, options: {shorten_to: 5}
-      expect(rendered).to have_content('Lorem ipsum dolor sit amet')
-      expect(rendered).to_not have_content('consectetur adipiscing elit')
-      expect(rendered).to have_selector('h1')
-    end
-
-    context 'and with options[:shorten_suffix] set to >' do
-      it "renders the html body shortened to 5 words" do
-        render content, content: content, options: {shorten_to: 5, shorten_suffix: '>'}
-        expect(rendered).to have_content('Lorem ipsum dolor sit amet>')
-      end
-    end
-  end
-
 end
