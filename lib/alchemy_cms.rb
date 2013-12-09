60ed9a9f3088dad36eeb861c9288875310546008
Thomas von Deyen <tvdeyen@gmail.com>
2013-12-10 00:20:32 +0100

    Do not raise if Rails is not 4.0.0
    Bundler already handles this kind of stuff.

--- lib/alchemy_cms.rb
+++ lib/alchemy_cms.rb
@@ -1,11 +1,5 @@
-require 'rails'
-
 # Instantiate the global Alchemy namespace
 module Alchemy
 end
 
-if Rails::VERSION::MAJOR == 4 && Rails::VERSION::MINOR == 0
-  require 'alchemy/engine'
-else
-  raise "Alchemy #{Alchemy::VERSION} needs Rails 4.0 or higher. You are currently using Rails #{Rails::VERSION::STRING}"
-end
+require 'alchemy/engine'
