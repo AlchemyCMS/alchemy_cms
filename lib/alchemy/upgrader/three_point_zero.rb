3760bb95652bd08428ff5b79f9df085782b08a10
Thomas von Deyen <tvdeyen@gmail.com>
2014-01-29 23:09:02 +0100

    Adds an upgrader task to rename the `registered` user role to `member`.

--- lib/alchemy/upgrader/three_point_zero.rb
+++ lib/alchemy/upgrader/three_point_zero.rb
@@ -11,12 +11,26 @@ add the `alchemy-devise` gem to your Gemfile.
 
 In order to provide your own user model,
 you have to be sure to met the API requirements
-mentioned in the dummy user model:
+mentioned in:
 
-  app/models/alchemy/dummy_user.rb
+  lib/alchemy/auth_accessors.rb
 
 NOTE
       todo notice
+    end
+
+    def rename_registered_role_ro_member
+      desc 'Rename the `registered` user role to `member`'
+      registered_users = Alchemy.user_class.where("alchemy_roles LIKE '%registered%'")
+      if registered_users.any?
+        registered_users.each do |user|
+          roles = user.read_attribute(:alchemy_roles).sub(/registered/, 'member')
+          user.update_column(:alchemy_roles, roles)
+          log "Renamed #{user.inspect} role to `member`"
+        end
+      else
+        log 'No users with `registered` role found.', :skip
+      end
     end
 
   end
