219fc25fc492acec74a5e467820f19e4c3acf9fd
Thomas von Deyen <tvdeyen@gmail.com>
2014-02-06 23:33:22 +0100

    Do not fail if javascript errors happen while testing with phantomjs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,7 +40,14 @@ require_relative "support/hint_examples.rb"
 if RUBY_PLATFORM =~ /darwin/
   require_relative "support/phantomjs_mavericks_fix.rb"
   Capybara.register_driver :poltergeist do |app|
-    Capybara::Poltergeist::Driver.new(app, phantomjs_logger: Alchemy::WarningSuppressor)
+    Capybara::Poltergeist::Driver.new(app, {
+      phantomjs_logger: Alchemy::WarningSuppressor,
+      js_errors: false
+    })
+  end
+else
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, js_errors: false)
   end
 end
 
@@ -48,7 +55,7 @@ end
 Capybara.default_driver = :rack_test
 Capybara.default_selector = :css
 Capybara.register_driver(:rack_test_translated_header) do |app|
-  Capybara::RackTest::Driver.new(app, :headers => { 'HTTP_ACCEPT_LANGUAGE' => 'de' })
+  Capybara::RackTest::Driver.new(app, headers: { 'HTTP_ACCEPT_LANGUAGE' => 'de' })
 end
 Capybara.javascript_driver = :poltergeist
 Capybara.ignore_hidden_elements = false
@@ -59,8 +66,8 @@ RSpec.configure do |config|
   config.filter_run :focus
   config.include Alchemy::Engine.routes.url_helpers
   config.include Alchemy::TestSupport::AuthHelpers
-  config.include Alchemy::TestSupport::ControllerRequests, :type => :controller
-  config.include Alchemy::TestSupport::IntegrationHelpers, :type => :feature
+  config.include Alchemy::TestSupport::ControllerRequests, type: :controller
+  config.include Alchemy::TestSupport::IntegrationHelpers, type: :feature
   config.include FactoryGirl::Syntax::Methods
 
   config.use_transactional_fixtures = true
