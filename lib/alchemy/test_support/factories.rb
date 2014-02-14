a21db27294a1beb5d8ce45599b3a092a8426260e
Robin Böning <rb@magiclabs.de>
2014-02-14 11:58:49 +0100

    Fixes attachment controller spec.

--- lib/alchemy/test_support/factories.rb
+++ lib/alchemy/test_support/factories.rb
@@ -98,4 +98,10 @@ FactoryGirl.define do
     name 'A Site'
     host 'domain.com'
   end
+
+  factory :attachment, :class => 'Alchemy::Attachment' do
+    file File.new(File.expand_path('../../../../spec/fixtures/image.png', __FILE__))
+    name 'image'
+    file_name 'image.png'
+  end
 end
