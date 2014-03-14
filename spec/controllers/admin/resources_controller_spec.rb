6f693731d7ee2822695d272b23bd58c88eaec601
Thomas von Deyen <tvdeyen@gmail.com>
2014-03-14 21:02:53 +0100

    Compare resources search query to lowercase column value. Closes #493

--- spec/controllers/admin/resources_controller_spec.rb
+++ spec/controllers/admin/resources_controller_spec.rb
@@ -1,9 +1,37 @@
 require "spec_helper"
 
-module Alchemy
-  describe Admin::ResourcesController do
-    it "should include ResourcesHelper" do
-      controller.respond_to?(:resource_window_size).should be_true
+class EventsController < Alchemy::Admin::ResourcesController
+end
+
+describe EventsController do
+  it "should include ResourcesHelper" do
+    controller.respond_to?(:resource_window_size).should be_true
+  end
+
+  describe '#index' do
+    let(:params) { Hash.new }
+    let(:peter)  { Event.create(name: 'Peter') }
+    let(:lustig) { Event.create(name: 'Lustig') }
+
+    before do
+      sign_in(admin_user)
+      peter; lustig
+    end
+
+    it "returns all records" do
+      get :index, params
+      assigns(:events).should include(peter)
+      assigns(:events).should include(lustig)
+    end
+
+    context 'with search query given' do
+      let(:params) { {query: 'PeTer'} }
+
+      it "returns only matching records" do
+        get :index, params
+        assigns(:events).should include(peter)
+        assigns(:events).should_not include(lustig)
+      end
     end
   end
 end
