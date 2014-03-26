951ee5bfde2c5da498d7d589b29beb6fca49cfbb
Thomas von Deyen <tvdeyen@gmail.com>
2014-03-26 17:07:27 +0100

    Fixes page etag calculation.
    This page etag should always be calculated dependent from current user (if any), not only if the user can visit Alchemy backend. This fixes issues with apps having own user models.

--- app/controllers/alchemy/pages_controller.rb
+++ app/controllers/alchemy/pages_controller.rb
@@ -14,12 +14,9 @@ module Alchemy
     authorize_resource only: 'show'
 
     # Showing page from params[:urlname]
-    # @page is fetched via before filter
-    # @root_page is fetched via before filter
-    # @language fetched via before_filter in alchemy_controller
-    # querying for search results if any query is present via before_filter
+    #
     def show
-      if !cache_page? || stale?(etag: page_etag, last_modified: @page.published_at, public: !@page.restricted)
+      if render_fresh_page?
         respond_to do |format|
           format.html { render layout: !request.xhr? }
           format.rss do
@@ -145,7 +142,7 @@ module Alchemy
       legacy_urls.last
     end
 
-    # Returns true if the page should be cached.
+    # Returns true if the page cache control headers should be set.
     #
     # == Disable Alchemy's page caching globally
     #
@@ -159,26 +156,37 @@ module Alchemy
     #     - name: contact
     #       cache: false
     #
-    # NOTE: Pages that are marked as searchresults are also not cached.
+    # Note: This only sets the cache control headers and skips rendering of the page body, if the cache is fresh.
+    # This does not disable the fragment caching in the views. So if you don't want a page and it's elements to be cached,
+    # then be sure to not use <% cache element %> in the views.
     #
     # @returns Boolean
+    #
     def cache_page?
       return false unless @page && Alchemy::Config.get(:cache_pages)
-      pagelayout = PageLayout.get(@page.page_layout)
-      pagelayout['cache'] != false && pagelayout['searchresults'] != true
+      page_layout = PageLayout.get(@page.page_layout)
+      page_layout['cache'] != false && page_layout['searchresults'] != true
     end
 
     # Returns the etag used for response headers.
     #
-    # If a user is logged in with sufficient rights to see the alchemy menu bar,
-    # we use another etag to prevent caching the menu bar.
+    # If a user is logged in, we append theirs etag to prevent caching of user related content.
+    #
+    # IMPORTANT: If your user does not have a +cache_key+ method (i.e. it's not an ActiveRecord model),
+    # you have to ensure to implement it and return a unique identifier for that particular user.
+    # Otherwise all users will see the same cached page, regardless of user's state.
     #
     def page_etag
-      if can?(:edit_content, @page)
-        @page.cache_key + current_alchemy_user.try(:cache_key)
-      else
-        @page.cache_key
-      end
+      @page.cache_key + current_alchemy_user.try(:cache_key).to_s
+    end
+
+    # We only render the page if either the cache is disabled for this page
+    # or the cache is stale, because it's been republished by the user.
+    #
+    def render_fresh_page?
+      !cache_page? || stale?(etag: page_etag,
+        last_modified: @page.published_at,
+        public: !@page.restricted)
     end
 
   end
