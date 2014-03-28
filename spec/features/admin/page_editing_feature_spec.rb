324a44c05eb8c9d64e73bc90f52c24aee93f11aa
Thomas von Deyen <tvdeyen@gmail.com>
2014-03-28 23:10:02 +0100

    Fixes page editing feature specs.
    They did not tested the real features. Now they do.

--- spec/features/admin/page_editing_feature_spec.rb
+++ spec/features/admin/page_editing_feature_spec.rb
@@ -1,15 +1,20 @@
 require 'spec_helper'
 
-describe 'Page editing feature', js: true do
-  let(:a_page) { FactoryGirl.create(:page) }
+describe 'Page editing feature' do
+  let(:a_page) { FactoryGirl.create(:public_page, visible: true) }
 
   before { authorize_as_admin }
 
-  context "in preview mode" do
+  context "in preview frame" do
     it "the menubar does not render on the page" do
-      visit alchemy.edit_admin_page_path(a_page)
-      within_frame('alchemy_preview_window') do
-        a_page.should_not have_selector('#alchemy_menubar')
+      visit alchemy.admin_page_path(a_page)
+      page.should_not have_selector('#alchemy_menubar')
+    end
+
+    it "navigation links are not clickable" do
+      visit alchemy.admin_page_path(a_page)
+      within('#navigation') do
+        page.should have_selector('a[href="javascript: void(0)"]')
       end
     end
   end
