5ae44fd0b56bed034810df8b253304094f7849fc
Robin Böning <rb@magiclabs.de>
2014-05-22 18:17:38 +0200

    Moves create_element! method out of before block.

--- spec/features/admin/element_trash_feature_spec.rb
+++ spec/features/admin/element_trash_feature_spec.rb
@@ -6,11 +6,11 @@ describe "element trash feature", js: true do
   before do
     authorize_as_admin
     visit edit_admin_page_path(a_page)
-    create_element! # We want to trash an article element later because it contains a richtext essence
-    expect(page).to have_no_selector(".spinner") # wait until spinner disappears
   end
 
   it "removes an element from the list" do
+    create_element! # We want to trash an article element later because it contains a richtext essence
+    expect(page).to have_no_selector(".spinner") # wait until spinner disappears
     expect(page).to have_selector(".element_editor", count: 1)
     within ".alchemy-elements-window .element_editor:first" do
       click_link Alchemy::I18n.t("trash element")
