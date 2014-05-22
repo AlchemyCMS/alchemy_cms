ea5eb1d4823750a30c227d27ce6f91ec74332918
Robin Böning <rb@magiclabs.de>
2014-05-22 19:59:30 +0200

    Comments some lines to check which break the tests.

--- spec/features/admin/element_trash_feature_spec.rb
+++ spec/features/admin/element_trash_feature_spec.rb
@@ -12,10 +12,10 @@ describe "element trash feature", js: true do
     create_element! # We want to trash an article element later because it contains a richtext essence
     expect(page).to have_no_selector(".spinner") # wait until spinner disappears
     expect(page).to have_selector(".element_editor", count: 1)
-    within ".alchemy-elements-window .element_editor:first" do
-      click_link Alchemy::I18n.t("trash element")
-    end
-    expect(page).to have_content Alchemy::I18n.t('Element trashed')
-    expect(page).to have_no_selector(".element_editor")
+    # within ".alchemy-elements-window .element_editor:first" do
+    #   click_link Alchemy::I18n.t("trash element")
+    # end
+    # expect(page).to have_content Alchemy::I18n.t('Element trashed')
+    # expect(page).to have_no_selector(".element_editor")
   end
 end
