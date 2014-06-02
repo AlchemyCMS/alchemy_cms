2ca6752021826b503f4a00b90a698bfdbac6889a
Jason York <github@yorkspace.com>
2014-06-02 13:58:43 -0500

    Remove Alchemy::Page's dependency to have the user extend ActiveModel

--- spec/dummy/app/models/dummy_user.rb
+++ spec/dummy/app/models/dummy_user.rb
@@ -1,7 +1,17 @@
-class DummyUser < ActiveRecord::Base
-  attr_accessor :alchemy_roles
+class DummyUser
+  include ActiveModel::Validations
+
+  attr_accessor :alchemy_roles, :language, :cache_key, :email, :password, :name, :id
 
   def self.logged_in
     []
+  end
+
+  def self.stamper_class_name
+    :DummyUser
+  end
+
+  def update_attributes(attributes)
+    attributes.each { |key,value| send("#{key}=".to_sym, value) }
   end
 end
