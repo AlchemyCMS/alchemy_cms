4f0a6cf6f9580d288b11262136007bf7ff1e80ad
Thomas von Deyen <tvdeyen@gmail.com>
2014-06-03 23:33:20 +0200

    Make DummyUser class inherit from ActiveRecord again.
    This reverts changes from 7b1e66940ff744535469f0bbeb563adadfa68f13

--- spec/dummy/app/models/dummy_user.rb
+++ spec/dummy/app/models/dummy_user.rb
@@ -1,17 +1,7 @@
-class DummyUser
-  include ActiveModel::Validations
-
-  attr_accessor :alchemy_roles, :language, :cache_key, :email, :password, :name, :id
+class DummyUser < ActiveRecord::Base
+  attr_accessor :alchemy_roles, :name
 
   def self.logged_in
     []
-  end
-
-  def self.stamper_class_name
-    :DummyUser
-  end
-
-  def update_attributes(attributes)
-    attributes.each { |key,value| send("#{key}=".to_sym, value) }
   end
 end
