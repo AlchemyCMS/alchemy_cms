4a16ada822d70ced03932d04d7bd10c45083e629
Martin Meyerhoff <mamhoff@googlemail.com>
2014-06-26 15:37:33 +0200

    Fix bugs on Image::Transformations
    This commit tries to fix the issues raised by Thomas. One by one:
    
    1. Refactoring app/views/alchemy/admin/essence_pictures/crop.html.erb
    - I didn't really put everything into a method, but rather used Ruby's to_json method.
    - @size_x and @size_y get merged into a @min_size parameter, because that's what it really is.
    
    2. Added lots of comments
    
    3. Moved all methods that aren't directly accessed from outside to private methods
    
    4. Added some tests for complex cropping behaviour
    
    5. Changed behaviour for the fixed_ratio option when cropping.
    Before, fixed_ratio would be "true" and then set the ratio to 1. Now, one fixed_ratio
    takes a number (width/height) and can be arbitrarily set. This makes sense when used in
    conjunction with the "x20"/"20x" syntax for image size.
    It only affects the image cropper. There is no support for ratio when rendering directly.
    
    6. Thumbnails of cropped images without mask now show the correct part of the image.
    
    7. Instead of raising an error when generating the default mask, the crop button is
    disabled when trying to crop an image that cannot be cropped.
    
    8. Changed "-resize" to Imagemagick option -thumbnail because it's
    [faster than -resize](http://www.imagemagick.org/script/command-line-options.php#thumbnail)
    
    9. Added upscale parameter to xy_crop_resize
    
    10. Use @image instead of @picture.image_file in the picture_controller.rb

--- app/models/alchemy/picture/transformations.rb
+++ app/models/alchemy/picture/transformations.rb
@@ -1,23 +1,19 @@
 module Alchemy
+
+  # This concern can extend classes that expose image_file, image_file_width and image_file_height.
+  # It provides methods for cropping and resizing.
+  #
   module Picture::Transformations
     extend ActiveSupport::Concern
-
-    #
-    # This concern can extend classes that expose image_file, image_file_width and image_file_height.
-    # It provides methods for cropping and resizing.
-    #
 
     # Returns the default centered image mask for a given size.
     # If the mask is bigger than the image, the mask is scaled down
     # so the largest possible part of the image is visible.
-    def default_mask(size = "0x0", upsample = false)
-      raise "No size given" if size.blank?
-
-      mask = sizes_from_string(size)
-
-      if is_smaller_than(mask) && !upsample
-        raise "This image is too small to crop"
-      end
+    #
+    def default_mask(mask_arg)
+      mask = mask_arg.dup
+      mask[:width] = image_file_width if mask[:width].zero?
+      mask[:height] = image_file_height if mask[:height].zero?
 
       crop_size = size_when_filling(mask)
       top_left = get_top_left_crop_corner(crop_size)
@@ -25,12 +21,14 @@ module Alchemy
       point_and_mask_to_points(top_left, crop_size)
     end
 
-
     # Returns a size value String for the thumbnail used in essence picture editors.
     #
-    def thumbnail_size(size_of_thumb = "111x93")
-      dimensions_of_thumb = sizes_from_string(size_of_thumb)
-      thumbnail_size = size_when_fitting(dimensions_of_thumb)
+    def thumbnail_size(size_string = "0x0", size_of_thumb = { width: 111, height: 93 })
+      size = sizes_from_string(size_string)
+      size[:width] = get_base_dimensions[:width] if size[:width].zero?
+      size[:height] = get_base_dimensions[:height] if size[:height].zero?
+
+      thumbnail_size = size_when_fitting(size, size_of_thumb)
 
       "#{thumbnail_size[:width]}x#{thumbnail_size[:height]}"
     end
@@ -41,49 +39,37 @@ module Alchemy
     def crop(size, crop_from = nil, crop_size = nil, upsample = false)
       raise "No size given!" if size.empty?
       render_to = sizes_from_string(size)
-      begin
+      if crop_from && crop_size
         top_left = point_from_string(crop_from)
         crop_dimensions = sizes_from_string(crop_size)
-        xy_crop_resize(render_to, top_left, crop_dimensions)
-      rescue
+        xy_crop_resize(render_to, top_left, crop_dimensions, upsample)
+      else
         center_crop(render_to, upsample)
       end
     end
 
-    # Given a string with an x, this function return a Hash with key :x and :y
+    # Returns the rendered resized image using imagemagick directly.
     #
-    def point_from_string(string = "0x0")
-      string = "0x0" if string.empty?
-      raise ArgumentError if !string.match(/(\d*x)|(x\d*)/)
-
-      x, y = string.scan(/(\d*)x(\d*)/)[0].map(&:to_i)
-
-      x = 0 if x.nil?
-      y = 0 if y.nil?
-      {
-        x: x,
-        y: y
-      }
+    def resize(size, upsample = false)
+      self.image_file.thumb(upsample ? size : "#{size}>")
     end
 
     # Given a string with an x, this function returns a Hash with point
-    # :width and :height
+    # :width and :height.
+    #
     def sizes_from_string(string = "0x0")
-      string = "0x0" if string.empty?
-      raise ArgumentError if !string.match(/(\d*x)|(x\d*)/)
+      string = "0x0" if string.nil? || string.empty?
+
+      raise ArgumentError unless string.match(/(\d*x\d*)/)
 
       width, height = string.scan(/(\d*)x(\d*)/)[0].map(&:to_i)
 
-      width = image_file_width if width.zero? || width.nil?
-      height = image_file_height if height.zero? || height.nil?
+      width = 0 if width.nil?
+      height = 0 if height.nil?
       {
         width: width,
         height: height
       }
-    end
-
-    def resize(size, upsample = false)
-      self.image_file.thumb(upsample ? size : "#{size}>")
     end
 
     # Returns true if picture's width is greater than it's height
@@ -107,107 +93,165 @@ module Alchemy
     end
     alias_method :square?, :square_format?
 
-
-    # Given dimensions for a possibly destructive crop operation,
-    # this function returns the top left corner as a Hash
-    # with keys :x, :y
-    #
-    def get_top_left_crop_corner(dimensions)
-      {
-        x: (image_file_width - dimensions[:width]) / 2,
-        y: (image_file_height - dimensions[:height]) / 2
-      }
-    end
-
-    def get_original_size
-      if self.respond_to?(:crop_size) && !self.crop_size.nil?
-        sizes_from_string(crop_size)
-      else
-        sizes_from_image_file
-      end
-    end
-
-    # Given dimensions with :width, :height
-    # this function returns dimensions which the image can fill.
-    #
-    def size_when_filling(dimensions)
-      zoom_x = dimensions[:width].to_f / image_file_width
-      zoom_y = dimensions[:height].to_f / image_file_height
-
-      zoom = zoom_x > zoom_y ? zoom_x : zoom_y
-      {
-        width: (dimensions[:width] / zoom).round.to_i,
-        height: (dimensions[:height] / zoom).round.to_i
-      }
-    end
-
-    # Given dimensions with :width, :height
-    # this function returns a dimensions in which the image can be fitted.
-    #
-    def size_when_fitting(dimensions)
-      original_size = get_original_size
-      zoom_x = dimensions[:width].to_f / original_size[:width]
-      zoom_y = dimensions[:height].to_f / original_size[:height]
-
-      zoom = zoom_x < zoom_y ? zoom_x : zoom_y
-      {
-        width: (original_size[:width] * zoom).round.to_i,
-        height: (original_size[:height] * zoom).round.to_i
-      }
-    end
-
-    # Given a point as a Hash with :x and :y, and a mask with
-    # :width and :height, this function returns the area on the
-    # underlying canvas as a Hash of two points
-    #
-    def point_and_mask_to_points(point, mask)
-      {
-        x1: point[:x],
-        y1: point[:y],
-        x2: point[:x] + mask[:width],
-        y2: point[:y] + mask[:height],
-      }
-    end
-
-
-
     # This function returns the :width and :height of the image file
     # as a Hash
-    def sizes_from_image_file
+    def image_size
       {
         width: image_file_width,
         height: image_file_height
       }
     end
 
-    def dimensions_to_string(dimensions)
-      "#{dimensions[:width]}x#{dimensions[:height]}"
+    # An Image smaller than dimensions
+    # can not be cropped to string - unless upsample is true.
+    #
+    def can_be_cropped_to(string, upsample = false)
+      dimensions = sizes_from_string(string)
+      return true if upsample
+      is_bigger_than(dimensions)
     end
 
-    def is_smaller_than(dimensions)
-      image_file_width < dimensions[:width] || image_file_height < dimensions[:height]
+    # Returns true if the class we're included in has a meaningful render_size attribute
+    #
+    def render_size?
+      self.respond_to?(:render_size) && !self.render_size.nil? && !self.render_size.empty?
     end
 
-    def center_crop(dimensions, upsample)
-      if self.is_smaller_than(dimensions) && upsample == false
-        dimensions = reduce_to_image(dimensions)
+    # Returns true if the class we're included in has a meaningful crop_size attribute
+    #
+     def crop_size?
+      self.respond_to?(:crop_size) && !self.crop_size.nil? && !self.crop_size.empty?
+    end
+
+    private
+
+      # Given a string with an x, this function return a Hash with key :x and :y
+      #
+      def point_from_string(string = "0x0")
+        string = "0x0" if string.empty?
+        raise ArgumentError if !string.match(/(\d*x)|(x\d*)/)
+
+        x, y = string.scan(/(\d*)x(\d*)/)[0].map(&:to_i)
+
+        x = 0 if x.nil?
+        y = 0 if y.nil?
+        {
+          x: x,
+          y: y
+        }
       end
-      self.image_file.thumb("#{dimensions_to_string(dimensions)}#")
-    end
 
-    def xy_crop_resize(dimensions, top_left, crop_dimensions)
-      crop_argument = "-crop #{dimensions_to_string(crop_dimensions)}"
-      crop_argument += "+#{top_left[:x]}+#{top_left[:y]}"
+      # Given dimensions for a possibly destructive crop operation,
+      # this function returns the top left corner as a Hash
+      # with keys :x, :y
+      #
+      def get_top_left_crop_corner(dimensions)
+        {
+          x: (image_file_width - dimensions[:width]) / 2,
+          y: (image_file_height - dimensions[:height]) / 2
+        }
+      end
 
-      resize_argument = "-resize #{dimensions_to_string(dimensions)}"
-      self.image_file.convert "#{crop_argument} #{resize_argument}"
-    end
+      # Gets the base dimensions (the dimensions of the Picture before scaling).
+      # If anything is missing, it gets padded with zero (Integer 0).
+      # This is the order of precedence: crop_size > image_size
+      def get_base_dimensions
+        if crop_size?
+          sizes_from_string(crop_size)
+        else
+          image_size
+        end
+      end
 
-    def reduce_to_image(dimensions)
-      {
-        width: dimensions[:width] > image_file_width ? image_file_width : dimensions[:width],
-        height: dimensions[:height] > image_file_height ? image_file_height : dimensions[:image_file_height]
-      }
-    end
+      # Given dimensions with :width, :height
+      # this function the dimensions of the base image if cropped to
+      # the aspect ratio of the dimensions hash that's passed in.
+      #
+      def size_when_filling(dimensions)
+        zoom_x = dimensions[:width].to_f / image_file_width
+        zoom_y = dimensions[:height].to_f / image_file_height
+
+        zoom = [zoom_x, zoom_y].max
+        {
+          width: (dimensions[:width] / zoom).round.to_i,
+          height: (dimensions[:height] / zoom).round.to_i
+        }
+      end
+
+      # Given dimensions with :width, :height
+      # this function returns the dimensions of target after being fitted
+      # into an area the size of the dimension hash that's passed in.
+      #
+      def size_when_fitting(dimensions, target)
+        zoom_x = target[:width].to_f / dimensions[:width]
+        zoom_y = target[:height].to_f / dimensions[:height]
+
+        zoom = [zoom_x, zoom_y].min
+        {
+          width: (dimensions[:width] * zoom).round.to_i,
+          height: (dimensions[:height] * zoom).round.to_i
+        }
+      end
+
+      # Given a point as a Hash with :x and :y, and a mask with
+      # :width and :height, this function returns the area on the
+      # underlying canvas as a Hash of two points
+      #
+      def point_and_mask_to_points(point, mask)
+        {
+          x1: point[:x],
+          y1: point[:y],
+          x2: point[:x] + mask[:width],
+          y2: point[:y] + mask[:height],
+        }
+      end
+
+      # Converts a dimensions hash to a string of from "20x20"
+      #
+      def dimensions_to_string(dimensions)
+        "#{dimensions[:width]}x#{dimensions[:height]}"
+      end
+
+      # Returns true if both dimensions of the base image are bigger than the dimensions hash.
+      #
+      def is_bigger_than(dimensions)
+        image_file_width > dimensions[:width] && image_file_height > dimensions[:height]
+      end
+
+      # Returns true is one dimension of the base image is smaller than the dimensions hash.
+      #
+      def is_smaller_than(dimensions)
+        !is_bigger_than(dimensions)
+      end
+
+      # Uses imagemagick to make a centercropped thumbnail. Does not scale the image up.
+      #
+      def center_crop(dimensions, upsample)
+        if is_smaller_than(dimensions) && upsample == false
+          dimensions = reduce_to_image(dimensions)
+        end
+        self.image_file.thumb("#{dimensions_to_string(dimensions)}#")
+      end
+
+      # Use imagemagick to custom crop an image. Uses -thumbnail for better performance when resizing.
+      #
+      def xy_crop_resize(dimensions, top_left, crop_dimensions, upsample)
+        crop_argument = "-crop #{dimensions_to_string(crop_dimensions)}"
+        crop_argument += "+#{top_left[:x]}+#{top_left[:y]}"
+
+        resize_argument = "-thumbnail #{dimensions_to_string(dimensions)}"
+        resize_argument += ">" unless upsample
+        self.image_file.convert "#{crop_argument} #{resize_argument}"
+      end
+
+      # Used only when centercropping because there's no argument
+      # to imagemagick that stops it from upsampling when cropping.
+      #
+      def reduce_to_image(dimensions)
+        {
+          width: [dimensions[:width], image_file_width].min,
+          height: [dimensions[:height], image_file_height].min
+        }
+      end
   end
 end
