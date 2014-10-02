f7bfbbcba1ffea76d02429a508314ba4134fd558
Marc <marc.bluemner@gmail.com>
2014-10-02 07:05:58 +0200

    render with protection @language

--- app/controllers/alchemy/admin/languages_controller.rb
+++ app/controllers/alchemy/admin/languages_controller.rb
@@ -2,6 +2,11 @@ module Alchemy
   module Admin
     class LanguagesController < Alchemy::Admin::ResourcesController
 
+      def index
+        @language.page_layout = (configured_page_layout or @language.page_layout)
+        render_with_protection @language
+      end
+
       def new
         @language = Alchemy::Language.new
         @language.page_layout = (configured_page_layout or @language.page_layout)
