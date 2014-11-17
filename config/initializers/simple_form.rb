1f6ecca53a8261c47a1c17a03064a63415d25865
Martin Meyerhoff <mamhoff@googlemail.com>
2014-11-17 13:18:45 +0100

    convert config.label_text from lambda to proc
    That way, it can accept two as well as three arguments and does not
    throw an error when updating. Copied from
    http://github.com/plataformatec/simple_form/commit/c5cd96675ca413c5b9ea7f20bbe61a0410dbd59a

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -94,7 +94,7 @@ SimpleForm.setup do |config|
   # config.item_wrapper_class = nil
 
   # How the label text should be generated altogether with the required text.
-  config.label_text = lambda { |label, required| "#{label}#{required}" }
+  config.label_text = proc { |label, required| "#{label}#{required}" }
 
   # You can define the class to use on all labels. Default is nil.
   config.label_class = 'control-label'
