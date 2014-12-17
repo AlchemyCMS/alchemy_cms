80f97df0cdc3f14a311fbcb55fa5fd2c3f0441ab
Thomas von Deyen <tvd@magiclabs.de>
2014-12-17 22:09:59 +0100

    Using current Alchemy version for Rails app template.

--- lib/rails/templates/alchemy.rb
+++ lib/rails/templates/alchemy.rb
@@ -1,7 +1,7 @@
 # This rails template installs Alchemy and all depending gems.
 require File.expand_path('../../../alchemy/version', __FILE__)
 
-gem 'alchemy_cms', github: 'magiclabs/alchemy_cms', branch: 'master'
+gem 'alchemy_cms', "#{Alchemy::VERSION}"
 gem 'alchemy-devise', github: 'magiclabs/alchemy-devise', branch: 'master'
 gem 'capistrano', '~> 2.15.5', group: 'development'
 
