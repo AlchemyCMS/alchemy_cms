3bb2bfaadaf1b624e8a737f1c3578bf0575590ae
Thomas von Deyen <tvd@magiclabs.de>
2015-03-02 09:31:05 +0100

    Fix branches for Alchemy Rails template.

--- lib/rails/templates/alchemy.rb
+++ lib/rails/templates/alchemy.rb
@@ -1,6 +1,7 @@
 # This rails template installs Alchemy and all depending gems.
-require File.expand_path('../../../alchemy/version', __FILE__)
+require File.expand_path("../../../alchemy/version", __FILE__)
 
-gem 'alchemy_cms', "#{Alchemy::VERSION}"
-gem 'alchemy-devise', '>= 2.1.0.beta3'
-gem 'capistrano', '~> 2.15.5', group: 'development'
+gem "alchemy_cms",    github: "AlchemyCMS/alchemy_cms",    branch: "3.1-stable"
+gem "alchemy-devise", github: "AlchemyCMS/alchemy-devise", branch: "2.1-stable"
+
+gem "capistrano", "~> 2.15.5", group: "development"
