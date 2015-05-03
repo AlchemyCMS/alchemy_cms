04eaf6e1ddd04dad602a4cf124083d501abd7996
Martin Meyerhoff <mamhoff@googlemail.com>
2015-05-03 15:50:55 +0200

    Extract SSL protection methods into Module
    THat way, we can include them in alchemy-devise.

---
+++ lib/alchemy/ssl_protection.rb
@@ -0,0 +1,30 @@
+module Alchemy
+  module SSLProtection
+    private
+
+    # Enforce ssl for login and all admin modules.
+    #
+    # Default is +false+
+    #
+    # === Usage
+    #
+    #   # config/alchemy/config.yml
+    #   ...
+    #   require_ssl: true
+    #   ...
+    #
+    # === Note
+    #
+    # You have to create a ssl certificate
+    # if you want to use the ssl protection.
+    #
+    def ssl_required?
+      !Rails.env.test? && configuration(:require_ssl)
+    end
+
+    # Redirects current request to https.
+    def enforce_ssl
+      redirect_to url_for(request.params.merge(protocol: 'https'))
+    end
+  end
+end
