26ea0500aeb2654a0d031295b89fc15c0c878098
Martin Meyerhoff <mamhoff@gmail.com>
2015-08-10 09:54:37 +0200

    Change migration to actually collect orphaned contents

--- db/migrate/20150808144451_convert_available_contents_to_nested_elements.rb
+++ db/migrate/20150808144451_convert_available_contents_to_nested_elements.rb
@@ -1,20 +1,23 @@
 class ConvertAvailableContentsToNestedElements < ActiveRecord::Migration
   def up
-    elements_with_available_contents = Alchemy::Element.all.select do |e|
-      e.definition['available_contents']
+    elements_with_nestable_elements = Alchemy::Element.all.select do |e|
+      e.definition['nestable_elements']
     end
-    elements_with_available_contents.each do |el|
-      available_content_names = el.definition['available_contents'].map { |e| e['name'] }
-      available_content_names.each do |name|
-        contents = el.contents.where(name: name)
-        contents.each do |content|
-          new_element = Alchemy::Element.new_from_scratch(name: "addable_#{content.name}")
-          new_element.contents << content
-          new_element.save!
-          el.nested_elements << new_element
-          el.contents.delete(content)
-          el.save
-        end
+
+    elements_with_nestable_elements.each do |el|
+      element_contents = el.definition['contents'].inject([]) do |content_names, definition|
+        content_names << definition['name']
+      end
+
+      orphaned_contents = el.contents.where.not(name: content_names)
+
+      orphaned_contents.each do |content|
+        parent = content.element
+        new_element = content.element.dup
+        new_element.update(name: "addable_#{content.name}")
+        content.element = new_element
+        new_element.save
+        parent.nested_elements << new_element
       end
     end
   end
