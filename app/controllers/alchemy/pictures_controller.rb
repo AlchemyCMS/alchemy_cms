a05855cab001e9cf3a6fababdb592a77acb40caf
Thomas von Deyen <tvd@magiclabs.de>
2015-09-04 00:27:08 +0200

    Flatten animated gifs before converting format.
    Based on @Andrekra solution.
    
    Fixes #794

--- app/controllers/alchemy/pictures_controller.rb
+++ app/controllers/alchemy/pictures_controller.rb
@@ -49,13 +49,16 @@ module Alchemy
       request.session_options[:skip] = true
       ALLOWED_IMAGE_TYPES.each do |type|
         format.send(type) do
+          options = []
           if type == 'jpeg'
             quality = params[:quality] || Config.get(:output_image_jpg_quality)
-            image = image.encode(type, "-quality #{quality}")
-          else
-            image = image.encode(type)
+            options << "-quality #{quality}"
           end
-          render text: image.data
+          # Flatten animated gifs, only if converting to a different format.
+          if type != "gif" && image.ext == 'gif'
+            options << "-flatten"
+          end
+          render text: image.encode(type, options.join(' ')).data
         end
       end
     end
