91a43def474fbf707a9c84d0c42c807c7cbdca09
Antonio Facciolo <antonio.facciolo@ad-notam.com>
2016-09-19 10:25:04 +0200

    add foreign keys to some table
    readd migration in dummy app

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927205604) do
+ActiveRecord::Schema.define(version: 20160930080104) do
 
   create_table "alchemy_attachments", force: :cascade do |t|
     t.string   "name"
