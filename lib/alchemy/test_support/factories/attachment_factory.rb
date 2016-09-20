575caf846da763eb22600d831b2672962fc822eb
Timo Recke <timo.recke@bitspire.de>
2016-09-20 12:46:00 +0200

    Fix factory fixtures for use in host app
    When using the alchemy factories in tests in the host app, the
    attachment and picture factory couldn't be loaded, as they referenced the
    image.png that was located in the spec/fixtures folder. The spec folder
    isn't included in the packed gem, though. This adds the image to the
    test_support path.

--- lib/alchemy/test_support/factories/attachment_factory.rb
+++ lib/alchemy/test_support/factories/attachment_factory.rb
@@ -2,7 +2,7 @@ require 'factory_girl'
 
 FactoryGirl.define do
   factory :alchemy_attachment, class: 'Alchemy::Attachment' do
-    file File.new(File.expand_path('../../../../../spec/fixtures/image.png', __FILE__))
+    file File.new(Alchemy::Engine.root.join('lib', 'alchemy', 'test_support', 'fixtures', 'image.png'))
     name 'image'
     file_name 'image.png'
   end
