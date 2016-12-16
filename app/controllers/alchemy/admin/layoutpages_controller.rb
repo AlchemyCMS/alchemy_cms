5daf9a4cd2a19862884c86c0e46d4346e72a497f
Andreas Schober <shobee@users.noreply.github.com>
2016-12-16 13:07:18 +0100

    Show languages for current site only
    Default scope on `Language` has been removed [here](https://github.com/AlchemyCMS/alchemy_cms/commit/2a2a3408b9162fd66bfed76bcec93491d3861fd2#diff-23d21467ab865799a19dd49ee8d172c0) and I think we never want to see *all* languages but from the current site only.

--- app/controllers/alchemy/admin/layoutpages_controller.rb
+++ app/controllers/alchemy/admin/layoutpages_controller.rb
@@ -6,7 +6,7 @@ module Alchemy
 
       def index
         @layout_root = Page.find_or_create_layout_root_for(Language.current.id)
-        @languages = Language.all
+        @languages = Language.on_current_site
       end
 
       def edit
