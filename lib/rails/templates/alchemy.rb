384e1d882e5b0dc98f0f87c3afe016070cc07f9d
Thomas von Deyen <tvd@bitspire.de>
2016-12-19 13:35:41 +0100

    Remove the stand alone installer
    The stand alone installer mainly creates a rails app and adds the gem into the gemspec, then runs the installer rake task.
    
    This is often cause of bugs and is something we don't want to maintain anymore.

--- lib/rails/templates/alchemy.rb
+++
@@ -1,7 +0,0 @@
-# This rails template installs Alchemy and all depending gems.
-require File.expand_path("../../../alchemy/version", __FILE__)
-
-gem "alchemy_cms",    github: "AlchemyCMS/alchemy_cms",    branch: "master"
-gem "alchemy-devise", github: "AlchemyCMS/alchemy-devise", branch: "master"
-
-gem "capistrano-alchemy", github: "AlchemyCMS/capistrano-alchemy", branch: "master", group: "development"
