aeb74f39c0d13a8a54d3f6b2701410fc66fca170
Thomas von Deyen <thomas@vondeyen.com>
2017-01-23 09:09:49 +0100

    Convert legacy page redirect spec into request spec
    Rails controller spec are unit tests to controllers where
    the routing and lots of Rails' internals are stubbed.
    
    Request specs test run with the same internals as in a real request.
    
    Also removes lots of stubbing in the specs itself, that's why
    these tests fail on Rails 5.
    
    By rewriting them into request specs they even work with Rails 5.

---
+++ spec/requests/alchemy/legacy_page_redirecting_spec.rb
@@ -0,0 +1,76 @@
+require 'spec_helper'
+
+RSpec.describe 'Redirecting to legacy page urls' do
+  let(:page) do
+    create(:alchemy_page, :public, name: 'New page name')
+  end
+
+  let(:second_page) do
+    create(:alchemy_page, :public, name: 'Second Page')
+  end
+
+  let(:legacy_page) do
+    create(:alchemy_page, :public, name: 'Legacy Url')
+  end
+
+  let!(:legacy_url) do
+    Alchemy::LegacyPageUrl.create(urlname: 'legacy-url', page: page)
+  end
+
+  let(:legacy_url2) do
+    Alchemy::LegacyPageUrl.create(urlname: 'legacy-url', page: second_page)
+  end
+
+  let(:legacy_url4) do
+    Alchemy::LegacyPageUrl.create(
+      urlname: 'index.php?option=com_content&view=article&id=48&Itemid=69',
+      page: second_page
+    )
+  end
+
+  context 'if url has an unknown format & get parameters' do
+    it "redirects permanently to page that belongs to legacy page url" do
+      get "/#{legacy_url4.urlname}"
+      expect(response.status).to eq(301)
+      expect(response).to redirect_to("/#{second_page.urlname}")
+    end
+  end
+
+  it "should not pass query string for legacy routes" do
+    get "/#{legacy_url4.urlname}"
+    expect(URI.parse(response["Location"]).query).to be_nil
+  end
+
+  it "should only redirect to legacy url if no page was found for urlname" do
+    get "/#{legacy_page.urlname}"
+    expect(response.status).to eq(200)
+    expect(response).not_to redirect_to("/#{page.urlname}")
+  end
+
+  it "should redirect to last page that has that legacy url" do
+    get "/#{legacy_url2.urlname}"
+    expect(response).to redirect_to("/#{second_page.urlname}")
+  end
+
+  context 'if the url has get parameters' do
+    let(:legacy_url3) do
+      Alchemy::LegacyPageUrl.create(urlname: 'index.php?id=2', page: second_page)
+    end
+
+    it "redirects" do
+      get "/#{legacy_url3.urlname}"
+      expect(response).to redirect_to("/#{second_page.urlname}")
+    end
+  end
+
+  context 'when the url has nested urlname' do
+    let(:legacy_url5) do
+      Alchemy::LegacyPageUrl.create(urlname: 'nested/legacy/url', page: second_page)
+    end
+
+    it "redirects" do
+      get "/#{legacy_url5.urlname}"
+      expect(response).to redirect_to("/#{second_page.urlname}")
+    end
+  end
+end
