fce9cfb2bccf1aa683f7a10f9378bbcee4e98c48
Thomas von Deyen <thomas@vondeyen.com>
2017-02-24 23:04:12 +0100

    Deprecate controller requests test helpers
    There is no need to use these helpers. Instead, in a functional/controller test against a Alchemy controller, just use standard Rails functionality by including:
    
        routes { Alchemy::Engine.routes }
    
    And then use standard Rails test `get`, `post` etc methods.

--- lib/alchemy/test_support/controller_requests.rb
+++ lib/alchemy/test_support/controller_requests.rb
@@ -1,3 +1,11 @@
+# *There is generally no need* to use this module. Instead, in
+# a functional/controller test against a Alchemy controller, just
+# use standard Rails functionality by including:
+#
+#   routes { Alchemy::Engine.routes }
+#
+# And then use standard Rails test `get`, `post` etc methods.
+#
 # Use this module to easily test Alchemy actions within Alchemy components
 # or inside your application to test routes for the mounted Alchemy engine.
 #
@@ -25,35 +33,47 @@
 #
 # Note: Based on Spree::TestingSupport::ControllerRequests. Thanks <3
 #
+# @deprecated Use Rails build in test request methods instead
+#
 module Alchemy
   module TestSupport
     module ControllerRequests
       extend ActiveSupport::Concern
 
       # Executes a request simulating GET HTTP method
+      # @deprecated Use Rails test `get` helper instead
       def alchemy_get(action, parameters = nil, session = nil, flash = nil)
         process_alchemy_action(action, parameters, session, flash, "GET")
       end
+      deprecate alchemy_get: :get, deprecator: Alchemy::Deprecation
 
       # Executes a request simulating POST HTTP method
+      # @deprecated Use Rails test `post` helper instead
       def alchemy_post(action, parameters = nil, session = nil, flash = nil)
         process_alchemy_action(action, parameters, session, flash, "POST")
       end
+      deprecate alchemy_post: :post, deprecator: Alchemy::Deprecation
 
       # Executes a request simulating PUT HTTP method
+      # @deprecated Use Rails test `put` helper instead
       def alchemy_put(action, parameters = nil, session = nil, flash = nil)
         process_alchemy_action(action, parameters, session, flash, "PUT")
       end
+      deprecate alchemy_put: :put, deprecator: Alchemy::Deprecation
 
       # Executes a request simulating DELETE HTTP method
+      # @deprecated Use Rails test `delete` helper instead
       def alchemy_delete(action, parameters = nil, session = nil, flash = nil)
         process_alchemy_action(action, parameters, session, flash, "DELETE")
       end
+      deprecate alchemy_delete: :delete, deprecator: Alchemy::Deprecation
 
       # Executes a simulated XHR request
+      # @deprecated Use Rails test `xhr` helper instead
       def alchemy_xhr(method, action, parameters = nil, session = nil, flash = nil)
         process_alchemy_xhr_action(method, action, parameters, session, flash)
       end
+      deprecate alchemy_xhr: :xhr, deprecator: Alchemy::Deprecation
 
       private
 
