8d0bbfd3d34cbcac64ac6ee7c091a635fdd78131
Robin Böning <rb@magiclabs.de>
2017-06-16 11:38:15 +0200

    Add failing spec for filtering pictures by tags in overlay

--- spec/features/admin/picture_overlay_spec.rb
+++ spec/features/admin/picture_overlay_spec.rb
@@ -7,6 +7,36 @@ RSpec.feature "Picture overlay" do
 
   let(:element) { create(:alchemy_element) }
 
+  describe "filter by tags", js: true do
+    let!(:a_page) { create(:alchemy_page, do_not_autogenerate: false) }
+    let!(:pic1) { create(:alchemy_picture, name: "Hill", tag_list: "landscape") }
+    let!(:pic2) { create(:alchemy_picture, name: "Skyscraper", tag_list: "city") }
+
+    scenario "shows only the pictures tagged with the selected tag" do
+      pending("Buggy at the moment. Fix is not implemented yet")
+
+      visit alchemy.edit_admin_page_path(a_page)
+
+      within "#element_area div[data-element-name='article']" do
+        click_on "Insert image"
+      end
+
+      within ".alchemy-dialog.modal" do
+        # We expect to see both pictures
+        expect(page).to have_selector("#overlay_picture_list a img", count: 2)
+
+        # Click on a tag to filter the pictures
+        within ".tag-list" do
+          click_on "landscape (1)"
+        end
+
+        # We expect to see only the picture tagged with 'landscape'.
+        expect(page).to have_selector("#overlay_picture_list a img", count: 1)
+        expect(page).to have_selector("#overlay_picture_list a img[alt='Hill']")
+      end
+    end
+  end
+
   describe "Upload button" do
     let(:options) do
       {grouped: true}
