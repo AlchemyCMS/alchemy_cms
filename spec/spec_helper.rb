51627daf7aeed98a769207192265db508f4950e7
Thomas von Deyen <thomas@vondeyen.com>
2017-06-20 21:17:56 +0200

    Do not silence deprecations in tests
    We want to see what we need to do for Rails 5.1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,7 +36,7 @@ ActionMailer::Base.delivery_method = :test
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.default_url_options[:host] = "test.com"
 
-ActiveSupport::Deprecation.silenced = true
+ActiveSupport::Deprecation.silenced = false
 
 Rails.backtrace_cleaner.remove_silencers!
 # Disable rails loggin for faster IO. Remove this if you want to have a test.log
