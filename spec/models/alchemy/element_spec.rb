0996813d94efd3c5458daa93468285cca0c1227f
Allan Reyes <allanbreyes@users.noreply.github.com>
2017-07-04 10:33:33 -0700

    Fix Psych error (#1272)
    * Add Symbol to YAML safe load whitelist classes
    
    * Add test coverage for loading definitions file
    
    * Allow Symbols in page layout YAML
    
    * Convert Psych/Symbol tests to unit tests

--- spec/models/alchemy/element_spec.rb
+++ spec/models/alchemy/element_spec.rb
@@ -117,6 +117,18 @@ module Alchemy
         expect(Element.definitions.collect { |el| el['name'] }).to include('erb_element')
       end
 
+      context "with a YAML file including a symbol" do
+        let(:yaml) { 'name: :symbol' }
+        before do
+          expect(File).to receive(:exist?).and_return(true)
+          expect(File).to receive(:read).and_return(yaml)
+        end
+
+        it "returns the definition without error" do
+          expect { Element.definitions }.to_not raise_error
+        end
+      end
+
       context "without existing yml files" do
         before { allow(File).to receive(:exist?).and_return(false) }
 
