5afde7d8f9353870258e7143563fd5cdc9be9323
Thomas von Deyen <thomas@vondeyen.com>
2018-01-09 22:19:29 +0100

    New lighter table style
    Remove the baby blueish table row color and add more whitespace into the
    table cells.

--- app/controllers/alchemy/admin/base_controller.rb
+++ app/controllers/alchemy/admin/base_controller.rb
@@ -112,7 +112,7 @@ module Alchemy
       def per_page_value_for_screen_size
         return 25 if session[:screen_size].blank?
         screen_height = session[:screen_size].split('x').last.to_i
-        (screen_height / 30) - 10
+        (screen_height / 50) - 12
       end
 
       # Does redirects for html and js requests
