7d0a29766a04ada54d4632c2d43fb8299db27a14
Thomas von Deyen <thomas@vondeyen.com>
2018-01-15 21:16:27 +0100

    Remove unused contents controller actions
    This is a leftover from "available contents" feature removal.

--- app/controllers/alchemy/admin/contents_controller.rb
+++ app/controllers/alchemy/admin/contents_controller.rb
@@ -7,11 +7,6 @@ module Alchemy
 
       authorize_resource class: Alchemy::Content
 
-      def new
-        @element = Element.find(params[:element_id])
-        @content = @element.contents.build
-      end
-
       def create
         @element = Element.find(params[:content][:element_id])
         @content = Content.create_from_scratch(@element, content_params)
@@ -26,11 +21,6 @@ module Alchemy
         end
       end
 
-      def update
-        @content = Content.find(params[:id])
-        @content.update_essence(content_params)
-      end
-
       def order
         Content.transaction do
           params[:content_ids].each_with_index do |id, idx|
@@ -38,13 +28,6 @@ module Alchemy
           end
         end
         @notice = Alchemy.t("Successfully saved content position")
-      end
-
-      def destroy
-        @content = Content.find(params[:id])
-        @content_dom_id = @content.dom_id
-        @notice = Alchemy.t("Successfully deleted content", content: @content.name_for_label)
-        @content.destroy
       end
 
       private
