f733a1fd424acfc590dacb56f0d839aa48edb917
Thomas von Deyen <thomas@vondeyen.com>
2018-02-07 11:26:52 +0100

    Install vue-turbolinks
    In order to teardown Vue components properly on Turbolinks page transitions, we need to install a small Vue mixin.

--- package/src/edit_page.js
+++ package/src/edit_page.js
@@ -1,18 +1,20 @@
 import Vue from "vue/dist/vue.esm"
 import Vuex from "vuex"
 import translate from "./i18n"
+import TurbolinksAdapter from "vue-turbolinks"
 import AlchemyDialogButton from "./components/dialog_button"
 import AlchemyElementsWindow from "./components/elements_window"
 import AlchemyPreviewWindow from "./components/preview_window"
 import storeConfig from "./vuex_store"
 
 Vue.use(Vuex)
+Vue.use(TurbolinksAdapter)
 Vue.filter("translate", function (value, replacement) {
   if (!value) return ""
   return translate(value, replacement)
 })
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("turbolinks:load", () => {
   const editPage = document.getElementById("edit_page")
   const store = new Vuex.Store(storeConfig)
 
@@ -25,7 +27,10 @@ window.addEventListener("DOMContentLoaded", () => {
         AlchemyElementsWindow,
         AlchemyPreviewWindow
       },
-      store
+      store,
+      beforeDestroy() {
+        new Alchemy.Spinner().spin(this.$el.parentNode)
+      }
     })
     Alchemy.vuexStore = store
   }
