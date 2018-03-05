066e6598bbff01822615f29ce2feb5efa76adf85
Thomas von Deyen <thomas@vondeyen.com>
2018-03-05 21:28:16 +0100

    Register events and locations as Alchemy module
    The event and location moduls are custom models from the dummy app we used to test the resources controller from Alchemy. By registering them as modules we can also use them when booting the dummy app locally in development.

--- spec/dummy/app/models/event.rb
+++ spec/dummy/app/models/event.rb
@@ -1,10 +1,19 @@
 class Event < ActiveRecord::Base
+  include Alchemy::Taggable
+
   validates_presence_of :name
   belongs_to :location
+
+  scope :starting_today, -> { where(starts_at: Time.current.at_midnight..Date.tomorrow.at_midnight) }
+  scope :future, -> { where("starts_at > ?", Date.tomorrow.at_midnight) }
 
   def self.alchemy_resource_relations
     {
       location: {attr_method: 'name', attr_type: 'string'}
     }
+  end
+
+  def self.alchemy_resource_filters
+    %w(starting_today future)
   end
 end
