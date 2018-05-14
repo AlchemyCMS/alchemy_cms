b0e1bb42788cf8f114d2a120662deea660623d2a
Thomas von Deyen <thomas@vondeyen.com>
2018-05-14 10:46:10 +0200

    Use be_successful in controller specs
    `be_success` is deprecated and will be removed in Rails 6

--- spec/controllers/alchemy/pages_controller_spec.rb
+++ spec/controllers/alchemy/pages_controller_spec.rb
@@ -193,7 +193,7 @@ module Alchemy
 
       it "renders page" do
         get :show, params: {urlname: still_public_page.urlname}
-        expect(response).to be_success
+        expect(response).to be_successful
       end
     end
 
@@ -207,7 +207,7 @@ module Alchemy
 
       it "renders page" do
         get :show, params: {urlname: still_public_page.urlname}
-        expect(response).to be_success
+        expect(response).to be_successful
       end
     end
 
