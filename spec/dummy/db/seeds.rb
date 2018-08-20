fd069783182a27d16dedb792ce2c4009ca80bcec
Thomas von Deyen <thomas@vondeyen.com>
2018-08-20 08:57:52 +0200

    Add seeds for 1000 dummy pages

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -1,3 +1,24 @@
 # frozen_string_literal: true
 
+require 'alchemy/test_support/factories'
+
 Alchemy::Seeder.seed!
+
+lang_root = FactoryBot.create(:alchemy_page, :language_root)
+page_levels = ENV.fetch('PAGE_LEVELS', 10).to_i
+pages_per_level = ENV.fetch('PAGES_PER_LEVEL', 100).to_i
+parent_ids = []
+parent_id = lang_root.id
+
+puts "\nCreating #{page_levels * pages_per_level} Alchemy pages. Hold tight, this may take a while!\n"
+
+page_levels.times do
+  pages_per_level.times do
+    page = FactoryBot.create(:alchemy_page, :public, parent_id: parent_id)
+    parent_ids << page.id
+    print "."
+  end
+  parent_id = parent_ids.sample
+end
+
+puts "\nDone!"
