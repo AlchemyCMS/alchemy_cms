0b485d7909f39498e3aa5aed8dcf6f605ef779b4
Marc Antwertinger <marc.antwertinger@4eb.de>
2019-01-22 10:01:09 +0100

    Delete foreign key before dropping table

--- db/migrate/20181030082614_remove_alchemy_cells.rb
+++ db/migrate/20181030082614_remove_alchemy_cells.rb
@@ -1,5 +1,10 @@
 class RemoveAlchemyCells < ActiveRecord::Migration[5.2]
   def change
+    remove_reference :alchemy_elements, :cell, index: true, foreign_key: {
+      to_table: :alchemy_cells,
+      on_update: :cascade,
+      on_delete: :cascade
+    }
     drop_table :alchemy_cells do |t|
       t.integer "page_id", null: false
       t.string "name"
@@ -7,10 +12,5 @@ class RemoveAlchemyCells < ActiveRecord::Migration[5.2]
       t.datetime "updated_at", null: false, precision: 6
       t.index ["page_id"], name: "index_alchemy_cells_on_page_id"
     end
-    remove_reference :alchemy_elements, :cell, index: true, foreign_key: {
-      to_table: :alchemy_cells,
-      on_update: :cascade,
-      on_delete: :cascade
-    }
   end
 end
