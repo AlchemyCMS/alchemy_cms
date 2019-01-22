10b5c73b701ed6f7591228793c6d09635878d413
Marc Antwertinger <marc.antwertinger@4eb.de>
2019-01-22 13:07:38 +0100

    Add additional test for cropping behavior
    Add a test to ensure cropping is disabled if crop_size and crop_from are not present even if cropping is explicitly enabled in essence settings

--- spec/models/alchemy/essence_picture_spec.rb
+++ spec/models/alchemy/essence_picture_spec.rb
@@ -220,23 +220,32 @@ module Alchemy
           )
           thumbnail_url
         end
+      end
 
-        context 'when crop is explicitely enabled in the options' do
-          let(:options) do
-            {crop: true}
-          end
+      context 'when crop is explicitly enabled in the options' do
+        let(:options) do
+          {crop: true}
+        end
 
+        context 'and crop size and crop from are present' do
           before do
             allow(essence).to receive(:crop_size).and_return('200x200')
             allow(essence).to receive(:crop_from).and_return('10x10')
           end
 
-          it "it enables cropping." do
+          it 'enables cropping' do
             expect(picture).to receive(:url).with(
               hash_including(crop: true)
             )
             thumbnail_url
           end
+        end
+
+        it 'skips cropping if crop size and crop from are missing' do
+          expect(picture).to receive(:url).with(
+            hash_including(crop: false)
+          )
+          thumbnail_url
         end
       end
 
