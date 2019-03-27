e4e3cbfc4e9793abee41bc7372eec628d36119c0
Thomas von Deyen <thomas@vondeyen.com>
2019-03-27 12:40:24 +0100

    Allow admins to switch all languages
    Admins and editors should be able to switch languages even if they are
    not public, so they can start preparing content.
    
    Closes #1535

--- spec/features/admin/language_tree_feature_spec.rb
+++ spec/features/admin/language_tree_feature_spec.rb
@@ -4,21 +4,45 @@ require 'rails_helper'
 
 RSpec.describe 'Language tree feature', type: :system, js: true do
   let(:klingon) { create(:alchemy_language, :klingon) }
+  let(:user) { build(:alchemy_dummy_user, :as_admin) }
 
   before do
     create(:alchemy_page, :language_root)
-    authorize_user(:as_admin)
+    authorize_user(user)
+  end
+
+  context "with a single language" do
+    it "one should not be able to switch the language tree" do
+      visit('/admin/pages')
+      expect(page).to_not have_selector('label', text: Alchemy.t("Language tree"))
+    end
   end
 
   context "in a multilangual environment" do
-    before do
-      create(:alchemy_page, :language_root, name: 'Klingon', language: klingon)
-    end
+    context 'even if one language is not public' do
+      let(:klingon) { create(:alchemy_language, :klingon, public: false) }
 
-    it "one should be able to switch the language tree" do
-      visit('/admin/pages')
-      select2 'Klingon', from: 'Language tree'
-      expect(page).to have_selector('#sitemap', text: 'Klingon')
+      before do
+        create(:alchemy_page, :language_root, name: 'Klingon', language: klingon)
+      end
+
+      context 'and an author' do
+        let(:user) { build(:alchemy_dummy_user, :as_author) }
+
+        it "one should not be able to switch the language tree" do
+          visit('/admin/pages')
+          expect(page).to_not have_selector('label', text: Alchemy.t("Language tree"))
+        end
+      end
+
+      context 'and an editor' do
+        let(:user) { build(:alchemy_dummy_user, :as_editor) }
+
+        it "one should be able to switch the language tree" do
+          visit('/admin/pages')
+          expect(page).to have_selector('label', text: Alchemy.t("Language tree"))
+        end
+      end
     end
   end
 
