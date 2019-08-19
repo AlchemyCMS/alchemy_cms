360a1cde995b904919660ecaae119c3c012c259e
Thomas von Deyen <thomas@vondeyen.com>
2019-08-19 13:45:44 +0200

    Update Dummy test app to Rails 5.2
    We were still loading the defaults of Rails 5.1
    
    In preparation for Rails 6 we should test with the latest 5.x version

--- spec/dummy/config/boot.rb
+++ spec/dummy/config/boot.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-# Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../../Gemfile', __dir__)
 
-require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
-require 'bootsnap/setup'
-$LOAD_PATH.unshift File.expand_path('../../../lib', __dir__)
+require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
