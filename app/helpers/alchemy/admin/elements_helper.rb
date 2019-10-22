762f2a865c59b91d9493da5a84618ee6df0721ef
Thomas von Deyen <thomas@vondeyen.com>
2019-10-22 12:24:56 +0200

    Add an ElementEditor decorator
    This allows us to move helper classes into this class and
    render the element editor partial in a more efficient way.

--- app/helpers/alchemy/admin/elements_helper.rb
+++ app/helpers/alchemy/admin/elements_helper.rb
@@ -20,29 +20,6 @@ module Alchemy
           ]
         end
       end
-
-      # CSS classes for the element editor partial.
-      def element_editor_classes(element)
-        [
-          'element-editor',
-          element.content_definitions.present? ? 'with-contents' : 'without-contents',
-          element.nestable_elements.any? ? 'nestable' : 'not-nestable',
-          element.taggable? ? 'taggable' : 'not-taggable',
-          element.folded ? 'folded' : 'expanded',
-          element.compact? ? 'compact' : nil,
-          element.fixed? ? 'is-fixed' : 'not-fixed'
-        ].join(' ')
-      end
-
-      # Tells us, if we should show the element footer.
-      def show_element_footer?(element, with_nestable_elements = nil)
-        return false if element.folded?
-        if with_nestable_elements
-          element.content_definitions.present? || element.taggable?
-        else
-          element.nestable_elements.empty?
-        end
-      end
     end
   end
 end
