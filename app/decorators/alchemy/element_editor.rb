088fd2068147d5e883f6e1c00b52cb5a4ef51638
Thomas von Deyen <thomas@vondeyen.com>
2019-10-22 12:24:56 +0200

    Add an ElementEditor decorator
    This allows us to move helper classes into this class and
    render the element editor partial in a more efficient way.

---
+++ app/decorators/alchemy/element_editor.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Alchemy
+  class ElementEditor < SimpleDelegator
+    alias_method :element, :__getobj__
+
+    def to_partial_path
+      "alchemy/admin/elements/element"
+    end
+
+    # CSS classes for the element editor partial.
+    def css_classes
+      [
+        'element-editor',
+        content_definitions.present? ? 'with-contents' : 'without-contents',
+        nestable_elements.any? ? 'nestable' : 'not-nestable',
+        taggable? ? 'taggable' : 'not-taggable',
+        folded ? 'folded' : 'expanded',
+        compact? ? 'compact' : nil,
+        fixed? ? 'is-fixed' : 'not-fixed'
+      ].join(' ')
+    end
+
+    # Tells us, if we should show the element footer and form inputs.
+    def editable?
+      return false if folded?
+      content_definitions.present? || taggable?
+    end
+
+    # Fixes Rails partial renderer calling to_model on the object
+    # which reveals the delegated element instead of this decorator.
+    def respond_to?(method_name)
+      return false if method_name == :to_model
+
+      super
+    end
+  end
+end
--- app/helpers/alchemy/admin/elements_helper.rb
+++ app/helpers/alchemy/admin/elements_helper.rb
@@ -20,25 +20,6 @@ module Alchemy
           ]
         end
       end
-
-      # CSS classes for the element editor partial.
-      def element_editor_classes(element)
-        [
-          'element-editor',
-          element.content_definitions.present? ? 'with-contents' : 'without-contents',
-          element.nestable_elements.any? ? 'nestable' : 'not-nestable',
-          element.taggable? ? 'taggable' : 'not-taggable',
-          element.folded ? 'folded' : 'expanded',
-          element.compact? ? 'compact' : nil,
-          element.fixed? ? 'is-fixed' : 'not-fixed'
-        ].join(' ')
-      end
-
-      # Tells us, if we should show the element footer and form inputs.
-      def element_editable?(element)
-        return false if element.folded?
-        element.content_definitions.present? || element.taggable?
-      end
     end
   end
 end
