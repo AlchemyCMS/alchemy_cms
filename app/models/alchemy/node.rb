41e3c41b31d3c108b1aa50fd46c90874b47246c7
Thomas von Deyen <thomas@vondeyen.com>
2019-10-29 22:09:20 +0100

    Add nodes

---
+++ app/models/alchemy/node.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Alchemy
+  class Node < BaseRecord
+    VALID_URL_REGEX = /\A(\/|\D[a-z\+\d\.\-]+:)/
+
+    acts_as_nested_set scope: 'language_id', touch: true
+    stampable stamper_class_name: Alchemy.user_class_name
+
+    belongs_to :language, class_name: 'Alchemy::Language'
+    belongs_to :page, class_name: 'Alchemy::Page', optional: true
+
+    validates :url, format: { with: VALID_URL_REGEX }, unless: -> { url.nil? }
+
+    # Returns the name
+    #
+    # Either the value is stored in the database
+    # or, if attached, the values comes from a page.
+    def name
+      read_attribute(:name).presence || page&.name
+    end
+
+    # Returns the url
+    #
+    # Either the value is stored in the database, aka. an external url.
+    # Or, if attached, the values comes from a page.
+    def url
+      page && "/#{page.urlname}" || read_attribute(:url).presence
+    end
+
+    def to_partial_path
+      "#{view_folder_name}/wrapper"
+    end
+
+    def view_folder_name
+      "alchemy/menus/#{name.parameterize.underscore}"
+    end
+  end
+end
