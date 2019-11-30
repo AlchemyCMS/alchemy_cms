a154c3dd783956cfbd96c1459e004f49ac6e71c0
Thomas von Deyen <thomas@vondeyen.com>
2019-12-01 00:12:11 +0100

    Use a Sprockets 3/4 manifest file
    Instead of using the Rails.config.assets array to define assets
    files we need to precompile as individual files we use the Sprockets
    3 recommended manifest file.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,14 +1,2 @@
 # Add Alchemy assets for precompiling
-Rails.application.config.assets.precompile += [
-  'alchemy/admin/all.css',
-  'alchemy/admin/all.js',
-  'alchemy/alchemy-logo.svg',
-  'alchemy/favicon.ico',
-  'alchemy/preview.js',
-  'alchemy/menubar.css',
-  'alchemy/menubar.js',
-  'alchemy/print.css',
-  'alchemy/welcome.css',
-  'Jcrop.gif',
-  'tinymce/*'
-]
+Rails.application.config.assets.precompile << 'alchemy_manifest.js'
