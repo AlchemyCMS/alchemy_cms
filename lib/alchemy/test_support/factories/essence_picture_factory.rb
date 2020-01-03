05e1c16f91bce0ec6e84f688912b63d12a05ba64
Thomas von Deyen <thomas@vondeyen.com>
2020-01-03 01:33:06 +0100

    Use contents settings for size in EssencePicture#picture_url
    Without we would need to pass the settings into the method as options
    although the essence always knows its content and therefore can read
    the size value from it.

--- lib/alchemy/test_support/factories/essence_picture_factory.rb
+++ lib/alchemy/test_support/factories/essence_picture_factory.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 require 'factory_bot'
+require 'alchemy/test_support/factories/content_factory'
 require 'alchemy/test_support/factories/picture_factory'
 
 FactoryBot.define do
   factory :alchemy_essence_picture, class: 'Alchemy::EssencePicture' do
     picture factory: :alchemy_picture
+
+    trait :with_content do
+      association :content, factory: :alchemy_content
+    end
   end
 end
