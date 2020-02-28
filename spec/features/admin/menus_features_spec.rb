6000d1806940c6634c3e7dc992d3e9fce1b997f0
Thomas von Deyen <thomas@vondeyen.com>
2020-02-28 10:19:07 +0100

    Fix new menu form
    We need to pass the current site as well.

--- spec/features/admin/menus_features_spec.rb
+++ spec/features/admin/menus_features_spec.rb
@@ -8,6 +8,21 @@ RSpec.describe "Admin Menus Features", type: :system do
   end
 
   describe 'adding a new menu' do
+    context 'on the index page' do
+      let!(:default_site) { create(:alchemy_site, :default) }
+
+      it 'creates menu' do
+        visit alchemy.admin_nodes_path
+
+        fill_in 'Name', with: 'Main Menu'
+        click_button 'create'
+
+        expect(page).to have_selector('.node_name', text: 'Main Menu')
+      end
+    end
+  end
+
+  describe 'adding a new menu' do
     context 'with multiple sites' do
       let!(:default_site) { create(:alchemy_site, :default) }
       let!(:site_2) { create(:alchemy_site, host: 'another-site.com') }
