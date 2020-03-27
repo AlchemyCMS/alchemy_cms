cb1b5ed70f8871deaafef743e6af5c15759137c2
Jury Ghidinelli <juryghidinelli@gmail.com>
2020-03-27 15:59:29 +0100

    Expandible menu

--- app/assets/javascripts/alchemy/alchemy.scrollable_menu.js
+++ app/assets/javascripts/alchemy/alchemy.scrollable_menu.js
@@ -1,6 +1,16 @@
 Alchemy.ScrollableMenu = {
-  
+  expandibleMenu: function(){    
+    $(".main_navi_entry.has_sub_navigation.active").addClass('expanded');
+    $(".main_navi_entry.has_sub_navigation").click(function(event){      
+      var target= $(event.target);
+      if(target.hasClass('main_navi_entry')){
+        $(this).toggleClass('expanded');
+      }
+    });
+  },
   init: function() {
+    this.expandibleMenu();
+
     new SimpleBar($('#left_menu')[0]);
   }
 }
