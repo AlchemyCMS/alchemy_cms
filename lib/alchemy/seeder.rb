2e50a5fe41ed7731ae3c244fa12d975ee289cf04
Martin Meyerhoff <mamhoff@gmail.com>
2020-03-27 14:01:22 +0100

    Add Site and Language creation to seeder

--- lib/alchemy/seeder.rb
+++ lib/alchemy/seeder.rb
@@ -30,6 +30,8 @@ module Alchemy
           log "There are already pages present in your database. " \
               "Please use `rake db:reset' if you want to rebuild your database.", :skip
         else
+          create_default_site! unless Alchemy::Site.default
+          create_default_language! unless Alchemy::Language.default
           seed_pages if contentpages.present?
           seed_layoutpages if layoutpages.present?
         end
@@ -106,6 +108,35 @@ module Alchemy
         log "Created page: #{page.name}"
         children.each do |child|
           create_page(child, parent: page)
+        end
+      end
+
+      # If no languages are present, create a default language based
+      # on the host app's Alchemy configuration.
+      def create_default_language!
+        default_language = Alchemy::Config.get(:default_language)
+        if default_language
+          Alchemy::Language.create!(
+            name:           default_language['name'],
+            language_code:  default_language['code'],
+            locale:         default_language['code'],
+            frontpage_name: default_language['frontpage_name'],
+            page_layout:    default_language['page_layout'],
+            public:         true,
+            default:        true,
+            site:           Alchemy::Site.default
+          )
+        else
+          raise DefaultLanguageNotFoundError
+        end
+      end
+
+      def create_default_site!
+        default_site = Alchemy::Config.get(:default_site)
+        if default_site
+          Alchemy::Site.create!(name: default_site['name'], host: default_site['host'])
+        else
+          raise DefaultSiteNotFoundError
         end
       end
     end
