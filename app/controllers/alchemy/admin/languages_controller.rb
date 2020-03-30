778fed24bfbc8372d03e93bf51365dcc88342223
Thomas von Deyen <thomas@vondeyen.com>
2020-03-30 13:58:03 +0200

    Only redirect to new site form if current site is missing

--- app/controllers/alchemy/admin/languages_controller.rb
+++ app/controllers/alchemy/admin/languages_controller.rb
@@ -3,19 +3,17 @@
 module Alchemy
   module Admin
     class LanguagesController < ResourcesController
-      before_action unless: -> { Alchemy::Site.any? } do
-        flash[:warning] = Alchemy.t('Please create a site first.')
-        redirect_to admin_sites_path
-      end
+      before_action :load_current_site, only: %i[index new]
 
       def index
-        @query = Language.on_current_site.ransack(search_filter_params[:q])
+        @query = Language.on_site(@current_site).ransack(search_filter_params[:q])
         @query.sorts = default_sort_order if @query.sorts.empty?
         @languages = @query.result.page(params[:page] || 1).per(items_per_page)
       end
 
       def new
         @language = Language.new(
+          site: @current_site,
           page_layout: Config.get(:default_language)['page_layout']
         )
       end
@@ -23,6 +21,16 @@ module Alchemy
       def switch
         set_alchemy_language(params[:language_id])
         do_redirect_to request.referer || alchemy.admin_dashboard_path
+      end
+
+      private
+
+      def load_current_site
+        @current_site = Alchemy::Site.current
+        if @current_site.nil?
+          flash[:warning] = Alchemy.t('Please create a site first.')
+          redirect_to admin_sites_path
+        end
       end
     end
   end
