0647ff9c1c92d9bc081de7fbf068e1b6cebcc82b
Mikael Norlén <mickenorlen@gmail.com>
2020-04-07 14:43:02 +0200

    Srcset respect cropping
    Crop if crop=true in options

--- app/models/alchemy/essence_picture_view.rb
+++ app/models/alchemy/essence_picture_view.rb
@@ -74,10 +74,49 @@ module Alchemy
 
     def srcset
       options[:srcset].map do |size|
-        url = essence.picture_url(size: size)
+        crop_from, crop_size = srcset_crop_for_size(size)
         width, height = size.split('x')
+        url = essence.picture_url(
+          size: size,
+          crop: @options[:crop],
+          crop_from: crop_from,
+          crop_size: crop_size
+        )
         width.present? ? "#{url} #{width}w" : "#{url} #{height}h"
       end
+    end
+
+    # Recalculate cropping to adjust aspect ratio and maintain center for any srcset size
+    def srcset_crop_for_size(size)
+      crop_size = essence.crop_size.presence
+
+      return [nil, nil] unless @options[:crop].present? && crop_size.present?
+
+      width, height = size.split('x').map(&:to_f)
+      crop_width, crop_height = crop_size.split('x').map(&:to_f)
+
+      crop_ratio = (crop_width / crop_height)
+      size_ratio = (width.to_f / height)
+
+      # Return same cropping if aspect ratio unchanged
+      return essence.crop_from, "#{crop_width}x#{crop_height}" if "%.2f" % crop_ratio == "%.2f" % size_ratio
+
+      crop_from_x, crop_from_y = essence.crop_from.split('x').map(&:to_i)
+      new_crop_height, new_crop_width = crop_height, crop_width
+      new_crop_from_x, new_crop_from_y = crop_from_x, crop_from_y
+
+      if size_ratio > crop_ratio # new size wider => offset y and reduce height
+        new_crop_height = crop_height * (crop_ratio / size_ratio)
+        new_crop_from_y = crop_from_y + (crop_height - new_crop_height) / 2
+      else # offset x and and reduce width
+        new_crop_width = (crop_width * (size_ratio / crop_ratio))
+        new_crop_from_x = crop_from_x + (crop_width - new_crop_width) / 2
+      end
+
+      new_crop_from = "#{new_crop_from_x.to_i}x#{new_crop_from_y.to_i}"
+      new_crop_size = "#{new_crop_width.to_i}x#{new_crop_height.to_i}"
+
+      [new_crop_from, new_crop_size]
     end
   end
 end
