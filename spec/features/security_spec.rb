9301a0610fd2b8fa4a35c20d2d2ca205e820076f
Thomas von Deyen <thomas@vondeyen.com>
2020-05-04 23:31:27 +0200

    Remove enforce_ssl (#1804)
    Removes the enforce_ssl configuration and the SSLProtection module.
    
    Please use the Rails build in config.force_ssl or your Webserver
    for that.

--- spec/features/security_spec.rb
+++ spec/features/security_spec.rb
@@ -9,18 +9,4 @@ RSpec.describe "Security.", type: :system do
       expect(current_path).to eq(Alchemy.login_path)
     end
   end
-
-  context "If ssl is enforced" do
-    before do
-      allow_any_instance_of(Alchemy::BaseController)
-        .to receive(:ssl_required?)
-        .and_return(true)
-      authorize_user(:as_admin)
-    end
-
-    it "redirects every request to https." do
-      visit "/admin/dashboard"
-      expect(current_url).to eq("https://127.0.0.1/admin/dashboard")
-    end
-  end
 end
