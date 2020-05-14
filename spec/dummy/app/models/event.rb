8ff8ca28854ceefe8f56825c22124fc08bfe3ce0
Martin Meyerhoff <mamhoff@gmail.com>
2020-05-14 14:23:52 +0200

    Add error flash to resource controller (#1827)
    This adds an error flash to the resource controller if some action does
    not succeed.

--- spec/dummy/app/models/event.rb
+++ spec/dummy/app/models/event.rb
@@ -5,6 +5,7 @@ class Event < ActiveRecord::Base
 
   validates_presence_of :name
   belongs_to :location
+  before_destroy :abort_if_name_is_undestructible
 
   scope :starting_today, -> { where(starts_at: Time.current.at_midnight..Date.tomorrow.at_midnight) }
   scope :future, -> { where("starts_at > ?", Date.tomorrow.at_midnight) }
@@ -17,5 +18,14 @@ class Event < ActiveRecord::Base
 
   def self.alchemy_resource_filters
     %w(starting_today future)
+  end
+
+  private
+
+  def abort_if_name_is_undestructible
+    if name == "Undestructible"
+      errors.add(:base, "This is the undestructible event!")
+      throw(:abort)
+    end
   end
 end
