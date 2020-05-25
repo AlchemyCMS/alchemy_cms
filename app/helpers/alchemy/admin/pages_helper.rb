68696e034cdd20fdbe01417f0c79a756691f31ad
Thomas von Deyen <thomas@vondeyen.com>
2020-05-25 13:32:22 +0200

    Allow page visible toggle (4.6) (#1838)
    * Use translated menu name in page form
    
    * Format admin pages helper with Rufo
    
    * Allow to pass a label to page_status_checkox helper
    
    * Only check for menus having same language as page
    
    We should only check if any menus are present to attach a page to for the same language.
    
    * Allow to toggle visible on pages attached to menus
    
    The Page#visible status is used to build the urlname of child pages. If a page is attached to a menu we should still be able to toggle that flag.
    
    * Add active button style
    
    * Reduce horizontal padding of small button

--- app/helpers/alchemy/admin/pages_helper.rb
+++ app/helpers/alchemy/admin/pages_helper.rb
@@ -9,13 +9,13 @@ module Alchemy
       #
       def preview_sizes_for_select
         options_for_select([
-          'auto',
-          [Alchemy.t('240', scope: 'preview_sizes'), 240],
-          [Alchemy.t('320', scope: 'preview_sizes'), 320],
-          [Alchemy.t('480', scope: 'preview_sizes'), 480],
-          [Alchemy.t('768', scope: 'preview_sizes'), 768],
-          [Alchemy.t('1024', scope: 'preview_sizes'), 1024],
-          [Alchemy.t('1280', scope: 'preview_sizes'), 1280]
+          "auto",
+          [Alchemy.t("240", scope: "preview_sizes"), 240],
+          [Alchemy.t("320", scope: "preview_sizes"), 320],
+          [Alchemy.t("480", scope: "preview_sizes"), 480],
+          [Alchemy.t("768", scope: "preview_sizes"), 768],
+          [Alchemy.t("1024", scope: "preview_sizes"), 1024],
+          [Alchemy.t("1280", scope: "preview_sizes"), 1280],
         ])
       end
 
@@ -27,30 +27,30 @@ module Alchemy
         if page.persisted? && page.definition.blank?
           [
             page_layout_missing_warning,
-            Alchemy.t(:page_type)
-          ].join('&nbsp;').html_safe
+            Alchemy.t(:page_type),
+          ].join("&nbsp;").html_safe
         else
           Alchemy.t(:page_type)
         end
       end
 
-      def page_status_checkbox(page, attribute)
-        label = page.class.human_attribute_name(attribute)
+      def page_status_checkbox(page, attribute, label: nil)
+        label_text = label || page.class.human_attribute_name(attribute)
 
         if page.attribute_fixed?(attribute)
           checkbox = check_box(:page, attribute, disabled: true)
-          hint = content_tag(:span, class: 'hint-bubble') do
+          hint = content_tag(:span, class: "hint-bubble") do
             Alchemy.t(:attribute_fixed, attribute: attribute)
           end
-          content = content_tag(:span, class: 'with-hint') do
-            "#{checkbox}\n#{label}\n#{hint}".html_safe
+          content = content_tag(:span, class: "with-hint") do
+            "#{checkbox}\n#{label_text}\n#{hint}".html_safe
           end
         else
           checkbox = check_box(:page, attribute)
-          content = "#{checkbox}\n#{label}".html_safe
+          content = "#{checkbox}\n#{label_text}".html_safe
         end
 
-        content_tag(:label, class: 'checkbox') { content }
+        content_tag(:label, class: "checkbox") { content }
       end
     end
   end
