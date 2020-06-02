86e59ccf530b0c2b621488d72e321e898b8cd79d
Thomas von Deyen <thomas@vondeyen.com>
2020-06-02 23:45:26 +0200

    Use depth for page tree serializer root_or_leaf (#1864)
    Depth is a cached attribute on the page that does not need calculation and will always be correct even if the root page has been removed.
    
    This commit helps to migrate to Alchemy 5 without making further changes to the page tree serializer. Without that change page trees that already have been migrated to "root-page-less" Alchemy 5 will have page toggle switches disappear in the first level.

--- app/serializers/alchemy/page_tree_serializer.rb
+++ app/serializers/alchemy/page_tree_serializer.rb
@@ -60,8 +60,8 @@ module Alchemy
         urlname: page.urlname,
         url_path: page.url_path,
         level: level,
-        root: level == 1,
-        root_or_leaf: level == 1 || !has_children,
+        root: page.depth == 1,
+        root_or_leaf: page.depth == 1 || !has_children,
         children: [],
       }
 
