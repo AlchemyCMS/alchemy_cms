8aa21c7b9081bfcde5e4ebf46bb366d30dba084d
Thomas von Deyen <thomas@vondeyen.com>
2020-06-02 22:34:33 +0200

    Show url name in Page tree (#1856)
    * Show url_path of page in admin page tree
    
    The url_path of a page is useful information. We should always display it not only if the page redirects to external.
    
    * Format sitemap.scss with Prettier
    
    * Change the sitemap urlname background color
    
    The old color was too prominent for that information.
    
    * Show a legend on top of admin page trees
    
    * Use a left aligned column layout for page tree
    
    * Slighty more space for node page name and url
    
    * Eager load associated records in page tree serializer
    
    Now that we need the language and its site we should eager load them in the page tree serializer. Also the locker of a page should be eager loaded.
    
    On a site with ~300 pages loading the page tree went from 722ms to 221ms on my machine.

--- spec/requests/alchemy/admin/pages_controller_spec.rb
+++ spec/requests/alchemy/admin/pages_controller_spec.rb
@@ -106,6 +106,8 @@ module Alchemy
           expect(page['name']).to eq(page_1.name)
           expect(page).to have_key('children')
           expect(page['children'].count).to eq(1)
+          expect(page).to have_key("url_path")
+          expect(page["url_path"]).to eq(page_1.url_path)
 
           page = page['children'].first
 
@@ -115,6 +117,8 @@ module Alchemy
           expect(page['name']).to eq(page_2.name)
           expect(page).to have_key('children')
           expect(page['children'].count).to eq(1)
+          expect(page).to have_key("url_path")
+          expect(page["url_path"]).to eq(page_2.url_path)
 
           page = page['children'].first
 
@@ -124,6 +128,8 @@ module Alchemy
           expect(page['name']).to eq(page_3.name)
           expect(page).to have_key('children')
           expect(page['children'].count).to eq(0)
+          expect(page).to have_key("url_path")
+          expect(page["url_path"]).to eq(page_3.url_path)
         end
 
         context "when branch is folded" do
