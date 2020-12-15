f431c23e2543d937755ecac37e51284829f56e9a
Thomas von Deyen <thomas@vondeyen.com>
2020-12-15 18:42:16 +0100

    Return elements from public versions in API
    Only elements that have a public version should be returned
    by the API.

--- lib/alchemy/test_support/factories/page_version_factory.rb
+++ lib/alchemy/test_support/factories/page_version_factory.rb
@@ -3,5 +3,9 @@
 FactoryBot.define do
   factory :alchemy_page_version, class: "Alchemy::PageVersion" do
     association :page, factory: :alchemy_page
+
+    trait :published do
+      public_on { Time.current }
+    end
   end
 end
