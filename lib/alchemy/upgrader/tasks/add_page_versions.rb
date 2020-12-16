e20131f111c9523303c65cff5e2d019a914f7822
Thomas von Deyen <thomas@vondeyen.com>
2020-12-16 21:50:33 +0100

    Add 6.0 upgrade task

--- lib/alchemy/upgrader/tasks/add_page_versions.rb
+++ lib/alchemy/upgrader/tasks/add_page_versions.rb
@@ -7,8 +7,7 @@ module Alchemy::Upgrader::Tasks
     include Thor::Actions
 
     no_tasks do
-      def create_public_page_versions_for_pages
-        puts "Create public page versions for pages.\n"
+      def create_public_page_versions
         Alchemy::Page.where.not(public_on: nil).find_each do |page|
           next if page.versions.published.any?
 
@@ -17,15 +16,14 @@ module Alchemy::Upgrader::Tasks
               public_on: page.public_on,
               public_until: page.public_until
             ).tap do |version|
-              Alchemy::Element.where(page_id: page.id).not_nested.available.order(:position).find_each do |element|
+              Alchemy::Element.where(page_version_id: page.draft_version.id).not_nested.available.order(:position).find_each do |element|
                 Alchemy::Element.copy(element, page_version_id: version.id)
               end
             end
           end
+
           print "."
         end
-
-        puts "\nDone."
       end
     end
   end
