a76b8a42db4fbce438fe0dec6209913747b3b2c3
Thomas Stockschläder <thomas@modevo.de>
2020-12-21 13:59:23 +0100

    Add element definition api (based on PageLayout definitions)

--- app/models/alchemy/element/definitions.rb
+++ app/models/alchemy/element/definitions.rb
@@ -13,41 +13,25 @@ module Alchemy
       # your own set of elements
       #
       def definitions
-        @definitions ||= read_definitions_file.map(&:with_indifferent_access)
+        ElementDefinition.all
       end
 
       # Returns one element definition by given name.
       #
       def definition_by_name(name)
-        definitions.detect { |d| d["name"] == name }
-      end
-
-      private
-
-      # Reads the element definitions file named +elements.yml+ from +config/alchemy/+ folder.
-      #
-      def read_definitions_file
-        if ::File.exist?(definitions_file_path)
-          ::YAML.safe_load(ERB.new(File.read(definitions_file_path)).result, YAML_WHITELIST_CLASSES, [], true) || []
-        else
-          raise LoadError, "Could not find elements.yml file! Please run `rails generate alchemy:install`"
-        end
-      end
-
-      # Returns the +elements.yml+ file path
-      #
-      def definitions_file_path
-        Rails.root.join "config/alchemy/elements.yml"
+        ElementDefinition.get(name)
       end
     end
 
     # The definition of this element.
     #
     def definition
-      if definition = self.class.definitions.detect { |d| d["name"] == name }
+      if definition = self.class.definition_by_name(name)
         definition
       else
-        log_warning "Could not find element definition for #{name}. Please check your elements.yml file!"
+        log_warning "Could not find element definition for #{
+                      name
+                    }. Please check your elements.yml file!"
         {}
       end
     end
