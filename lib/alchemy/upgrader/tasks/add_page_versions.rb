12247aac5a938e6bb2af52bdf0311201461dc4e6
Thomas von Deyen <thomas@vondeyen.com>
2021-02-12 17:35:11 +0100

    Adjust upgrader to page legacy timestamps

--- lib/alchemy/upgrader/tasks/add_page_versions.rb
+++ lib/alchemy/upgrader/tasks/add_page_versions.rb
@@ -8,22 +8,24 @@ module Alchemy::Upgrader::Tasks
 
     no_tasks do
       def create_public_page_versions
-        Alchemy::Page.where.not(public_on: nil).find_each do |page|
-          next if page.versions.published.any?
+        Alchemy::Deprecation.silence do
+          Alchemy::Page.where.not(legacy_public_on: nil).find_each do |page|
+            next if page.versions.published.any?
 
-          Alchemy::Page.transaction do
-            page.versions.create!(
-              public_on: page.public_on,
-              public_until: page.public_until
-            ).tap do |version|
-              # We must not use .find_each here to not mess up the order of elements
-              page.draft_version.elements.not_nested.available.each do |element|
-                Alchemy::Element.copy(element, page_version_id: version.id)
+            Alchemy::Page.transaction do
+              page.versions.create!(
+                public_on: page.legacy_public_on,
+                public_until: page.legacy_public_until
+              ).tap do |version|
+                # We must not use .find_each here to not mess up the order of elements
+                page.draft_version.elements.not_nested.available.each do |element|
+                  Alchemy::Element.copy(element, page_version_id: version.id)
+                end
               end
             end
-          end
 
-          print "."
+            print "."
+          end
         end
       end
     end
