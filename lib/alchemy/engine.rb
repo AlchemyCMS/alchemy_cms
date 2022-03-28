3725e33809b746d70da7e5a8bc61fe8077fbcec2
Thomas von Deyen <thomas@vondeyen.com>
2022-03-28 14:48:09 +0200

    Register webp as mime type
    Rails 6.1.5 does not have a image/webp mime type registered.
    
    # Conflicts:
    #	lib/alchemy/engine.rb

--- lib/alchemy/engine.rb
+++ lib/alchemy/engine.rb
@@ -45,6 +45,17 @@ module Alchemy
       end
     end
 
+    initializer "alchemy.webp-mime_type" do
+      # Rails does not know anything about webp even in 2022
+      unless Mime::Type.lookup_by_extension(:webp)
+        Mime::Type.register("image/webp", :webp)
+      end
+      # Dragonfly uses Rack to read the mime type and guess what
+      unless Rack::Mime::MIME_TYPES[".webp"]
+        Rack::Mime::MIME_TYPES[".webp"] = "image/webp"
+      end
+    end
+
     initializer "alchemy.error_tracking" do
       if defined?(Airbrake)
         require_relative "error_tracking/airbrake_handler"
