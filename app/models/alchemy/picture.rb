5d4bbb8a2e4ad30d994bdd143e9847960438dd7b
Thomas von Deyen <thomas@vondeyen.com>
2022-04-12 17:56:39 +0200

    Use image_file_extension as original format
    ActiveStorage expects an extension and not an mime type.

--- app/models/alchemy/picture.rb
+++ app/models/alchemy/picture.rb
@@ -174,7 +174,7 @@ module Alchemy
 
       url(
         flatten: true,
-        format: image_file_format || "jpg",
+        format: image_file_extension || "jpg",
         size: size
       )
     end
@@ -208,18 +208,12 @@ module Alchemy
       end
     end
 
-    # Returns the suffix of the filename.
-    #
-    def suffix
-      image_file.ext
-    end
-
     # Returns a humanized, readable name from image filename.
     #
     def humanized_name
       return "" if image_file_name.blank?
 
-      convert_to_humanized_name(image_file_name, suffix)
+      convert_to_humanized_name(image_file_name, image_file_extension)
     end
 
     # Returns the format the image should be rendered with
@@ -231,7 +225,7 @@ module Alchemy
       if convertible?
         Alchemy.config.get(:image_output_format)
       else
-        image_file_format
+        image_file_extension
       end
     end
 
@@ -269,6 +263,13 @@ module Alchemy
     def deletable?
       picture_ingredients.empty?
     end
+
+    def image_file_extension
+      image_file&.filename&.extension
+    end
+
+    alias_method :suffix, :image_file_extension
+    deprecate suffix: :image_file_extension, deprecator: Alchemy::Deprecation
 
     # A size String from original image file values.
     #
