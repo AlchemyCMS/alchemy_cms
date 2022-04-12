0bc3bd0e65b16448224bf11edb4b4ad25e8e996f
Thomas von Deyen <thomas@vondeyen.com>
2022-04-12 17:56:28 +0200

    Add activestorage

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -10,6 +10,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "active_job/railtie"
+require "active_storage/engine"
 # require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
