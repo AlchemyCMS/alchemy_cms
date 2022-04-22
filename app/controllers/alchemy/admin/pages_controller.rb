2c7e111f2178a437e89d5234d9bf057d475c2074
Martin Meyerhoff <mamhoff@gmail.com>
2022-04-22 22:35:51 +0200

    Preload page includes in Admin::Admin::PagesController
    Here, we need to preload the draft version because that's what we look
    at.

--- app/controllers/alchemy/admin/pages_controller.rb
+++ app/controllers/alchemy/admin/pages_controller.rb
@@ -324,7 +324,7 @@ module Alchemy
       end
 
       def load_resource
-        @page = Page.find(params[:id])
+        @page = Page.includes(page_includes).find(params[:id])
       end
 
       def pages_from_raw_request
@@ -401,6 +401,23 @@ module Alchemy
 
       def set_preview_mode
         @preview_mode = true
+      end
+
+      def page_includes
+        {
+          :tags,
+          language: :site,
+          draft_version: {
+            elements: [
+              :page,
+              :touchable_pages,
+              {
+                ingredients: :related_object,
+                contents: :essence,
+              },
+            ],
+          },
+        }
       end
     end
   end
