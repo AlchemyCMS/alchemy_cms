e60efce154a6532f995dad55a489174811252709
Antonio Facciolo <afdev82@gmail.com>
2022-07-22 12:01:40 +0200

    set migrations from Rails 6.0

--- db/migrate/20220514072456_restrict_on_delete_page_id_foreign_key_from_alchemy_nodes.rb
+++ db/migrate/20220514072456_restrict_on_delete_page_id_foreign_key_from_alchemy_nodes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class RestrictOnDeletePageIdForeignKeyFromAlchemyNodes < ActiveRecord::Migration[6.1]
+class RestrictOnDeletePageIdForeignKeyFromAlchemyNodes < ActiveRecord::Migration[6.0]
   def up
     remove_foreign_key :alchemy_nodes, :alchemy_pages
     add_foreign_key :alchemy_nodes, :alchemy_pages, column: :page_id, on_delete: :restrict
--- spec/dummy/db/migrate/20220514072456_restrict_on_delete_page_id_foreign_key_from_alchemy_nodes.rb
+++ spec/dummy/db/migrate/20220514072456_restrict_on_delete_page_id_foreign_key_from_alchemy_nodes.rb
@@ -1,4 +1,4 @@
-class RestrictOnDeletePageIdForeignKeyFromAlchemyNodes < ActiveRecord::Migration[6.1]
+class RestrictOnDeletePageIdForeignKeyFromAlchemyNodes < ActiveRecord::Migration[6.0]
   def up
     remove_foreign_key :alchemy_nodes, :alchemy_pages
     add_foreign_key :alchemy_nodes, :alchemy_pages, column: :page_id, on_delete: :restrict
