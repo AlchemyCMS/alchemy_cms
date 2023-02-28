f83fdc1af82ba8c1f05b68cc2ed0a378634d1054
Antonio Facciolo <afdev82@gmail.com>
2023-02-28 15:27:09 +0100

    fix rubocop complaints

--- lib/non_stupid_digest_assets.rb
+++ lib/non_stupid_digest_assets.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "sprockets/manifest"
 
 module NonStupidDigestAssets
@@ -7,22 +9,23 @@ module NonStupidDigestAssets
   class << self
     def assets(assets)
       return assets if whitelist.empty?
+
       whitelisted_assets(assets)
     end
 
     private
 
     def whitelisted_assets(assets)
-      assets.select do |logical_path, digest_path|
+      assets.select do |logical_path, _digest_path|
         whitelist.any? do |item|
-          item === logical_path
+          item == logical_path
         end
       end
     end
   end
 
   module CompileWithNonDigest
-    def compile *args
+    def compile(*args)
       paths = super
       NonStupidDigestAssets.assets(assets).each do |(logical_path, digest_path)|
         full_digest_path = File.join dir, digest_path
@@ -48,4 +51,4 @@ module NonStupidDigestAssets
   end
 end
 
-Sprockets::Manifest.send(:prepend, NonStupidDigestAssets::CompileWithNonDigest)
+Sprockets::Manifest.prepend NonStupidDigestAssets::CompileWithNonDigest
