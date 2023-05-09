92d9d66c6f0a36cd6b172696b55d405296e9c5e6
Thomas von Deyen <thomas@vondeyen.com>
2023-05-09 18:51:00 +0200

    Add Ingredients::PageView component

---
+++ app/components/alchemy/ingredients/page_view.rb
@@ -0,0 +1,15 @@
+module Alchemy
+  module Ingredients
+    class PageView < BaseView
+      delegate :page, to: :ingredient
+
+      def call
+        link_to page.name, alchemy.show_page_path(urlname: page.urlname)
+      end
+
+      def render?
+        !!page
+      end
+    end
+  end
+end
