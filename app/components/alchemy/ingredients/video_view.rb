0f2e4dc93a05807b8131dcb4b2118940f215af8f
Thomas von Deyen <thomas@vondeyen.com>
2023-05-10 09:03:32 +0200

    Add Ingredients::VideoView component

---
+++ app/components/alchemy/ingredients/video_view.rb
@@ -0,0 +1,39 @@
+module Alchemy
+  module Ingredients
+    class VideoView < BaseView
+      delegate :attachment, to: :ingredient
+
+      def call
+        content_tag(:video, html_options) do
+          tag(:source, src: src, type: attachment.file_mime_type)
+        end
+      end
+
+      def render?
+        !attachment.nil?
+      end
+
+      private
+
+      def src
+        alchemy.show_attachment_path(
+          attachment,
+          format: attachment.suffix
+        )
+      end
+
+      def html_options
+        {
+          controls: ingredient.controls,
+          autoplay: ingredient.autoplay,
+          loop: ingredient.loop,
+          muted: ingredient.muted,
+          playsinline: ingredient.playsinline,
+          preload: ingredient.preload.presence,
+          width: ingredient.width.presence,
+          height: ingredient.height.presence
+        }
+      end
+    end
+  end
+end
