04d98ac0d48462b0bcf12c7a2163da9ebb025311
Thomas von Deyen <thomas@vondeyen.com>
2023-05-18 10:04:45 +0200

    style: Autofix lint errors

--- app/controllers/alchemy/pages_controller.rb
+++ app/controllers/alchemy/pages_controller.rb
@@ -122,7 +122,7 @@ module Alchemy
 
       @page ||= Language.current.pages.contentpages.find_by(
         urlname: params[:urlname],
-        language_code: params[:locale] || Language.current.code,
+        language_code: params[:locale] || Language.current.code
       )
     end
 
