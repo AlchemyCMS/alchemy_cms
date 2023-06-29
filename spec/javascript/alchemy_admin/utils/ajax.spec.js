4644b756898b1fcf4087c80c853ae7501d9ed48d
Thomas von Deyen <thomas@vondeyen.com>
2023-06-29 09:43:01 +0200

    Move JS spec files into spec folder
    We pinned all JS files under app/javascript into importmap
    and do not want to load test files.

--- spec/javascript/alchemy_admin/utils/ajax.spec.js
+++ spec/javascript/alchemy_admin/utils/ajax.spec.js
@@ -1,5 +1,9 @@
 import xhrMock from "xhr-mock"
-import { get, patch, post } from "../ajax"
+import {
+  get,
+  patch,
+  post
+} from "../../../../app/javascript/alchemy_admin/utils/ajax"
 
 const token = "s3cr3t"
 
