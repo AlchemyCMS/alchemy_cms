AlchemyCMS/alchemy_cms

commit c98605a1f49ac7607654ac6a4b7b1e79a1004af3

Fix author edit_content permissions
We were missing a third argument in order to be able to use this
permission with the `.accessible_by` scope.

app/controllers/alchemy/api/pages_controller.rb
@@ -8,14 +8,9 @@ module Alchemy
     # Returns all pages as json object
     #
     def index
-      # Fix for cancancan not able to merge multiple AR scopes for logged in users
-      if can? :edit_content, Alchemy::Page
-        @pages = Alchemy::Page.all
-      else
-        language = Alchemy::Language.find_by(id: params[:language_id]) || Alchemy::Language.current
-        @pages = Alchemy::Page.accessible_by(current_ability, :index)
-        @pages = @pages.where(language: language)
-      end
+      language = Alchemy::Language.find_by(id: params[:language_id]) || Alchemy::Language.current
+      @pages = Alchemy::Page.accessible_by(current_ability, :index)
+      @pages = @pages.where(language: language)
       @pages = @pages.includes(*page_includes)
       @pages = @pages.ransack(params[:q]).result
 

lib/alchemy/permissions.rb
@@ -121,7 +121,9 @@ module Alchemy
         can :manage, Alchemy::Node
         can [:read, :url], Alchemy::Picture
         can [:read, :autocomplete], Alchemy::Tag
-        can(:edit_content, Alchemy::Page) { |p| p.editable_by?(@user) }
+        can :edit_content, Alchemy::Page, Alchemy::Page.all do |page|
+          page.editable_by?(@user)
+        end
       end
     end
 

spec/controllers/alchemy/api/pages_controller_spec.rb
@@ -85,9 +85,10 @@ module Alchemy
           let(:site_2) { create(:alchemy_site) }
           let(:language_2) { create(:alchemy_language, site: site_2) }
           let!(:site_2_page) { create(:alchemy_page, :public, language: language_2) }
+          let!(:unpublished_page) { create(:alchemy_page, language: default_language) }
 
           context "as guest user" do
-            it "only returns pages for current site" do
+            it "only returns public pages for current site" do
               get :index, format: :json
               expect(result["pages"].map { |r| r["id"] }).to match_array([
                 page.parent_id,
@@ -101,13 +102,12 @@ module Alchemy
               authorize_user(build(:alchemy_dummy_user, :as_author))
             end
 
-            it "returns all pages" do
+            it "returns all pages for current site" do
               get :index, format: :json
               expect(result["pages"].map { |r| r["id"] }).to match_array([
                 page.parent_id,
                 page.id,
-                site_2_page.parent_id,
-                site_2_page.id,
+                unpublished_page.id,
               ])
             end
           end

commit dfb17f95bf59a810c4be21cb514ef11ec21bcf75

Allow authors to link to all pages again
This fixes an issue introduced in c98605a1f49ac7607654ac6a4b7b1e79a1004af3

Closes #2500

app/controllers/alchemy/api/pages_controller.rb
@@ -8,9 +8,7 @@ module Alchemy
     # Returns all pages as json object
     #
     def index
-      language = Alchemy::Language.find_by(id: params[:language_id]) || Alchemy::Language.current
       @pages = Alchemy::Page.accessible_by(current_ability, :index)
-      @pages = @pages.where(language: language)
       @pages = @pages.includes(*page_includes)
       @pages = @pages.ransack(params[:q]).result
 

lib/alchemy/permissions.rb
@@ -41,8 +41,8 @@ module Alchemy
           e.public? && !e.restricted?
         end
 
-        can :read, Alchemy::Page, Alchemy::Page.published.not_restricted do |p|
-          p.public? && !p.restricted?
+        can :read, Alchemy::Page, Alchemy::Page.published.not_restricted.from_current_site do |p|
+          p.public? && !p.restricted? && p.site == Alchemy::Site.current
         end
       end
     end
@@ -64,8 +64,8 @@ module Alchemy
           e.public?
         end
 
-        can :read, Alchemy::Page, Alchemy::Page.published do |p|
-          p.public?
+        can :read, Alchemy::Page, Alchemy::Page.published.from_current_site do |p|
+          p.public? && p.site == Alchemy::Site.current
         end
       end
     end

spec/controllers/alchemy/api/pages_controller_spec.rb
@@ -86,13 +86,29 @@ module Alchemy
           let(:language_2) { create(:alchemy_language, site: site_2) }
           let!(:site_2_page) { create(:alchemy_page, :public, language: language_2) }
           let!(:unpublished_page) { create(:alchemy_page, language: default_language) }
+          let!(:restricted_page) { create(:alchemy_page, :public, :restricted, language: default_language) }
 
           context "as guest user" do
-            it "only returns public pages for current site" do
+            it "only returns published unrestricted pages for current site" do
               get :index, format: :json
               expect(result["pages"].map { |r| r["id"] }).to match_array([
                 page.parent_id,
                 page.id
+              ])
+            end
+          end
+
+          context "as member user" do
+            before do
+              authorize_user(build(:alchemy_dummy_user))
+            end
+
+            it "only returns all published pages for current site" do
+              get :index, format: :json
+              expect(result["pages"].map { |r| r["id"] }).to match_array([
+                page.parent_id,
+                page.id,
+                restricted_page.id
               ])
             end
           end
@@ -102,38 +118,9 @@ module Alchemy
               authorize_user(build(:alchemy_dummy_user, :as_author))
             end
 
-            it "returns all pages for current site" do
+            it "returns all pages" do
               get :index, format: :json
-              expect(result["pages"].map { |r| r["id"] }).to match_array([
-                page.parent_id,
-                page.id,
-                unpublished_page.id
-              ])
-            end
-          end
-        end
-
-        context "with language_id param" do
-          subject { get :index, params: {format: :json, language_id: try(:language)&.id} }
-
-          let!(:default_language_page) { create(:alchemy_page, :public, language: default_language, name: "same-name") }
-
-          context "when a language with that id exists" do
-            let(:language) { create(:alchemy_language, :klingon) }
-            let!(:default_language_page) { create(:alchemy_page, :public, language: default_language, name: "same-name") }
-            let!(:klingon_page) { create(:alchemy_page, :public, language: language, name: "same-name") }
-
-            it "only returns results from that language" do
-              subject
-              expect(result["pages"].map { |r| r["id"] }).to match_array [language.pages.root.id, klingon_page.id]
-            end
-          end
-
-          context "when a language with that id does not exist" do
-            it "uses the default language" do
-              subject
-              expect(result["pages"].map { |r| r["id"] }).to include(default_language_page.id)
-              expect(result["pages"].map { |r| r["language_code"] }).not_to include("kl")
+              expect(result["pages"].map { |r| r["id"] }).to match_array(Alchemy::Page.pluck(:id))
             end
           end
         end