eca53a352dcbca3dade37db96c36b9c60d9ddb6b
Sascha Karnatz <122262394+sascha-karnatz@users.noreply.github.com>
2023-08-30 10:41:07 +0200

    Migrate picture selector from jquery to vanilla JS
    try to reduce the jQuery usage and migrate the picture selector section from jQuery to vanilla Javascript.

--- app/javascript/alchemy_admin/picture_selector.js
+++ app/javascript/alchemy_admin/picture_selector.js
@@ -1,31 +1,37 @@
+import { on } from "alchemy_admin/utils/events"
+
 /**
  * Multiple picture select handler for the picture archive.
  */
 export default function PictureSelector() {
-  const $selected_item_tools = $(".selected_item_tools")
-  const $picture_selects = $(".picture_tool.select input")
+  const selectedItemTools = document.querySelector(".selected_item_tools")
+  const checkedInputs = () =>
+    document.querySelectorAll("#picture_archive input:checked")
 
-  $picture_selects.on("change", function () {
-    if ($picture_selects.filter(":checked").size() > 0) {
-      $selected_item_tools.show()
-    } else {
-      $selected_item_tools.hide()
-    }
+  // make the item toolbar visible and show the checkbox also if it is not hovered anymore
+  on("change", ".picture_tool.select", "input", (event) => {
+    selectedItemTools.style.display =
+      checkedInputs().length > 0 ? "block" : "none"
 
-    if (this.checked) {
-      $(this).parent().addClass("visible").removeClass("hidden")
-    } else {
-      $(this).parent().removeClass("visible").addClass("hidden")
-    }
+    const parentElementClassList = event.target.parentElement.classList
+    const checked = event.target.checked
+
+    parentElementClassList.toggle("visible", checked)
+    parentElementClassList.toggle("hidden", !checked)
   })
 
-  $("a#edit_multiple_pictures").on("click", function (e) {
-    const $this = $(this)
-    const picture_ids = $("input:checkbox", "#picture_archive").serialize()
-    const url = $this.attr("href") + "?" + picture_ids
+  // open the edit view in a dialog modal
+  on("click", ".selected_item_tools", "a#edit_multiple_pictures", (event) => {
+    event.preventDefault()
+
+    const searchParameters = new URLSearchParams()
+    checkedInputs().forEach((entry) =>
+      searchParameters.append(entry.name, entry.value)
+    )
+    const url = event.target.href + "?" + searchParameters.toString()
 
     Alchemy.openDialog(url, {
-      title: $this.attr("title"),
+      title: event.target.title,
       size: "400x295"
     })
   })
