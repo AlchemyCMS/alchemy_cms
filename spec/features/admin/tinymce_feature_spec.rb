bd50facd4b6c3a25d4cb12ca67d80cbcf204435d
Thomas von Deyen <thomas@vondeyen.com>
2023-11-23 21:25:34 +0100

    Preload tinymce assets
    Tinymce loads the editor content css and its plugins on initialize.
    Since we init tinymce only when its visible it takes long to init the
    editor. Using browser preload features to make sure the assets
    are already loaded.

--- spec/features/admin/tinymce_feature_spec.rb
+++ spec/features/admin/tinymce_feature_spec.rb
@@ -29,4 +29,56 @@ RSpec.describe "TinyMCE Editor", type: :system do
       )
     end
   end
+
+  describe "assets preloading" do
+    it "should preload assets" do
+      visit admin_dashboard_path
+      expect(page)
+        .to have_css('link[rel="preload"][href="/assets/tinymce/skins/alchemy/skin.min.css"]')
+        .and have_css('link[rel="preload"][href="/assets/tinymce/skins/alchemy/content.min.css"]')
+    end
+
+    context "with asset host" do
+      around do |example|
+        host = ActionController::Base.config.asset_host
+        ActionController::Base.config.asset_host = "https://myhost.com"
+        example.run
+        ActionController::Base.config.asset_host = host
+      end
+
+      it "should preload assets from host" do
+        visit admin_dashboard_path
+        expect(page)
+          .to have_css('link[rel="preload"][href="https://myhost.com/assets/tinymce/skins/alchemy/skin.min.css"]')
+          .and have_css('link[rel="preload"][href="https://myhost.com/assets/tinymce/skins/alchemy/content.min.css"]')
+      end
+    end
+
+    context "when content_css is configured" do
+      before do
+        Alchemy::Tinymce.init = {content_css: "/assets/custom-stylesheet.css"}
+      end
+
+      it "should preload it" do
+        visit admin_dashboard_path
+        expect(page)
+          .to have_css('link[rel="preload"][href="/assets/custom-stylesheet.css"]')
+      end
+
+      context "with asset host" do
+        around do |example|
+          host = ActionController::Base.config.asset_host
+          ActionController::Base.config.asset_host = "https://myhost.com"
+          example.run
+          ActionController::Base.config.asset_host = host
+        end
+
+        it "should preload it from host" do
+          visit admin_dashboard_path
+          expect(page)
+            .to have_css('link[rel="preload"][href="https://myhost.com/assets/custom-stylesheet.css"]')
+        end
+      end
+    end
+  end
 end
