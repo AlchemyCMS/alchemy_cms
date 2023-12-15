f6d98254b39979eb528d80d7410fd46037c13403
Thomas von Deyen <thomas@vondeyen.com>
2023-12-15 11:38:09 +0100

    Refactor SortableElements
    Move private functions into module. Remove the form token
    argument and use getToken() as well as get rid of the implicit
    this mutation jQuery does to set options on the fly.
    
    All of this to get rid of jquery.ui.sortable in the near future.

--- app/javascript/alchemy_admin/sortable_elements.js
+++ app/javascript/alchemy_admin/sortable_elements.js
@@ -1,8 +1,74 @@
-export default function SortableElements(page_id, form_token, selector) {
+import { getToken } from "alchemy_admin/utils/ajax"
+
+let initializedSortableElements = false
+
+function onStart(ui) {
+  const $sortable = ui.item.closest(".ui-sortable")
+  const name = ui.item.data("element-name")
+  const $dropzone = $(`[data-droppable-elements~='${name}']`)
+
+  $sortable.sortable("option", "connectWith", $dropzone)
+  $sortable.sortable("refresh")
+  $dropzone.css("minHeight", 36)
+  ui.item.addClass("dragged")
+
+  if (ui.item.hasClass("compact")) {
+    ui.placeholder.addClass("compact").css({
+      height: ui.item.outerHeight()
+    })
+  }
+}
+
+function onUpdate(event, ui, page_id) {
+  // This callback is called twice for both elements, the source and the receiving
+  // but, we only want to call ajax callback once on the receiving element.
+  if (initializedSortableElements) {
+    return
+  }
+
+  initializedSortableElements = true
+
+  const $sortable = ui.item.closest(".ui-sortable")
+  const element_ids = $.map($sortable.children(), (child) =>
+    $(child).attr("data-element-id")
+  )
+  const parent_element_id = ui.item
+    .parent()
+    .closest("[data-element-id]")
+    .data("element-id")
+  const params = {
+    page_id,
+    authenticity_token: getToken(),
+    element_ids
+  }
+
+  if (parent_element_id != null) {
+    params["parent_element_id"] = parent_element_id
+  }
+  $(event.target).css("cursor", "progress")
+  $.ajax({
+    url: Alchemy.routes.order_admin_elements_path,
+    type: "POST",
+    data: params,
+    complete() {
+      initializedSortableElements = false
+      $(event.target).css("cursor", "")
+    }
+  })
+}
+
+function onStop(ui) {
+  const name = ui.item.data("element-name")
+  const $dropzone = $(`[data-droppable-elements~='${name}']`)
+
+  $dropzone.css("minHeight", "")
+  ui.item.removeClass("dragged")
+}
+
+export default function SortableElements(page_id, selector) {
   if (selector == null) {
     selector = "#element_area .sortable-elements"
   }
-  let initializedSortableElements = false
   const $sortable_area = $(selector)
 
   const sortable_options = {
@@ -14,61 +80,9 @@ export default function SortableElements(page_id, form_token, selector) {
     cursor: "move",
     containment: $("#element_area"),
     tolerance: "pointer",
-    update(event, ui) {
-      // This callback is called twice for both elements, the source and the receiving
-      // but, we only want to call ajax callback once on the receiving element.
-      if (initializedSortableElements) {
-        return
-      }
-      const $this = ui.item.parent().closest(".ui-sortable")
-      let params = {}
-      initializedSortableElements = true
-      const element_ids = $.map($this.children(), (child) =>
-        $(child).attr("data-element-id")
-      )
-      const parent_element_id = ui.item
-        .parent()
-        .closest("[data-element-id]")
-        .data("element-id")
-      params = {
-        page_id,
-        authenticity_token: encodeURIComponent(form_token),
-        element_ids
-      }
-      if (parent_element_id != null) {
-        params["parent_element_id"] = parent_element_id
-      }
-      $(event.target).css("cursor", "progress")
-      $.ajax({
-        url: Alchemy.routes.order_admin_elements_path,
-        type: "POST",
-        data: params,
-        complete() {
-          initializedSortableElements = false
-          $(event.target).css("cursor", "")
-        }
-      })
-    },
-    start(_evt, ui) {
-      const $this = $(this)
-      const name = ui.item.data("element-name")
-      const $dropzone = $(`[data-droppable-elements~='${name}']`)
-      $this.sortable("option", "connectWith", $dropzone)
-      $this.sortable("refresh")
-      $dropzone.css("minHeight", 36)
-      ui.item.addClass("dragged")
-      if (ui.item.hasClass("compact")) {
-        ui.placeholder.addClass("compact").css({
-          height: ui.item.outerHeight()
-        })
-      }
-    },
-    stop(_evt, ui) {
-      const name = ui.item.data("element-name")
-      const $dropzone = $(`[data-droppable-elements~='${name}']`)
-      $dropzone.css("minHeight", "")
-      ui.item.removeClass("dragged")
-    }
+    start: (_evt, ui) => onStart(ui),
+    update: (evt, ui) => onUpdate(evt, ui, page_id),
+    stop: (_evt, ui) => onStop(ui)
   }
 
   $sortable_area.sortable(sortable_options)
