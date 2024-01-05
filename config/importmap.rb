623248a8cfba1fc565fecd1bf88051e8a4483cec
Thomas von Deyen <thomas@vondeyen.com>
2024-01-05 12:05:52 +0100

    Bundle all third party dependencies into vendor
    We already build our own optimized shoelace package.
    Since we already use yarn to handle dependencies we
    download all of our third party dependencies and build
    a packages for them and make them available via source
    map.
    
    That way we can make use of automated dependency updates
    (like dependabot) and automatically build the packages with
    github actions. That would be significantly harder with importmap-
    rails.

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,9 @@
-pin "@ungap/custom-elements", to: "https://ga.jspm.io/npm:@ungap/custom-elements@1.3.0/index.js", preload: true
-pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js", preload: true
-pin "sortablejs", to: "https://ga.jspm.io/npm:sortablejs@1.15.1/modular/sortable.esm.js", preload: true
+pin "@ungap/custom-elements", to: "ungap-custom-elements.min.js", preload: true # @1.3.0
+pin "flatpickr", to: "flatpickr.esm.js", preload: true # @4.6.13
+pin "sortablejs", to: "sortable.esm.js", preload: true # @1.15.1
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "shoelace", to: "shoelace.esm.js", preload: true
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.2/app/assets/javascripts/rails-ujs.esm.js", preload: true
+pin "@rails/ujs", to: "rails-ujs.esm.js", preload: true # @7.1.2
 
 pin "alchemy_admin", to: "alchemy_admin.js", preload: true
 pin_all_from File.expand_path("../app/javascript/alchemy_admin", __dir__), under: "alchemy_admin", preload: true
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,38 @@
 import resolve from "@rollup/plugin-node-resolve"
 
-export default {
-  input: "bundles/shoelace.js",
-  output: {
-    file: "vendor/javascript/shoelace.esm.js",
-    format: "esm"
+export default [
+  {
+    input: "node_modules/flatpickr/dist/esm/index.js",
+    output: {
+      file: "vendor/javascript/flatpickr.esm.js"
+    },
+    plugins: [resolve()],
+    context: "window"
   },
-  plugins: [resolve()]
-}
+  {
+    input: "node_modules/sortablejs/modular/sortable.esm.js",
+    output: {
+      file: "vendor/javascript/sortable.esm.js"
+    }
+  },
+  {
+    input: "node_modules/@ungap/custom-elements/min.js",
+    output: {
+      file: "vendor/javascript/ungap-custom-elements.min.js"
+    }
+  },
+  {
+    input: "node_modules/@rails/ujs/app/assets/javascripts/rails-ujs.esm.js",
+    output: {
+      file: "vendor/javascript/rails-ujs.esm.js"
+    }
+  },
+  {
+    input: "bundles/shoelace.js",
+    output: {
+      file: "vendor/javascript/shoelace.esm.js",
+      format: "esm"
+    },
+    plugins: [resolve()]
+  }
+]
