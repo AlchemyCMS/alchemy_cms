a1475fad3c160e3ddd7d4b9ff1526c10e1ad4a0f
Sascha Karnatz <122262394+sascha-karnatz@users.noreply.github.com>
2024-02-26 12:56:26 +0100

    Transform the tag selector into a web component
    it is still a Select2 select until we find a better alternative, but it is now abstracted into a web component and a View Component to make it easier usable and avoid initialization issues.

---
+++ app/javascript/alchemy_admin/components/tags_autocomplete.js
@@ -0,0 +1,57 @@
+class TagsAutocomplete extends HTMLElement {
+  connectedCallback() {
+    this.classList.add("autocomplete_tag_list")
+    $(this.input).select2(this.select2Config)
+  }
+
+  get input() {
+    return this.getElementsByTagName("input")[0]
+  }
+
+  get select2Config() {
+    return {
+      tags: true,
+      tokenSeparators: [","],
+      openOnEnter: false,
+      minimumInputLength: 1,
+      createSearchChoice: this.#createSearchChoice,
+      ajax: {
+        url: this.getAttribute("url"),
+        dataType: "json",
+        data: (term) => {
+          return { term }
+        },
+        results: (data) => {
+          return { results: data }
+        }
+      },
+      initSelection: this.#initSelection
+    }
+  }
+
+  #createSearchChoice(term, data) {
+    if (
+      $(data).filter(function () {
+        return this.text.localeCompare(term) === 0
+      }).length === 0
+    ) {
+      return {
+        id: term,
+        text: term
+      }
+    }
+  }
+
+  #initSelection(element, callback) {
+    const data = []
+    $(element.val().split(",")).each(function () {
+      data.push({
+        id: this.trim(),
+        text: this
+      })
+    })
+    callback(data)
+  }
+}
+
+customElements.define("alchemy-tags-autocomplete", TagsAutocomplete)
