fe52b3772bbd54f99b233542bd8d10929a8aa838
Thomas von Deyen <thomas@vondeyen.com>
2024-03-04 21:22:25 +0100

    Do not include timezone in datepickers only displaying date
    We must not include timezone information when storing date
    columns. It would lead to wrongly stored dates otherwise.
    
    (cherry picked from commit 3575840060149474f0d91b9f827d5b3dd30b65bc)

--- app/javascript/alchemy_admin/components/datepicker.js
+++ app/javascript/alchemy_admin/components/datepicker.js
@@ -13,14 +13,25 @@ class Datepicker extends AlchemyHTMLElement {
   }
 
   afterRender() {
+    this.flatpickr = flatpickr(
+      this.getElementsByTagName("input")[0],
+      this.flatpickrOptions
+    )
+  }
+
+  disconnected() {
+    this.flatpickr.destroy()
+  }
+
+  get flatpickrOptions() {
+    const enableTime = /time/.test(this.inputType)
     const options = {
       // alchemy_i18n supports `zh_CN` etc., but flatpickr only has two-letter codes (`zh`)
       locale: currentLocale().slice(0, 2),
       altInput: true,
       altFormat: translate(`formats.${this.inputType}`),
       altInputClass: "flatpickr-input",
-      dateFormat: "Z",
-      enableTime: /time/.test(this.inputType),
+      enableTime,
       noCalendar: this.inputType === "time",
       time_24hr: translate("formats.time_24hr"),
       onValueUpdate(_selectedDates, _dateStr, instance) {
@@ -28,11 +39,11 @@ class Datepicker extends AlchemyHTMLElement {
       }
     }
 
-    this.flatpickr = flatpickr(this.getElementsByTagName("input")[0], options)
-  }
+    if (enableTime) {
+      options.dateFormat = "Z"
+    }
 
-  disconnected() {
-    this.flatpickr.destroy()
+    return options
   }
 }
 
