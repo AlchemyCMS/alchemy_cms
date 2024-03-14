07904fe779e1b7f8eaf63e47d565ba0e9268dc55
Thomas von Deyen <thomas@vondeyen.com>
2024-03-14 21:56:05 +0100

    wip: multi language image descriptions

---
+++ app/controllers/alchemy/admin/picture_descriptions_controller.rb
@@ -0,0 +1,19 @@
+module Alchemy
+  module Admin
+    class PictureDescriptionsController < Alchemy::Admin::ResourcesController
+      def index
+        load_resource
+      end
+
+      def edit
+        @picture_description = @picture.descriptions.find_or_initialize_by(language_id: params[:language_id])
+      end
+
+      private
+
+      def load_resource
+        @picture = Alchemy::Picture.find(params[:picture_id])
+      end
+    end
+  end
+end
---
+++ spec/dummy/db/migrate/20240314110942_create_alchemy_picture_descriptions.alchemy.rb
@@ -0,0 +1,48 @@
+# This migration comes from alchemy (originally 20240314105244)
+class CreateAlchemyPictureDescriptions < ActiveRecord::Migration[7.0]
+  class Base < ActiveRecord::Base
+    self.abstract_class = true
+    self.table_name_prefix = "alchemy_"
+  end
+
+  class Site < Base
+  end
+
+  class Language < Base
+    belongs_to :site, class_name: "Site"
+    def self.default = where(site_id: Site.first).find_by(default: true)
+  end
+
+  class Picture < Base
+    has_many :descriptions, class_name: "PictureDescription"
+  end
+
+  class PictureDescription < Base
+    belongs_to :picture, class_name: "Picture"
+    belongs_to :language, class_name: "Language"
+  end
+
+  def change
+    create_table :alchemy_picture_descriptions do |t|
+      t.belongs_to :picture, null: false, foreign_key: {to_table: :alchemy_pictures}
+      t.belongs_to :language, null: false, foreign_key: {to_table: :alchemy_languages}
+      t.text :text
+
+      t.timestamps
+    end
+
+    reversible do |dir|
+      dir.up do
+        language = Language.default
+        Picture.find_each do |picture|
+          picture.descriptions.create!(
+            text: picture.description.presence,
+            language: language
+          )
+        end
+      end
+    end
+
+    remove_column :alchemy_pictures, :description, :text
+  end
+end
