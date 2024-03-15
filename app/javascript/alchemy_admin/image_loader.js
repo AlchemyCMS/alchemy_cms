42ea34bc3ee0d97bfb446c5a806a2f45f4558d9d
Thomas von Deyen <thomas@vondeyen.com>
2024-03-15 16:25:53 +0100

    Fix image archive error icon size

--- app/javascript/alchemy_admin/image_loader.js
+++ app/javascript/alchemy_admin/image_loader.js
@@ -38,9 +38,9 @@ export default class ImageLoader {
   }
 
   onError(evt) {
-    const message = `Could not load "${this.image.src}"`
+    const message = `Could not load ${this.image.src}`
     this.spinner.stop()
-    this.parent.innerHTML = `<span class="icon error ri-alert-line" title="${message}" />`
+    this.parent.innerHTML = `<alchemy-icon name="alert" class="error" title="${message}" />`
     console.error(message, evt)
     this.unbind()
   }
