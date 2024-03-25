3383976c63b3ac5e92b2732e1b64c2b39a1159e7
Thomas von Deyen <thomas@vondeyen.com>
2024-03-25 18:19:05 +0100

    Add Alchemy::Admin::ListFilter component
    And refactor the existing `list_filter.js` file into a
    `<alchemy-list-filter>` component.

---
+++ app/components/alchemy/admin/list_filter.rb
@@ -0,0 +1,42 @@
+module Alchemy
+  module Admin
+    # Creates a live filter for lists of DOM items.
+    #
+    # The items must have a html +name+ attribute that holds the filterable value.
+    #
+    # == Example
+    #
+    # Given a list of items:
+    #
+    #   <%= render Alchemy::Admin::ListFilter.new('#products .product') %>
+    #
+    #   <ul id="products">
+    #     <li class="product" name="kat litter">Kat Litter</li>
+    #     <li class="product" name="milk">Milk</li>
+    #   </ul>
+    #
+    # @param [String] items_selector - A CSS selector string that represents the items to filter
+    # @param [String] name_attribute - A name that represents the attribute on the items to get filtered by
+    #
+    class ListFilter < ViewComponent::Base
+      erb_template <<~ERB
+        <alchemy-list-filter items-selector="<%= items_selector %>" name-attribute="<%= name_attribute %>">
+          <input type="text" class="js_filter_field" />
+          <alchemy-icon name="search"></alchemy-icon>
+          <button type="button" class="js_filter_field_clear icon_button">
+            <alchemy-icon name="close" size="1x"></alchemy-icon>
+          </button>
+        </alchemy-list-filter>
+      ERB
+
+      def initialize(items_selector, name_attribute: "name")
+        @items_selector = items_selector
+        @name_attribute = name_attribute
+      end
+
+      private
+
+      attr_reader :items_selector, :name_attribute
+    end
+  end
+end
---
+++ app/javascript/alchemy_admin/components/list_filter.js
@@ -0,0 +1,68 @@
+class ListFilter extends HTMLElement {
+  constructor() {
+    super()
+    this.#attachEvents()
+  }
+
+  #attachEvents() {
+    this.filterField.addEventListener("keyup", () => {
+      const term = this.filterField.value
+      this.clearButton.style.visibility = "visible"
+      this.filter(term)
+    })
+    this.clearButton.addEventListener("click", (e) => {
+      e.preventDefault()
+      this.clear()
+    })
+    this.filterField.addEventListener("focus", () =>
+      key.setScope("list_filter")
+    )
+    key("esc", "list_filter", () => {
+      this.clear()
+      this.filterField.blur()
+    })
+  }
+
+  filter(term) {
+    if (term === "") {
+      this.clearButton.style.visibility = "hidden"
+    }
+
+    this.items.forEach((item) => {
+      const name = item.getAttribute(this.nameAttribute)?.toLowerCase()
+      // indexOf is much faster then match()
+      if (name.indexOf(term.toLowerCase()) !== -1) {
+        item.classList.remove("hidden")
+      } else {
+        item.classList.add("hidden")
+      }
+    })
+  }
+
+  clear() {
+    this.filterField.value = ""
+    this.filter("")
+  }
+
+  get nameAttribute() {
+    return this.getAttribute("name-attribute") || "name"
+  }
+
+  get clearButton() {
+    return this.querySelector('button[type="button"]')
+  }
+
+  get filterField() {
+    return this.querySelector('input[type="text"]')
+  }
+
+  get items() {
+    return document.querySelectorAll(this.itemsSelector)
+  }
+
+  get itemsSelector() {
+    return this.getAttribute("items-selector")
+  }
+}
+
+customElements.define("alchemy-list-filter", ListFilter)
