36903fea89da4ea6ef542d1c329a4a6041f82c06
Thomas von Deyen <thomas@vondeyen.com>
2024-05-22 15:37:09 +0200

    Add alchemy-action - component
    Allows to call functions that are registered as action from a
    `turbo_stream` response. It supports closeCurrentDialog, reloadPreview,
    and updateAnchorIcon. The closeCurrentDialog is an intermediate solution
    until all dialogs will respond with a promise.

---
+++ spec/javascript/alchemy_admin/components/action.spec.js
@@ -0,0 +1,49 @@
+import "alchemy_admin/components/action"
+import { renderComponent } from "./component.helper"
+import * as PreviewWindow from "alchemy_admin/components/preview_window"
+import IngredientAnchorLink from "alchemy_admin/ingredient_anchor_link"
+
+jest.mock("alchemy_admin/components/preview_window", () => {
+  return {
+    __esModule: true,
+    reloadPreview: jest.fn()
+  }
+})
+
+jest.mock("alchemy_admin/ingredient_anchor_link", () => {
+  return {
+    __esModule: true,
+    default: {
+      updateIcon: jest.fn()
+    }
+  }
+})
+
+describe("alchemy-action", () => {
+  beforeEach(jest.clearAllMocks)
+
+  it("call reloadPreview function", () => {
+    renderComponent(
+      "alchemy-action",
+      `<alchemy-action name="reloadPreview"></alchemy-action>`
+    )
+    expect(PreviewWindow.reloadPreview).toBeCalled()
+  })
+
+  it("call updateAnchorIcon function with parameter", () => {
+    renderComponent(
+      "alchemy-action",
+      `<alchemy-action name="updateAnchorIcon" params="[123, true]"></alchemy-action>`
+    )
+    expect(IngredientAnchorLink.updateIcon).toBeCalledWith(123, true)
+  })
+
+  it("call closeCurrentDialog function ", () => {
+    window.Alchemy.closeCurrentDialog = jest.fn()
+    renderComponent(
+      "alchemy-action",
+      `<alchemy-action name="closeCurrentDialog"></alchemy-action>`
+    )
+    expect(window.Alchemy.closeCurrentDialog).toBeCalled()
+  })
+})
