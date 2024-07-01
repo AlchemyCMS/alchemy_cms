588b3e1deb7a5190a6a149287a3738c2d8d32a19
dbwinger <dbwinger@gmail.com>
2024-07-01 14:46:59 -0400

    Clear current language from session when switching sites in admin
    (cherry picked from commit d6de41ad48954084d2772ea3f79ce48bf55803d2)

--- app/controllers/concerns/alchemy/admin/current_language.rb
+++ app/controllers/concerns/alchemy/admin/current_language.rb
@@ -6,10 +6,20 @@ module Alchemy
       extend ActiveSupport::Concern
 
       included do
+        # This needs to happen before BaseController#current_alchemy_site sets the session value.
+        prepend_before_action :clear_current_language_from_session, if: :switching_site?, only: :index
         before_action :load_current_language
       end
 
       private
+
+      def switching_site?
+        params[:site_id].present? && (params[:site_id] != session[:alchemy_site_id]&.to_s)
+      end
+
+      def clear_current_language_from_session
+        session.delete(:alchemy_language_id)
+      end
 
       def load_current_language
         @current_language = if session[:alchemy_language_id].present?
