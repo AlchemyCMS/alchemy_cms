3cb2cff186f0c6c775dd268c251e692372426eea
Thomas von Deyen <thomas@vondeyen.com>
2024-08-27 19:05:54 +0200

    Fix Datetime view with rfc822 format option
    Deprecated since Rails 7.0 and gets removed
    in Rails 7.2. Using `to_fs` instead.

--- spec/views/alchemy/ingredients/datetime_view_spec.rb
+++ spec/views/alchemy/ingredients/datetime_view_spec.rb
@@ -6,14 +6,10 @@ describe "alchemy/ingredients/_datetime_view" do
   let(:ingredient) { Alchemy::Ingredients::Datetime.new(value: "2013-10-27 21:14:16 +0100") }
   let(:options) { {} }
 
-  before do
-    allow(view).to receive(:options).and_return(options)
-  end
-
   context "with date value" do
     context "without date_format passed" do
       it "translates the date value with default format" do
-        render ingredient
+        render ingredient, options: options
         expect(rendered).to have_content("Sun, 27 Oct 2013 20:14:16 +0000")
       end
     end
@@ -22,7 +18,7 @@ describe "alchemy/ingredients/_datetime_view" do
       let(:options) { {date_format: "rfc822"} }
 
       it "renders the date rfc822 conform" do
-        render ingredient
+        render ingredient, options: options
         expect(rendered).to have_content("Sun, 27 Oct 2013 20:14:16 +0000")
       end
     end
@@ -32,7 +28,7 @@ describe "alchemy/ingredients/_datetime_view" do
     let(:ingredient) { Alchemy::Ingredients::Datetime.new(value: nil) }
 
     it "renders nothing" do
-      render ingredient
+      render ingredient, options: options
       expect(rendered).to eq("")
     end
   end
