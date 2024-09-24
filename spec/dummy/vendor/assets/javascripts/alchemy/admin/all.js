e63f2660b004faecaefe97c0b5017cc18557f6cf
Thomas von Deyen <thomas@vondeyen.com>
2024-09-24 08:51:08 +0200

    Remove the Sprockets manifest
    We do not compile any JS with Sprockets anymore \o/

--- spec/dummy/vendor/assets/javascripts/alchemy/admin/all.js
+++ spec/dummy/vendor/assets/javascripts/alchemy/admin/all.js
@@ -8,5 +8,4 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require alchemy/admin
 //= require alchemy_i18n/de
