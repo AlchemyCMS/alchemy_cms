78b514570bed306a14328deae2ba4290b32023e6
Thomas von Deyen <thomas@vondeyen.com>
2025-01-05 21:15:21 +0100

    Revert "Remove the Sprockets manifest"
    We want to keep it so we do not break anything in 7.4
    
    This reverts commit b338ef4794dfffcb5417efccf861daefd84a6671.

---
+++ app/assets/javascripts/alchemy/admin.js
@@ -0,0 +1,10 @@
+// Alchemy CMS Sprockets Manifest
+// ------------------------------
+//
+// This manifest file is deprecated and will be removed in Alchemy 8.0.
+//
+
+console.warn(
+  "The 'alchemy/admin' Sprockets manifest is deprecated and will be removed in 8.0. " +
+    "Please remove 'require alchemy/admin' from your 'vendor/assets/javascripts/alchemy/admin/all.js' file."
+)
--- spec/dummy/vendor/assets/javascripts/alchemy/admin/all.js
+++ spec/dummy/vendor/assets/javascripts/alchemy/admin/all.js
@@ -8,4 +8,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require alchemy/admin
 //= require alchemy_i18n/de
