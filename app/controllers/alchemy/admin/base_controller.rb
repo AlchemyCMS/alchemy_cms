cc0bb4c85f5ebde7f4ef3cfb238f0d46ed3d4155
Thomas von Deyen <thomas@vondeyen.com>
2025-01-05 14:37:35 +0100

    Use safe redirect paths in admin redirects
    This makes sure all redirects we do in the admin via do_redirect_to uses a safe redirect url.
    
    (cherry picked from commit 7adefce7fd612bfbde5fb8e4ba261b1407713cc7)
    
    # Conflicts:
    #	app/controllers/alchemy/admin/base_controller.rb

--- app/controllers/alchemy/admin/base_controller.rb
+++ app/controllers/alchemy/admin/base_controller.rb
@@ -46,6 +46,12 @@ module Alchemy
         path.to_s.match? %r{^#{mount_path}admin/}
       end
 
+      def relative_referer_path(referer = request.referer)
+        return unless referer
+
+        URI(referer).path
+      end
+
       # Disable layout rendering for xhr requests.
       def set_layout
         (request.xhr? || turbo_frame_request?) ? false : "alchemy/admin"
@@ -120,15 +126,18 @@ module Alchemy
         end
       end
 
-      # Does redirects for html and js requests
+      # Does redirects for html, turbo_stream and js requests
+      #
+      # Makes sure that the redirect path is safe.
       #
       def do_redirect_to(url_or_path)
+        redirect_path = safe_redirect_path(url_or_path)
         respond_to do |format|
           format.js {
-            @redirect_url = url_or_path
+            @redirect_url = redirect_path
             render :redirect
           }
-          format.html { redirect_to url_or_path }
+          format.html { redirect_to redirect_path }
         end
       end
 
