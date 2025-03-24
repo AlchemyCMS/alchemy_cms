2d8a763395d9ca524d6410cff4a26b35d5338ec1
Martin Meyerhoff <mamhoff@gmail.com>
2025-03-24 15:45:17 +0100

    Error handling: Use custom error class
    This new class allows us targeted rescuing, so we get good error
    messages even on collection options.

--- lib/alchemy/configuration/base_option.rb
+++ lib/alchemy/configuration/base_option.rb
@@ -9,15 +9,13 @@ module Alchemy
 
       def initialize(value:, name:, **args)
         @name = name
-        @value = validate(value) unless value.nil?
+        validate(value) unless value.nil?
+        @value = value
       end
       attr_reader :name, :value
 
-      private
-
       def validate(value)
-        raise TypeError, "#{name} must be set as a #{self.class.value_class.name}, given #{value.inspect}" unless value.is_a?(self.class.value_class)
-        value
+        raise ConfigurationError.new(name, value, self.class.value_class.name) unless value.is_a?(self.class.value_class)
       end
     end
   end
--- lib/alchemy/configuration/boolean_option.rb
+++ lib/alchemy/configuration/boolean_option.rb
@@ -5,10 +5,8 @@ require "alchemy/configuration/base_option"
 module Alchemy
   class Configuration
     class BooleanOption < BaseOption
-      private
-
       def validate(value)
-        raise TypeError, "#{name} must be a Boolean, given #{value.inspect}" unless value.is_a?(TrueClass) || value.is_a?(FalseClass)
+        raise ConfigurationError.new(name, value, "Boolean") unless value.is_a?(TrueClass) || value.is_a?(FalseClass)
         value
       end
     end
