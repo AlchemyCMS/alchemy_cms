ab64a1d6e7d4872cf24fd70d8b65d90f81e9dd19
Thomas von Deyen <vondeyen@blish.cloud>
2025-03-28 12:28:50 +0100

    Fix picture dialog search form
    We need to update it now that we updated the search
    form to use Ransack for filters in 78157e7e0.

--- spec/features/admin/picture_assignment_overlay_spec.rb
+++ spec/features/admin/picture_assignment_overlay_spec.rb
@@ -17,4 +17,23 @@ RSpec.describe "Picture assignment overlay", type: :system do
       expect(page).to have_css %(a[data-method="put"][href*="/admin/pictures/#{picture.id}/assign"])
     end
   end
+
+  describe "filtering the picture list" do
+    let!(:picture) { create(:alchemy_picture, name: "Blue Jeans") }
+    let!(:picture2) { create(:alchemy_picture, name: "Kittens") }
+    let(:element) { create(:alchemy_element, :with_ingredients, name: "header") }
+    let(:ingredient) { element.ingredients.last }
+
+    scenario "by name reduces the list" do
+      visit alchemy.admin_pictures_path(form_field_id: "ingredients_#{ingredient.id}_picture_id")
+      within "#resource_search" do
+        fill_in "q[name_or_image_file_name_cont]", with: "Blue"
+        find("button[type='submit']").click
+      end
+      within "#assign_image_list" do
+        expect(page).to have_content "Blue Jeans"
+        expect(page).to_not have_content "Kittens"
+      end
+    end
+  end
 end
