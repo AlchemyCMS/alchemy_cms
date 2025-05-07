efe3e856817949285f1897bbb39433e3ee27ea33
Thomas von Deyen <vondeyen@blish.cloud>
2025-05-07 10:30:26 +0200

    Use regular form for site and locale selects
    That way we can use the alchemy-auto-submit component we have
    and regular http requests instead of a dedicated change event
    handler.

--- app/javascript/alchemy_admin/initializer.js
+++ app/javascript/alchemy_admin/initializer.js
@@ -1,26 +1,6 @@
 import Hotkeys from "alchemy_admin/hotkeys"
 import pleaseWaitOverlay from "alchemy_admin/please_wait_overlay"
 
-/**
- * add change listener to select to redirect the user after selecting another locale or site
- * @param {string} selectId
- * @param {string} parameterName
- * @param {boolean} forcedReload
- */
-function selectHandler(selectId, parameterName, forcedReload = false) {
-  $(`select#${selectId}`).on("change", function (e) {
-    let url = window.location.pathname
-    let delimiter = url.match(/\?/) ? "&" : "?"
-    const location = `${url}${delimiter}${parameterName}=${$(this).val()}`
-
-    if (forcedReload) {
-      window.location.href = location
-    } else {
-      Turbo.visit(location, {})
-    }
-  })
-}
-
 export default function Initializer() {
   // We obviously have javascript enabled.
   $("html").removeClass("no-js")
@@ -33,17 +13,6 @@ export default function Initializer() {
 
   // Hack for enabling tab focus for <a>'s styled as button.
   $("a.button").attr({ tabindex: 0 })
-
-  // Locale select handler
-  selectHandler("change_locale", "admin_locale", true)
-
-  // Site select handler
-  selectHandler("change_site", "site_id")
-
-  // Submit forms of selects with `data-autosubmit="true"`
-  $('select[data-auto-submit="true"]').on("change", function () {
-    $(this.form).submit()
-  })
 
   // Override the filter of keymaster.js so we can blur the fields on esc key.
   key.filter = function (event) {
