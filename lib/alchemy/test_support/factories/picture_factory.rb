68ee1bdc9162a74dad3f34d69401f3c1b93220b2
Thomas von Deyen <vondeyen@blish.cloud>
2025-06-24 12:48:22 +0200

    Add active storage adapter
    Introduces a new storage adapter for active storage.
    
    Use it by setting[^1]
    
    ```
    Alchemy.config.storage_adapter = "active_storage"
    ```
    
    [^1] Obviously active storage needs to be installed in your app.

--- lib/alchemy/test_support/factories/picture_factory.rb
+++ lib/alchemy/test_support/factories/picture_factory.rb
@@ -12,8 +12,24 @@ FactoryBot.define do
 
     after(:build) do |picture, acc|
       if acc.image_file
-        picture.image_file = acc.image_file
-        picture.image_file_size = acc.image_file.size
+        case Alchemy.storage_adapter.name
+        when :active_storage
+          filename = acc.image_file.original_filename
+          content_type = Marcel::MimeType.for(extension: File.extname(filename))
+          picture.image_file.attach(
+            io: acc.image_file.open,
+            filename:,
+            content_type:,
+            identify: false,
+            metadata: {
+              width: 1,
+              height: 1
+            }
+          )
+        when :dragonfly
+          picture.image_file = acc.image_file
+          picture.image_file_size = acc.image_file.size
+        end
       end
     end
 
