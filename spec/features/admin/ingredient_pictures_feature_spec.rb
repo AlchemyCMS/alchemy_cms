97d71311f736c8d51142d8f49c89aa81d67c8a9e
Thomas von Deyen <vondeyen@blish.cloud>
2025-07-13 20:04:58 +0200

    fix(picture_fields): use correct local variable
    `picture_editor` is not available.

--- spec/features/admin/ingredient_pictures_feature_spec.rb
+++ spec/features/admin/ingredient_pictures_feature_spec.rb
@@ -6,15 +6,23 @@ RSpec.feature "Ingredient Pictures admin feature", type: :system do
   end
 
   let(:language) { create(:alchemy_language) }
+  let(:element) { create(:alchemy_element, name: "all_you_can_eat") }
   let(:picture) { create(:alchemy_picture) }
-  let(:ingredient_picture) { create(:alchemy_ingredient_picture, picture: picture) }
+  let(:ingredient_picture) { create(:alchemy_ingredient_picture, picture: picture, element: element) }
 
-  let!(:picture_description) do
-    Alchemy::PictureDescription.create!(picture: picture, language: language, text: "A nice picture")
+  describe "picture description" do
+    let!(:picture_description) do
+      Alchemy::PictureDescription.create!(picture: picture, language: language, text: "A nice picture")
+    end
+
+    scenario "Picture description is used as default for ingredient picture alt text" do
+      visit alchemy.edit_admin_ingredient_path(ingredient_picture)
+      expect(page).to have_field("Alternative text", placeholder: "A nice picture")
+    end
   end
 
-  scenario "Picture description is used as default for ingredient picture alt text" do
+  scenario "Picture css class can be selected by editor" do
     visit alchemy.edit_admin_ingredient_path(ingredient_picture)
-    expect(page).to have_field("Alternative text", placeholder: "A nice picture")
+    expect(page).to have_select("Style")
   end
 end
