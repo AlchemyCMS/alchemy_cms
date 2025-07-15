c5564395db0c781ce2ebcf768affc4212c419c70
Antonio Facciolo <afdev82@gmail.com>
2025-07-15 11:30:42 +0200

    Leave only alchemy_admin.js for Sprockets manifest, fixes #3245

--- app/assets/config/alchemy_manifest.js
+++ app/assets/config/alchemy_manifest.js
@@ -1,5 +1,5 @@
+//= link alchemy_admin.js
 //= link_tree ../builds/alchemy/
 //= link_tree ../builds/tinymce/
 //= link_tree ../images/alchemy/
-//= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
