444e9f6e00792fb46c2e600ec8a6da29d8990ada
Martin Meyerhoff <mamhoff@gmail.com>
2025-10-09 03:53:19 +0200

    Convert `Alchemy.publish_targets` to option
    Yet another collection option, this time with class names!

--- lib/alchemy.rb
+++ lib/alchemy.rb
@@ -51,29 +51,8 @@ module Alchemy
     delegate :admin_stylesheets, to: :config
     deprecate admin_stylesheets: "Use Alchemy.config.admin_stylesheets instead", deprecator: Alchemy::Deprecation
 
-    # Define page publish targets
-    #
-    # A publish target is a ActiveJob that gets performed
-    # whenever a user clicks the publish page button.
-    #
-    # Use this to trigger deployment hooks of external
-    # services in an asychronous way.
-    #
-    # == Example
-    #
-    #     # app/jobs/publish_job.rb
-    #     class PublishJob < ApplicationJob
-    #       def perform(page)
-    #         RestClient.post(ENV['BUILD_HOOK_URL'])
-    #       end
-    #     end
-    #
-    #     # config/initializers/alchemy.rb
-    #     Alchemy.publish_targets << PublishJob
-    #
-    def publish_targets
-      @_publish_targets ||= Set.new
-    end
+    delegate :publish_targets, to: :config
+    deprecate publish_targets: "Use Alchemy.config.publish_targets instead", deprecator: Alchemy::Deprecation
 
     # Configure tabs in the link dialog
     #
