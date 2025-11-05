956b21bc1a1c26efb104cf7653de2102783ae619
Marc Antwertinger <marc.antwertinger@4eb.com>
2025-11-05 14:59:17 +0100

    Add upload button test for allowed file types

---
+++ spec/views/alchemy/admin/uploader/_button.html.erb_spec.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "alchemy/admin/uploader/_button.html.erb" do
+  let(:object) { Alchemy::Picture.new }
+  let(:file_attribute) { :image_file }
+  let(:redirect_url) { "/admin/pictures" }
+
+  before do
+    allow(view).to receive(:can?).and_return(true)
+  end
+
+  context "when wildcard is configured (all file types allowed)" do
+    before do
+      allow(Alchemy.config.uploader.allowed_filetypes).to receive(:alchemy_pictures) do
+        ["*"]
+      end
+    end
+
+    it "does not render the accept attribute" do
+      render partial: "alchemy/admin/uploader/button",
+             locals: { object: object, file_attribute: file_attribute, redirect_url: redirect_url }
+
+      expect(rendered).to have_selector('input[type="file"].fileupload')
+      expect(rendered).not_to have_selector('input[type="file"][accept]')
+    end
+  end
+
+  context "when specific file types are configured" do
+    before do
+      allow(Alchemy.config.uploader.allowed_filetypes).to receive(:alchemy_pictures) do
+        ["jpg", "png", "gif"]
+      end
+    end
+
+    it "renders the accept attribute with the correct file extensions" do
+      render partial: "alchemy/admin/uploader/button",
+             locals: { object: object, file_attribute: file_attribute, redirect_url: redirect_url }
+
+      expect(rendered).to have_selector('input[type="file"].fileupload')
+      expect(rendered).to have_selector('input[type="file"][accept=".jpg, .png, .gif"]')
+    end
+  end
+
+  context "with Attachment object and wildcard" do
+    let(:object) { Alchemy::Attachment.new }
+    let(:file_attribute) { :file }
+
+    before do
+      allow(Alchemy.config.uploader.allowed_filetypes).to receive(:alchemy_attachments) do
+        ["*"]
+      end
+    end
+
+    it "does not render the accept attribute" do
+      render partial: "alchemy/admin/uploader/button",
+             locals: { object: object, file_attribute: file_attribute, redirect_url: redirect_url }
+
+      expect(rendered).to have_selector('input[type="file"].fileupload')
+      expect(rendered).not_to have_selector('input[type="file"][accept]')
+    end
+  end
+
+  context "with Attachment object and specific file types" do
+    let(:object) { Alchemy::Attachment.new }
+    let(:file_attribute) { :file }
+
+    before do
+      allow(Alchemy.config.uploader.allowed_filetypes).to receive(:alchemy_attachments) do
+        ["pdf", "doc", "docx"]
+      end
+    end
+
+    it "renders the accept attribute with the correct file extensions" do
+      render partial: "alchemy/admin/uploader/button",
+             locals: { object: object, file_attribute: file_attribute, redirect_url: redirect_url }
+
+      expect(rendered).to have_selector('input[type="file"].fileupload')
+      expect(rendered).to have_selector('input[type="file"][accept=".pdf, .doc, .docx"]')
+    end
+  end
+end
