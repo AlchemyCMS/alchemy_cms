1d05eaa1f865bd02253a65250f184b81e0810894
Thomas von Deyen <vondeyen@blish.cloud>
2026-01-11 22:27:34 +0100

    Add Shoelace button and icon component
    Signed-off-by: Thomas von Deyen <vondeyen@blish.cloud>

--- bundles/shoelace.js
+++ bundles/shoelace.js
@@ -7,6 +7,8 @@ import "@shoelace-style/shoelace/dist/components/tab-panel/tab-panel.js"
 import "@shoelace-style/shoelace/dist/components/tooltip/tooltip.js"
 import "@shoelace-style/shoelace/dist/components/progress-bar/progress-bar.js"
 import "@shoelace-style/shoelace/dist/components/dialog/dialog.js"
+import "@shoelace-style/shoelace/dist/components/button/button.js"
+import "@shoelace-style/shoelace/dist/components/icon/icon.js"
 import { registerIconLibrary } from "@shoelace-style/shoelace/dist/utilities/icon-library.js"
 import { setDefaultAnimation } from "@shoelace-style/shoelace/dist/utilities/animation-registry.js"
 export { registerIconLibrary, setDefaultAnimation }
