AlchemyCMS/alchemy_cms

commit 63e0a23b0d53be276199fede81553dc17952e57b

Fix element position not persisting after drag and drop for new elements
Skip validations when saving element position in order action to allow newly created elements (which may not yet meet validation requirements) to be reordered before ingredients are filled in.

Add spec to verify position is saved even when element has validation errors.

app/controllers/alchemy/admin/elements_controller.rb
@@ -100,13 +100,14 @@ module Alchemy
 
       def order
         @element = Element.find(params[:element_id])
-        @element.update(
-          parent_element_id: params[:parent_element_id],
-          position: params[:position]
-        )
-        if params[:parent_element_id].present?
-          @parent_element = Element.find_by(id: params[:parent_element_id])
-        end
+
+        # Update position
+        @element.parent_element_id = params[:parent_element_id] if params.key?(:parent_element_id)
+        @element.position = params[:position]
+
+        # Skip validations when updating position, since new records may not yet meet all
+        # validation requirements.
+        @element.save(validate: false)
 
         render json: {
           message: Alchemy.t(:successfully_saved_element_position),

spec/controllers/alchemy/admin/elements_controller_spec.rb
@@ -55,6 +55,19 @@ module Alchemy
         ])
       end
 
+      it "saves position without validation" do
+        # Create an element that would fail validation if validated
+        element = create(:alchemy_element, page_version: page_version)
+
+        # Stub validation to fail
+        allow_any_instance_of(Element).to receive(:valid?).and_return(false)
+
+        # Position should still be saved
+        expect {
+          post :order, params: {element_id: element.id, position: 2}
+        }.to change { element.reload.position }.to(2)
+      end
+
       context "when nested inside parent element" do
         let(:parent) { create(:alchemy_element) }
 

commit 8f1208842aef109dd4ce2933aa81dc6bc497f1f6

fix: Dragging nested element out of nesting element
We need to be able to nil the `parent_element_id`.

This fixes a regression introduced in 63e0a23b0d53be276199fede81553dc17952e57b

app/controllers/alchemy/admin/elements_controller.rb
@@ -101,7 +101,7 @@ module Alchemy
         @element = Element.find(params[:element_id])
 
         # Update position
-        @element.parent_element_id = params[:parent_element_id] if params.key?(:parent_element_id)
+        @element.parent_element_id = params[:parent_element_id]
         @element.position = params[:position]
 
         # Skip validations when updating position, since new records may not yet meet all

spec/controllers/alchemy/admin/elements_controller_spec.rb
@@ -88,6 +88,20 @@ module Alchemy
           }
           expect(element_3.reload.parent_element_id).to eq parent.id
         end
+
+        context "and dragged outside of parent element" do
+          before do
+            element_3.update(parent_element: parent)
+          end
+
+          it "nils parent_element_id" do
+            expect {
+              post :order, params: {
+                element_id: element_3.id
+              }
+            }.to change { element_3.reload.parent_element_id }.to(nil)
+          end
+        end
       end
     end
 