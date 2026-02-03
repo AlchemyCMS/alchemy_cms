285d88252f84f667343c474ee3e80e90edeb6309
Thomas von Deyen <vondeyen@blish.cloud>
2026-02-03 16:08:42 +0100

    Use say_with_time in migrations
    This gives a nicer output.

--- spec/dummy/db/migrate/20260120163927_convert_select_value_for_multiple.alchemy.rb
+++ spec/dummy/db/migrate/20260120163927_convert_select_value_for_multiple.alchemy.rb
@@ -1,10 +1,12 @@
 # This migration comes from alchemy (originally 20251106150010)
-class ConvertSelectValueForMultiple < ActiveRecord::Migration[7.2]
+class ConvertSelectValueForMultiple < ActiveRecord::Migration[7.1]
   def up
-    execute <<-SQL
+    say_with_time "Converting Alchemy::Ingredients::Select values to multiple" do
+      update <<-SQL.squish
         UPDATE alchemy_ingredients
-        SET value = CONCAT('["', value, '"]')
-        WHERE type = 'Alchemy::Ingredients::Select' AND value NOT LIKE '["%"]' ;
-    SQL
+        SET value = '["' || value || '"]'
+        WHERE type = 'Alchemy::Ingredients::Select' AND value NOT LIKE '["%"]';
+      SQL
+    end
   end
 end
