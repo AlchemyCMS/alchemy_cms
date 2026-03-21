AlchemyCMS/alchemy_cms

commit e53c8f1d80fd2d79a81764efd24a481a1ef5c4f2

feat: Don't allow to edit ingredients if not permitted to
Without the edit ability we need to make sure
the input is not editable.

app/views/alchemy/ingredients/shared/_picture_tools.html.erb
@@ -1,27 +1,26 @@
 <% linkable = picture_editor.settings[:linkable] != false %>
 <% croppable = picture_editor.allow_image_cropping? %>
+<% disabled = cannot?(:edit, picture_editor.ingredient) %>
 
-<%= content_tag "sl-tooltip", content: Alchemy.t("Edit Picturemask"), placement: "top-start", disabled: !croppable do %>
+<%= content_tag "sl-tooltip", content: Alchemy.t("Edit Picturemask"), placement: "top-start", disabled: !croppable || disabled do %>
   <%= link_to_dialog render_icon(:crop),
-    alchemy.crop_admin_ingredient_path(picture_editor.ingredient, {
+    (croppable && !disabled) ? alchemy.crop_admin_ingredient_path(picture_editor.ingredient, {
       crop_from_form_field_id: picture_editor.form_field_id(:crop_from),
       crop_size_form_field_id: picture_editor.form_field_id(:crop_size),
       picture_id: picture_editor.picture&.id
-    }), {
+    }) : nil, {
       size: "1080x615",
       title: Alchemy.t("Edit Picturemask"),
       image_loader: false,
       padding: false
     }, {
-      class: croppable ? "crop_link icon_button" : "disabled crop_link icon_button" ,
-      tabindex: croppable ? nil : "-1",
-      onclick: "return false"
+      class: "crop_link icon_button"
     } %>
 <% end %>
 
-<%= content_tag "sl-tooltip", content: picture_editor.picture ? Alchemy.t(:swap_image) : Alchemy.t(:insert_image) do %>
+<%= content_tag "sl-tooltip", content: picture_editor.picture ? Alchemy.t(:swap_image) : Alchemy.t(:insert_image), disabled: disabled do %>
   <%= link_to_dialog render_icon("image-add"),
-    alchemy.admin_pictures_path(
+    disabled ? nil : alchemy.admin_pictures_path(
       form_field_id: picture_editor.form_field_id(:picture_id)
     ),
     {
@@ -39,9 +38,9 @@
   <span class="icon_button disabled"><%= render_icon("link-unlink", style: "m") %></span>
 <% end %>
 
-<%= content_tag "sl-tooltip", content: Alchemy.t(:edit_image_properties), placement: "top-end" do %>
+<%= content_tag "sl-tooltip", content: Alchemy.t(:edit_image_properties), placement: "top-end", disabled: disabled do %>
   <%= link_to_dialog render_icon(:edit),
-    alchemy.edit_admin_ingredient_path(id: picture_editor.id, language_id: @page.language_id),
+    disabled ? nil : alchemy.edit_admin_ingredient_path(id: picture_editor.id, language_id: @page.language_id),
     {
       title: Alchemy.t(:edit_image_properties),
       size: "380x255"

spec/components/alchemy/ingredients/picture_editor_spec.rb
@@ -26,6 +26,8 @@ RSpec.describe Alchemy::Ingredients::PictureEditor, type: :component do
     allow(ingredient).to receive(:settings) { settings }
     vc_test_view_context.class.include Alchemy::Admin::BaseHelper
     vc_test_view_context.instance_variable_set(:@page, alchemy_page)
+    allow(vc_test_view_context).to receive(:can?).and_return(true)
+    allow(vc_test_view_context).to receive(:cannot?).and_return(false)
   end
 
   subject do
@@ -108,6 +110,37 @@ RSpec.describe Alchemy::Ingredients::PictureEditor, type: :component do
     context "with another column given" do
       it "has for attribute set to ingredient form field id for that column" do
         is_expected.to have_selector("label[for='element_#{element.id}_ingredient_#{ingredient.id}_picture_id']")
+      end
+    end
+  end
+
+  context "without edit permission" do
+    before do
+      allow(vc_test_view_context).to receive(:can?).and_return(false)
+      allow(vc_test_view_context).to receive(:cannot?).and_return(true)
+    end
+
+    it "does not render a delete button" do
+      is_expected.not_to have_selector("button.picture_tool.delete")
+    end
+
+    it "renders a disabled insert image link" do
+      is_expected.to have_selector("a.icon_button.disabled")
+      is_expected.not_to have_selector('a.icon_button[href*="pictures"]')
+    end
+
+    it "renders a disabled edit properties link" do
+      is_expected.not_to have_selector('a.icon_button[href*="edit"]')
+    end
+
+    context "with image cropping enabled" do
+      before do
+        allow(ingredient).to receive(:allow_image_cropping?) { true }
+      end
+
+      it "renders a disabled cropping link" do
+        is_expected.to have_selector("a.disabled.crop_link")
+        is_expected.not_to have_selector('a.crop_link[href*="crop"]')
       end
     end
   end

app/components/alchemy/ingredients/base_editor.rb
@@ -68,6 +68,10 @@ module Alchemy
 
       private
 
+      def editable?
+        @_editable ||= can?(:edit, ingredient)
+      end
+
       # Returns the translated role for displaying in labels
       #
       # Translate it in your locale yml file:

app/components/alchemy/ingredients/boolean_editor.rb
@@ -9,7 +9,7 @@ module Alchemy
           concat label_tag(nil, for: form_field_id) {
             safe_join([
               hidden_field_tag(form_field_name, "0", id: nil),
-              check_box_tag(form_field_name, "1", value, id: form_field_id),
+              check_box_tag(form_field_name, "1", value, id: form_field_id, disabled: !editable?),
               ingredient_role,
               render_hint_for(ingredient, size: "1x", fixed_width: false)
             ])

app/components/alchemy/ingredients/color_editor.rb
@@ -6,7 +6,7 @@ module Alchemy
       def input_field
         content_tag("alchemy-color-select") do
           if colors.present?
-            concat select_tag(form_field_name, options_for_select(colors, color_value))
+            concat select_tag(form_field_name, options_for_select(colors, color_value), disabled: !editable?)
           end
           concat color_field_tag(form_field_name, value, disabled: disabled?)
         end
@@ -36,7 +36,7 @@ module Alchemy
       end
 
       def disabled?
-        color_value != "custom_color" && colors.present?
+        !editable? || (color_value != "custom_color" && colors.present?)
       end
     end
   end

app/components/alchemy/ingredients/datetime_editor.rb
@@ -12,7 +12,8 @@ module Alchemy
               name: form_field_name,
               id: form_field_id,
               value: value,
-              type: settings[:input_type]
+              type: settings[:input_type],
+              disabled: !editable?
             }
           )
           concat tag.label(

app/components/alchemy/ingredients/file_editor.rb
@@ -13,27 +13,29 @@ module Alchemy
             class: "file_icon"
           )
           concat tag.div(attachment&.name, class: "file_name")
-          concat link_to(
-            render_icon(:times), "#",
-            class: [
-              "remove_file_link",
-              attachment ? nil : "hidden"
-            ],
-            data: {
-              form_field_id: form_field_id(:attachment_id)
-            }
-          )
+          if editable?
+            concat link_to(
+              render_icon(:times), "#",
+              class: [
+                "remove_file_link",
+                attachment ? nil : "hidden"
+              ],
+              data: {
+                form_field_id: form_field_id(:attachment_id)
+              }
+            )
+          end
           concat(
             tag.div(class: "file_tools") do
               concat dialog_link
               concat link_to_dialog(
                 render_icon(:edit),
-                alchemy.edit_admin_ingredient_path(ingredient),
+                editable? ? alchemy.edit_admin_ingredient_path(ingredient) : nil,
                 {
                   title: Alchemy.t(:edit_file_properties),
                   size: "400x215"
                 },
-                title: Alchemy.t(:edit_file_properties)
+                title: editable? ? Alchemy.t(:edit_file_properties) : nil
               )
             end
           )
@@ -48,18 +50,18 @@ module Alchemy
       def dialog_link
         link_to_dialog(
           render_icon("file-add"),
-          alchemy.admin_attachments_path(
+          editable? ? alchemy.admin_attachments_path(
             form_field_id: form_field_id(:attachment_id),
             only: Array(settings[:only]),
             except: Array(settings[:except])
-          ),
+          ) : nil,
           {
             title: Alchemy.t(:assign_file),
             size: "780x585",
             padding: false
           },
           class: "file_icon",
-          title: Alchemy.t(:assign_file)
+          title: editable? ? Alchemy.t(:assign_file) : nil
         )
       end
     end

app/components/alchemy/ingredients/headline_editor.rb
@@ -11,6 +11,7 @@ module Alchemy
             value,
             minlength: length_validation&.fetch(:minimum, nil),
             maxlength: length_validation&.fetch(:maximum, nil),
+            readonly: !editable?,
             required: presence_validation?,
             pattern: format_validation,
             id: form_field_id)
@@ -24,12 +25,14 @@ module Alchemy
 
           concat(
             tag.div(class: ["input-addon", "right", has_size_select? ? "second" : nil].compact) do
-              content_tag("sl-tooltip", content: ingredient.class.human_attribute_name(:level)) do
+              content_tag("sl-tooltip",
+                content: ingredient.class.human_attribute_name(:level),
+                disabled: !editable?) do
                 select_tag(
                   form_field_name(:level),
                   options_for_select(level_options, level),
                   class: "custom-select",
-                  disabled: !has_level_select?
+                  disabled: !has_level_select? || !editable?
                 )
               end
             end
@@ -38,11 +41,14 @@ module Alchemy
           if has_size_select?
             concat(
               tag.div(class: "input-addon right") do
-                content_tag("sl-tooltip", content: ingredient.class.human_attribute_name(:size)) do
+                content_tag("sl-tooltip",
+                  content: ingredient.class.human_attribute_name(:size),
+                  disabled: !editable?) do
                   select_tag(
                     form_field_name(:size),
                     options_for_select(size_options, size),
-                    class: "custom-select"
+                    class: "custom-select",
+                    disabled: !editable?
                   )
                 end
               end

commit 0c6cdc9b48ca7f23f6c712e53ff6cd5aad0ff9d3

Fix picture crop dialog link not opening for newly assigned pictures
When a picture ingredient had crop configured but no picture assigned yet,
the crop link was rendered as a plain <a> tag without the alchemy-dialog-link
custom element behavior. After assigning a picture, the JavaScript would
update the href and remove the disabled class, but the link still navigated
normally instead of opening a dialog because it was never registered as a
custom element.

This fixes a bug introduced in e53c8f1d8

app/views/alchemy/ingredients/shared/_picture_tools.html.erb
@@ -1,10 +1,11 @@
 <% linkable = picture_editor.settings[:linkable] != false %>
 <% croppable = picture_editor.allow_image_cropping? %>
+<% crop_configured = !!picture_editor.settings[:crop] %>
 <% disabled = cannot?(:edit, picture_editor.ingredient) %>
 
 <%= content_tag "sl-tooltip", content: Alchemy.t("Edit Picturemask"), placement: "top-start", disabled: !croppable || disabled do %>
   <%= link_to_dialog render_icon(:crop),
-    (croppable && !disabled) ? alchemy.crop_admin_ingredient_path(picture_editor.ingredient, {
+    (crop_configured && !disabled) ? alchemy.crop_admin_ingredient_path(picture_editor.ingredient, {
       crop_from_form_field_id: picture_editor.form_field_id(:crop_from),
       crop_size_form_field_id: picture_editor.form_field_id(:crop_size),
       picture_id: picture_editor.picture&.id
@@ -14,7 +15,7 @@
       image_loader: false,
       padding: false
     }, {
-      class: "crop_link icon_button"
+      class: ["crop_link icon_button", ("disabled" unless croppable)].compact.join(" ")
     } %>
 <% end %>
 

spec/components/alchemy/ingredients/picture_editor_spec.rb
@@ -79,6 +79,8 @@ RSpec.describe Alchemy::Ingredients::PictureEditor, type: :component do
   end
 
   context "with image cropping enabled" do
+    let(:settings) { {crop: true, size: "400x300"} }
+
     before do
       allow(ingredient).to receive(:allow_image_cropping?) { true }
     end
@@ -93,6 +95,22 @@ RSpec.describe Alchemy::Ingredients::PictureEditor, type: :component do
 
     it "has crop_size hidden field" do
       is_expected.to have_selector("input[type=\"hidden\"][id=\"#{ingredient_editor.form_field_id(:crop_size)}\"]")
+    end
+  end
+
+  context "with crop configured but no picture assigned" do
+    let(:settings) { {crop: true, size: "400x300"} }
+
+    before do
+      allow(ingredient).to receive(:allow_image_cropping?) { false }
+    end
+
+    it "renders crop link as a dialog link so it works after picture assignment" do
+      is_expected.to have_selector('a.crop_link[is="alchemy-dialog-link"][href*="crop"]')
+    end
+
+    it "renders crop link as disabled" do
+      is_expected.to have_selector("a.crop_link.disabled")
     end
   end
 