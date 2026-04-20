6a14d59576464b8eba0ca23a98b15ee1808a8e3a
Thomas von Deyen <vondeyen@blish.cloud>
2026-04-20 17:15:19 +0200

    fix(ClipboardButton): Move DOM mutation out of constructor
    Web Components spec requires constructors to avoid mutating the DOM,
    because custom element constructors run during cloneNode(true). Writing
    innerHTML in the constructor leaves clones in an inconsistent state and
    can trip up libraries like jQuery's clone(true), which SortableJS uses
    for drag clones.
    
    Prepend the icon via insertAdjacentHTML inside connectedCallback with
    a guard that makes the insertion idempotent. Initialize the Clipboard
    instance on connect so it is paired with the disconnectedCallback
    teardown and survives reconnection.

--- app/javascript/alchemy_admin/components/clipboard_button.js
+++ app/javascript/alchemy_admin/components/clipboard_button.js
@@ -2,12 +2,8 @@ import "clipboard"
 import { growl } from "alchemy_admin/growler"
 
 class ClipboardButton extends HTMLElement {
-  constructor() {
-    super()
-
-    this.innerHTML = `
-      <alchemy-icon name="clipboard"></alchemy-icon>
-    `
+  connectedCallback() {
+    this.innerHTML = '<alchemy-icon name="clipboard"></alchemy-icon>'
 
     this.clipboard = new ClipboardJS(this, {
       text: () => {
