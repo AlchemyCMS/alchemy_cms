37beec9c2d4c1ac01af19761e3855484c596af32
Thomas von Deyen <vondeyen@blish.cloud>
2026-06-15 10:11:29 +0200

    feat(Forms): Use CSS grid for form rows.
    Instead of a float based layout that made it necessary to
    have a clearfix and add every single potential input / component
    that goes into the second column into a CSS selector we use
    the very much established CSS grid now.
    
    Most changes are deletions, but some quirks were necessary, but
    all of them make very much sense.
    
    This changes the label/input ratio from 0.35 / 1.65 to 1 / 2.25
    in order to make more room for inputs and components and still keep
    the label wide enough.

--- app/components/alchemy/admin/element_schedule_timestamps.rb
+++ app/components/alchemy/admin/element_schedule_timestamps.rb
@@ -2,18 +2,16 @@ module Alchemy
   module Admin
     class ElementScheduleTimestamps < ViewComponent::Base
       erb_template <<~HTML
-        <div class="input">
-          <div class="input-row">
-            <div class="input-column">
-              <label for="element_public_on"><%= Alchemy::Element.human_attribute_name(:public_on) %></label>
-              <%= datetime_local_field :element, :public_on, include_seconds: false %>
-              <%= error_for(:public_on) %>
-            </div>
-            <div class="input-column">
-              <label for="element_public_until"><%= Alchemy::Element.human_attribute_name(:public_until) %></label>
-              <%= datetime_local_field :element, :public_until, include_seconds: false %>
-              <%= error_for(:public_until) %>
-            </div>
+        <div class="input-row">
+          <div class="input-column">
+            <label for="element_public_on" class="control-label"><%= Alchemy::Element.human_attribute_name(:public_on) %></label>
+            <%= datetime_local_field :element, :public_on, include_seconds: false %>
+            <%= error_for(:public_on) %>
+          </div>
+          <div class="input-column">
+            <label for="element_public_until" class="control-label"><%= Alchemy::Element.human_attribute_name(:public_until) %></label>
+            <%= datetime_local_field :element, :public_until, include_seconds: false %>
+            <%= error_for(:public_until) %>
           </div>
         </div>
       HTML
