3a1c675346a6a0f4c503e4cbf6abc706dec6c07f
Thomas von Deyen <vondeyen@blish.cloud>
2026-07-02 12:41:51 +0200

    feat: Make dragonfly an optional dependency
    With Active Storage being the default storage adapter, apps should not
    be forced to install dragonfly and its ImageMagick toolchain anymore.
    Apps still using the legacy dragonfly storage adapter declare the gem
    in their own Gemfile instead, so all dragonfly specific code paths only
    load if the gem is actually present.

--- spec/libraries/dragonfly/processors/auto_orient_spec.rb
+++ spec/libraries/dragonfly/processors/auto_orient_spec.rb
@@ -3,15 +3,18 @@
 require "rails_helper"
 require_relative "../../../support/dragonfly_test_app"
 
-RSpec.describe Alchemy::Dragonfly::Processors::AutoOrient, if: Alchemy.storage_adapter.dragonfly? do
-  let(:app) { dragonfly_test_app }
-  let(:file) { fixture_file_upload("80x60.png") }
-  let(:image) { Dragonfly::Content.new(app, file) }
-  let(:processor) { described_class.new }
+# Only loadable with the dragonfly gem installed
+if Alchemy.storage_adapter.dragonfly?
+  RSpec.describe Alchemy::Dragonfly::Processors::AutoOrient do
+    let(:app) { dragonfly_test_app }
+    let(:file) { fixture_file_upload("80x60.png") }
+    let(:image) { Dragonfly::Content.new(app, file) }
+    let(:processor) { described_class.new }
 
-  it "works" do
-    expect {
-      processor.call(image)
-    }.to_not raise_error
+    it "works" do
+      expect {
+        processor.call(image)
+      }.to_not raise_error
+    end
   end
 end
