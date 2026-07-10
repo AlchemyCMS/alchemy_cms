32a5f9d16ac4eeec970c624862d9f59fe9ca18a7
Thomas von Deyen <vondeyen@blish.cloud>
2026-07-10 08:40:08 +0200

    chore(docker): slim and share the dev Docker image (#4049)

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
 
 Alchemy::Seeder.seed!
+
+# Development admin so you can log into the dummy app right away (admin/test1234).
+if defined?(Alchemy::User)
+  Alchemy::User.find_or_create_by!(login: "admin") do |user|
+    user.email = "admin@example.com"
+    user.password = "test1234"
+    user.password_confirmation = "test1234"
+    user.alchemy_roles = ["admin"]
+  end
+end
