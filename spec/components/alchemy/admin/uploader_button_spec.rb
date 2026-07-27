8fb282c906bec2ea07c516631f46564a995d8772
Thomas von Deyen <vondeyen@blish.cloud>
2026-07-27 12:33:11 +0200

    chore: Convert uploader button into a view component
    Rendering the `alchemy/admin/uploader/button` partial
    still works but is deprecated now.

---
+++ spec/components/alchemy/admin/uploader_button_spec.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Alchemy::Admin::UploaderButton, type: :component do
+  let(:object) { Alchemy::Picture.new }
+  let(:file_attribute) { :image_file }
+  let(:redirect_url) { "/admin/pictures" }
+
+  subject(:render) do
+    render_inline(described_class.new(object:, file_attribute:, redirect_url:))
+  end
+
+  it "renders a alchemy-uploader component" do
+    render
+    expect(page).to have_selector("alchemy-uploader[redirect-url='/admin/pictures']")
+  end
+
+  context "when wildcard is configured (all file types allowed)" do
+    before do
+      allow(Alchemy.config.uploader.allowed_filetypes).to receive(:alchemy_pictures) do
+        ["*"]
+      end
+    end
+
+    it "does not render the accept attribute" do
+      render
+      expect(page).to have_selector('input[type="file"].fileupload')
+      expect(page).not_to have_selector('input[type="file"][accept]')
+    end
+  end
+
+  context "when specific file types are configured" do
+    before do
+      allow(Alchemy.config.uploader.allowed_filetypes).to receive(:alchemy_pictures) do
+        ["jpg", "png", "gif"]
+      end
+    end
+
+    it "renders the accept attribute with the correct file extensions" do
+      render
+      expect(page).to have_selector('input[type="file"].fileupload')
+      expect(page).to have_selector('input[type="file"][accept=".jpg, .png, .gif"]')
+    end
+  end
+
+  context "with Attachment object and wildcard" do
+    let(:object) { Alchemy::Attachment.new }
+    let(:file_attribute) { :file }
+
+    before do
+      allow(Alchemy.config.uploader.allowed_filetypes).to receive(:alchemy_attachments) do
+        ["*"]
+      end
+    end
+
+    it "does not render the accept attribute" do
+      render
+      expect(page).to have_selector('input[type="file"].fileupload')
+      expect(page).not_to have_selector('input[type="file"][accept]')
+    end
+  end
+
+  context "with Attachment object and specific file types" do
+    let(:object) { Alchemy::Attachment.new }
+    let(:file_attribute) { :file }
+
+    before do
+      allow(Alchemy.config.uploader.allowed_filetypes).to receive(:alchemy_attachments) do
+        ["pdf", "doc", "docx"]
+      end
+    end
+
+    it "renders the accept attribute with the correct file extensions" do
+      render
+      expect(page).to have_selector('input[type="file"].fileupload')
+      expect(page).to have_selector('input[type="file"][accept=".pdf, .doc, .docx"]')
+    end
+  end
+
+  describe "upload_hash field" do
+    context "when the object responds to upload_hash" do
+      it "renders a hidden upload_hash field" do
+        render
+        expect(page).to have_selector('input[type="hidden"][name="picture[upload_hash]"]', visible: :hidden)
+      end
+    end
+
+    context "when the object does not respond to upload_hash" do
+      let(:object) { Alchemy::Attachment.new }
+      let(:file_attribute) { :file }
+
+      it "does not render a upload_hash field" do
+        render
+        expect(page).not_to have_selector('input[name*="upload_hash"]', visible: :all)
+      end
+    end
+  end
+end
