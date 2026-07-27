73bb91c16af0db4b5e3504284795a64fe7afe580
Thomas von Deyen <vondeyen@blish.cloud>
2026-07-27 16:27:06 +0200

    chore: Convert gradient-toolbar extend into class
    That way we share the same button styles with
    all toolbars, not only the main toolbar.

--- spec/features/admin/resources_integration_spec.rb
+++ spec/features/admin/resources_integration_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "Resources", type: :system do
   describe "index view" do
     it "should have a button for creating a new resource items" do
       visit "/admin/events"
-      expect(page).to have_selector('#toolbar sl-tooltip a.icon_button alchemy-icon[name="add"]')
+      expect(page).to have_selector('.toolbar a.button.with_icon alchemy-icon[name="add"]')
     end
 
     it "should list existing items" do
@@ -119,7 +119,7 @@ RSpec.describe "Resources", type: :system do
               expect(page).to_not have_content("yesterday")
             end
 
-            within "#toolbar" do
+            within ".toolbar" do
               expect(page).to have_link(href: %r{q%5Bby_timeframe%5D=starting_today})
             end
           end
