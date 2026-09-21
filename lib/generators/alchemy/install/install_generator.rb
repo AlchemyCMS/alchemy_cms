bfdaa05f1d9edc5361acdadc81216e9b02d6a669
Thomas von Deyen <vondeyen@blish.cloud>
2026-09-21 16:00:25 +0200

    fix(installer): Abort if gutentag migrations fail
    The installer would continue otherwise
    
    (cherry picked from commit 5ccddca0a0616fd63eea62647ffdeb02964e18ae)

--- lib/generators/alchemy/install/install_generator.rb
+++ lib/generators/alchemy/install/install_generator.rb
@@ -89,8 +89,8 @@ module Alchemy
       end
 
       def install_gutentag_migrations
-        rake "gutentag:install:migrations"
-        generate "gutentag:migration_versions"
+        rake "gutentag:install:migrations", abort_on_failure: true
+        generate "gutentag:migration_versions", abort_on_failure: true
       end
 
       def setup_database
